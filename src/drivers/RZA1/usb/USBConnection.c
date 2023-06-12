7c9ea25a75e3a5b5e9296ddc96af4b7afb1d925b
m-m-adams <mark.adams@queensu.ca>
2023-06-12 12:03:27 -0400

    remove comment about duplicate configs not working

--- src/drivers/RZA1/usb/USBConnection.c
+++ src/drivers/RZA1/usb/USBConnection.c
@@ -42,8 +42,8 @@ extern uint8_t* g_midi_string_table[];
 static usb_descriptor_t usb_descriptor = {
     (uint8_t*)g_midi_device,        /* Pointer to the device descriptor */
     (uint8_t*)g_midi_configuration, /* Pointer to the configuration descriptor for Full-speed */
-    (uint8_t*)g_midi_configuration,
-    /* Pointer to the configuration descriptor for High-speed */ // Won't work if this is set to the same memory as the full-speed one, though it does work if pointing to a duplicate one, weirdly. NULL seems fine
+    (uint8_t*)g_midi_configuration, /* Pointer to the configuration descriptor for High-speed */
+
     (uint8_t*)0,                   // Don't think we need this /* Pointer to the qualifier descriptor */
     (uint8_t**)g_midi_string_table /* Pointer to the string descriptor table */
 };
