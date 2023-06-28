de955dbe2d677e3445713937731fd486cbf81491
Katherine Whitlock <kate@skylinesynths.nyc>
2023-06-28 01:59:18 -0400

    Add __libc_init_array to resetprg (confirmed working)

--- src/resetprg.c
+++ src/resetprg.c
@@ -70,12 +70,9 @@
 #define HAVE_CALL_INDIRECT
 #endif
 
-#ifdef HAVE_INITFINI_ARRAY
-#define _init __libc_init_array
-#define _fini __libc_fini_array
-#endif
-
 extern int R_CACHE_L1Init(void);
+
+extern void __libc_init_array(void);
 
 void* __dso_handle = NULL;
 
@@ -147,6 +144,8 @@ void resetprg(void) {
 
 	__enable_irq();
 	__enable_fiq();
+
+	__libc_init_array();
 
 	main1();
 
