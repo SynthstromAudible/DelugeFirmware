3cf50a4ad1f5dd7b18361529869c6bb8859c7583
Aria Burrell <litui@litui.ca>
2023-07-05 13:19:53 -0600

    DBT suggested fixes.
    Removed and ignored openocd.log.
    
    Fixed python formatting.

--- scripts/dbt_tools/link_quirks.py
+++ scripts/dbt_tools/link_quirks.py
@@ -2,18 +2,19 @@ from SCons.Action import Action
 from SCons.Builder import Builder
 from SCons.Script import Touch
 
+
 def exists():
     return True
 
 
 def generate(env):
     """Force use of C++ instead of ld for linking.
-    
+
     This adapts what was in place in e2studio.
     """
     env.Replace(
         LINKER_SCRIPT_PATH="linker_script_rz_a1l.ld",
-        LINKCOM="$CXX $CCFLAGS -o $TARGET $SOURCES $LINKFLAGS"
+        LINKCOM="$CXX $CCFLAGS -o $TARGET $SOURCES $LINKFLAGS",
     )
 
     env.Append(
