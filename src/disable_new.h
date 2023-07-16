03bbe5e193e2c801ee059716b2f19b1778a22b0c
Paul Freund <freund.paul@lvl3.org>
2023-07-16 11:37:19 +0200

    Forgot formatting

--- src/disable_new.h
+++ src/disable_new.h
@@ -2,18 +2,18 @@
 #include <stdexcept>
 
 inline void* operator new(std::size_t) {
-    extern void *DO_NOT_USE_UNARY_NEW();
-    return DO_NOT_USE_UNARY_NEW();
+	extern void* DO_NOT_USE_UNARY_NEW();
+	return DO_NOT_USE_UNARY_NEW();
 }
 inline void* operator new(std::size_t, std::nothrow_t) noexcept {
-    extern void *DO_NOT_USE_UNARY_NEW();
-    return DO_NOT_USE_UNARY_NEW();
+	extern void* DO_NOT_USE_UNARY_NEW();
+	return DO_NOT_USE_UNARY_NEW();
 }
 inline void* operator new[](std::size_t) {
-    extern void *DO_NOT_USE_UNARY_NEW();
-    return DO_NOT_USE_UNARY_NEW();
+	extern void* DO_NOT_USE_UNARY_NEW();
+	return DO_NOT_USE_UNARY_NEW();
 }
-inline void* operator new[](std::size_t, std::nothrow_t) noexcept{
-    extern void *DO_NOT_USE_UNARY_NEW();
-    return DO_NOT_USE_UNARY_NEW();
+inline void* operator new[](std::size_t, std::nothrow_t) noexcept {
+	extern void* DO_NOT_USE_UNARY_NEW();
+	return DO_NOT_USE_UNARY_NEW();
 };
