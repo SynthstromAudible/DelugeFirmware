7ec6c83781e1e9f89e8e40f571ec5335bb76bbdd
seangoodvibes <138174805+seangoodvibes@users.noreply.github.com>
2023-07-26 23:11:46 -0400

    Update deluge.cpp

--- src/deluge/deluge.cpp
+++ src/deluge/deluge.cpp
@@ -16,6 +16,7 @@
 */
 
 #include <gui/views/automation_clip_view.h>
+#include "definitions_cxx.hpp"
 #include "gui/views/arranger_view.h"
 #include "processing/engines/audio_engine.h"
 #include "storage/audio/audio_file_manager.h"
