096c51e79fe9f93ddedd506be84c363b69e3c8f7
Katherine Whitlock <kate@skylinesynths.nyc>
2023-07-28 19:23:02 -0400

    Update VSCode files

--- scripts/tasks/task-configure.py
+++ scripts/tasks/task-configure.py
@@ -46,22 +46,28 @@ def argparser() -> argparse.ArgumentParser:
 
 def main(argv: Sequence[str] = sys.argv) -> int:
     (args, unknown_args) = argparser().parse_known_args(argv)
-    os.chdir(util.get_git_root())
+
+    project_root = util.get_git_root()
+    build_dir = project_root.absolute() / "build"
+    source_dir = project_root.absolute()
+
     if args.force:
         result = importlib.import_module("task-nuke").main()
         if result != 0:
             return result
 
-    configure_args = [
-        "-B",
-        "build",
-        "-G",
-        "Ninja Multi-Config",
-        "-DCMAKE_CROSS_CONFIGS:STRING=all",
-        "-DCMAKE_DEFAULT_CONFIGS=Debug;Release",
-        "-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE",
+    configure_args = []
+    configure_args += ["-B", build_dir]  # build location
+    configure_args += ["-S", source_dir]  # project root
+    configure_args += ["-G", "Ninja Multi-Config"]  # generator
+
+    configure_args += [
+        "-DCMAKE_CROSS_CONFIGS:STRING=all"  # which configs will reference others
+        "-DCMAKE_DEFAULT_CONFIGS=Debug;Release",  # set the default (empty) configs
+        "-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE",  # export compile commands
     ]
 
+    # add the metadata suffix (see project CMakeLists.txt)
     if args.tag_metadata:
         configure_args += [
             "-DENABLE_VERSIONED_OUTPUT:BOOL=TRUE",
