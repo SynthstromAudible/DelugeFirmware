80d4dfd314cbebe65a7cd47f039ed3347807ae80
Katherine Whitlock <kate@skylinesynths.nyc>
2023-07-30 19:25:56 -0400

    e2 via CMake (#265)
    * CMake working
    
    * Update for current source tree
    
    * Flag parity with e2
    
    * Source changes (include fixup/copy define)
    
    * Update CMake config
    
    * Update DBT tasks
    
    * Add option to surpress python upgrade to dbt.cmd
    
    * e2 build config via Makefile
    
    * Update .gitignore
    
    * CMake build Tasks
    
    * Set generator in VSCode CMake settings
    
    * CMakeLists.txt population
    
    * Update target name
    
    * Add `dbt --help`
    
    * Add `dbt nuke` task for build dir removal
    
    * Update GitHub build actions
    
    * Add buildgen prelude to CMakeLists.txt
    
    * Update README
    
    * Add version and metadata tag option
    
    * Fix build, update README
    
    * Update VSCode files
    
    * VSCode tasks problem matcher
    
    * e2 project filters
    
    * Fix string bug with dbt configure
    
    * Hopefully fix build
    
    * Globbed source lists
    
    * Move some things to more logical locations
    
    * Move firmware targets back to toplevel
    
    * Fix issue with linker script (hopefully the last time)
    
    * Embed firmware version in firmware file

---
+++ scripts/cmake/CMakeToolchainDeluge.cmake
@@ -0,0 +1,65 @@
+set(CMAKE_SYSTEM_NAME               Generic)
+set(CMAKE_SYSTEM_PROCESSOR          arm)
+
+IF(DEFINED ENV{DBT_TOOLCHAIN_PATH})
+  set(TOOLCHAIN_ROOT $ENV{DBT_TOOLCHAIN_PATH})
+else()
+  set(TOOLCHAIN_ROOT ".")
+endif()
+
+if(WIN32)
+  set(TOOLCHAIN_EXT ".exe" )
+  set(TOOLCHAIN_TRIPLE "win32-x86_64")
+else()
+  set(TOOLCHAIN_EXT "" )
+  set(TOOLCHAIN_TRIPLE ${CMAKE_HOST_SYSTEM_NAME})
+  string(TOLOWER ${TOOLCHAIN_TRIPLE} TOOLCHAIN_TRIPLE)
+  set(TOOLCHAIN_TRIPLE "${TOOLCHAIN_TRIPLE}-${CMAKE_HOST_SYSTEM_PROCESSOR}")
+endif(WIN32)
+
+cmake_path(SET ARM_TOOLCHAIN_ROOT ${TOOLCHAIN_ROOT}/toolchain/${TOOLCHAIN_TRIPLE}/arm-none-eabi-gcc/)
+cmake_path(ABSOLUTE_PATH ARM_TOOLCHAIN_ROOT)
+
+cmake_path(SET CMAKE_SYSROOT ${ARM_TOOLCHAIN_ROOT}/arm-none-eabi)
+cmake_path(SET CMAKE_FIND_ROOT_PATH ${ARM_TOOLCHAIN_ROOT}/arm-none-eabi)
+cmake_path(SET ARM_TOOLCHAIN_BIN_PATH ${ARM_TOOLCHAIN_ROOT}/bin)
+
+# Without that flag CMake is not able to pass test compilation check
+set(CMAKE_TRY_COMPILE_TARGET_TYPE   STATIC_LIBRARY)
+
+set(CMAKE_AR           ${ARM_TOOLCHAIN_BIN_PATH}/arm-none-eabi-ar${TOOLCHAIN_EXT} CACHE FILEPATH "Path to archiver.")
+set(CMAKE_ASM_COMPILER ${ARM_TOOLCHAIN_BIN_PATH}/arm-none-eabi-gcc${TOOLCHAIN_EXT} CACHE FILEPATH "Path to ASM compiler.")
+set(CMAKE_C_COMPILER   ${ARM_TOOLCHAIN_BIN_PATH}/arm-none-eabi-gcc${TOOLCHAIN_EXT} CACHE FILEPATH "Path to C Compiler.")
+set(CMAKE_CXX_COMPILER ${ARM_TOOLCHAIN_BIN_PATH}/arm-none-eabi-g++${TOOLCHAIN_EXT} CACHE FILEPATH "Path to C++ Compiler.")
+set(CMAKE_LINKER       ${ARM_TOOLCHAIN_BIN_PATH}/arm-none-eabi-ld${TOOLCHAIN_EXT} CACHE FILEPATH "Path to linker.")
+set(CMAKE_OBJCOPY      ${ARM_TOOLCHAIN_BIN_PATH}/arm-none-eabi-objcopy${TOOLCHAIN_EXT} CACHE FILEPATH "Path to objcopy.")
+set(CMAKE_RANLIB       ${ARM_TOOLCHAIN_BIN_PATH}/arm-none-eabi-ranlib${TOOLCHAIN_EXT} CACHE FILEPATH "Path to ranlib.")
+set(CMAKE_SIZE         ${ARM_TOOLCHAIN_BIN_PATH}/arm-none-eabi-size${TOOLCHAIN_EXT} CACHE FILEPATH "Path to size.")
+set(CMAKE_STRIP        ${ARM_TOOLCHAIN_BIN_PATH}/arm-none-eabi-strip${TOOLCHAIN_EXT} CACHE FILEPATH "Path to strip.")
+
+set(CMAKE_ASM_FLAGS_RELEASE "-O2 -DNDEBUG" CACHE STRING "Flags used by the ASM compiler during RELEASE builds." FORCE)
+set(CMAKE_C_FLAGS_RELEASE "-O2 -DNDEBUG" CACHE STRING "Flags used by the C compiler during RELEASE builds."   FORCE)
+set(CMAKE_CXX_FLAGS_RELEASE "-O2 -DNDEBUG" CACHE STRING "Flags used by the C++ compiler during RELEASE builds."  FORCE)
+
+set(CMAKE_EXE_LINKER_FLAGS "--specs=nano.specs --specs=rdimon.specs" CACHE STRING "Flags used by the linker during all build types.")
+
+# Architecture
+set(ARCH_FLAGS
+  -mcpu=cortex-a9
+  #-march=armv7-a
+  -marm
+  -mthumb-interwork
+  -mlittle-endian
+  -mfloat-abi=hard
+  -mfpu=neon
+)
+add_compile_options(${ARCH_FLAGS})
+add_link_options(${ARCH_FLAGS})
+
+add_compile_options(
+  -fmessage-length=0
+)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
---
+++ scripts/tasks/task-clean.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+import argparse
+import subprocess
+import sys
+import util
+import os
+
+def argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="clean",
+        description="Cleanup any build artifacts in the current environment",
+    )
+    parser.group = "Building"
+    return parser
+
+
+def main() -> int:
+    os.chdir(util.get_git_root())
+
+    if not os.path.exists("build"):
+        print("Already clean!")
+        exit()
+        
+    cmake_args = [
+        "--build",
+        "build",
+        "--target", 'clean',
+    ]
+    result = subprocess.run(['cmake'] + cmake_args, env=os.environ)
+    return result.returncode
+
+if __name__ == "__main__":
+    main()
