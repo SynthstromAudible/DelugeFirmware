d6fe4696dbe9527155c0d92a1c20c3886f3d070f
m-m-adams <mark.adams@queensu.ca>
2023-08-06 12:16:39 -0400

    create lp ladder crtp classes

---
+++ src/deluge/dsp/filter/lpladder.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2015-2023 Synthstrom Audible Limited
+ *
+ * This file is part of The Synthstrom Audible Deluge Firmware.
+ *
+ * The Synthstrom Audible Deluge Firmware is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "definitions_cxx.hpp"
+#include "dsp/filter/filter.h"
+#include "dsp/filter/ladder_components.h"
+#include "util/functions.h"
+#include <cstdint>
+
+class LpLadderFilter : public LowPassFilter<LpLadderFilter> {
+public:
+	LpLadderFilter() = default;
+	//returns a compensatory gain value
+	q31_t set_config(q31_t lpfFrequency, q31_t lpfResonance, LPFMode lpfMode, q31_t filterGain);
+	void do_filter(q31_t* outputSample, q31_t* endSample, int32_t sampleIncrememt, int32_t extraSaturation);
+	void reset_filter() {
+		lpfLPF1.reset();
+		lpfLPF2.reset();
+		lpfLPF3.reset();
+		lpfLPF4.reset();
+	}
+
+private:
+	inline q31_t do24dBLPFOnSample(q31_t input, int32_t saturationLevel);
+	inline q31_t doDriveLPFOnSample(q31_t input, int32_t extraSaturation = 0);
+	inline void renderLPLadder(q31_t* startSample, q31_t* endSample, LPFMode lpfMode, int32_t sampleIncrement,
+	                           int32_t extraSaturation, int32_t extraSaturationDrive);
+
+	//all ladders are in this class to share the basic components
+	//this differentiates between them
+	LPFMode lpfMode;
+
+	//state
+	q31_t noiseLastValue;
+	BasicFilterComponent lpfLPF1;
+	BasicFilterComponent lpfLPF2;
+	BasicFilterComponent lpfLPF3;
+	BasicFilterComponent lpfLPF4;
+
+	//configuration
+	q31_t processedResonance;
+	q31_t divideByTotalMoveabilityAndProcessedResonance;
+
+	//moveability is tan(f)/(1+tan(f))
+	q31_t moveability;
+	q31_t divideBy1PlusTannedFrequency;
+
+	// All feedbacks have 1 represented as 1073741824
+	q31_t lpf1Feedback;
+	q31_t lpf2Feedback;
+	q31_t lpf3Feedback;
+
+	bool doOversampling;
+};
