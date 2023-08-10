f5ffa40cfdaffd1e1bea8a7cc51f83222be71955
Paul Freund <freund.paul@lvl3.org>
2023-08-10 19:41:59 +0200

    Display clips in a grid in song (session) view (#251)
    * Added template for description of feature
    
    * Mini cleanup during analysis
    
    * Enum classes and format
    
    * Fixed enum classes not being compatible with the usage pattern
    
    * Added layout selection
    
    * Added a lot of comments regarding sections and basic section rendering in sidebar
    
    * Added grid drawing code (except for animations) and todos
    
    * Quick commit to work on another branch
    
    * In the middle of implementing session view input, still need to add a lot of behavior and fix some functions
    
    * Some grid functions are already working with lots of TODOs to do
    
    * Implemented a lot, fixed a lot, still have a lot to go. Animations, cloning and edge cases are the biggest areas
    
    * Progress with a lot more stuff working, creating MIDI/CV is not one of them.
    
    * Little bit of cleanup
    
    * Fixed all the buttons :)
    
    * Allow copying between all compatible clip types
    
    * Fixed x scrolling and high x clips, fixed allocation of all types. Animations left
    
    * Small cleanup of members
    
    * Figured out what graphicsRoutine does and removed it for grid since we don't show the playhead
    
    * This might fix re-rendering :)
    
    * Finished fixing flashing on arm
    
    * Fixed immediate stop on launching empty sections
    
    * Added section legato mode
    
    * Fixed mute and record actions
    
    * Implemented MIDI Learn commands and flashing
    
    * Moved clip transitioning animations to session
    
    * Explode animation works but there is some weird flashes of wrong buffers and keyboard view animations ends as row going to session
    
    * Moved rendering back from keyboard screen to session screen to session so other layouts can handle it
    
    * Formatting fix for last commit
    
    * Fixed regression extra flash transitioning from clips in rows mode
    
    * Fixed explode animation and creating non cloned new clip
    
    * Fix stuttering not being terminated on pad up
    
    * Fixed move and record to arranger
    
    * Only flash if we are not inside the animation
    
    * dbt format for last commit
    
    * Added feature description
    
    * Still ready to review/merge, just found a small behavior problem making the video. Now only new clips in empty tracks play automatically
    
    * Fixed arrangement record requiring long press times
    
    * Fixed delete only working after 300ms
    
    * Fixed section launch behavior
    
    * Launch section legato on button down instead of up for faster switching
    
    * Fixed shift+horizontal+pad not instant soloing
    
    * Allow clips to be opened by holding pad and clicking clip
    
    * Added a small helper function
    
    * Aligned instrument changing and load button with arranger behavior
    
    * Fixed scrolling through track presets
    
    * Trying to narrow down the copying bug, mini refactor
    
    * [WIP] Working on new clip copying code
    
    * First part of fixing review comments
    
    * Second round of review fixes
    
    * Moved extern declarations to the respective headers
    
    * Seems the crashes I saw hit #320, seems like copy to existing track works now
    
    * Dbt format
    
    * Need to clean that up and test but looks like it solves copying to new instruments
    
    * Refactored new track generation and fixed copying to empty tracks
    
    * Fixed Drums being copied to new track
    
    * Fixed copying MIDI/CV clips to new tracks

--- src/deluge/model/song/song.cpp
+++ src/deluge/model/song/song.cpp
@@ -1053,6 +1053,10 @@ weAreInArrangementEditorOrInClipInstance:
 	storageManager.writeAttribute("xScroll", xScroll[NAVIGATION_CLIP]);
 	storageManager.writeAttribute("xZoom", xZoom[NAVIGATION_CLIP]);
 	storageManager.writeAttribute("yScrollSongView", songViewYScroll);
+	storageManager.writeAttribute("songGridScrollX", songGridScrollX);
+	storageManager.writeAttribute("songGridScrollY", songGridScrollY);
+	storageManager.writeAttribute("sessionLayout", sessionLayout);
+
 	storageManager.writeAttribute("yScrollArrangementView", arrangementYScroll);
 	storageManager.writeAttribute("xScrollArrangementView", xScroll[NAVIGATION_ARRANGEMENT]);
 	storageManager.writeAttribute("xZoomArrangementView", xZoom[NAVIGATION_ARRANGEMENT]);
@@ -1373,8 +1377,22 @@ int32_t Song::readFromFile() {
 
 		default:
 unknownTag:
+			if (!strcmp(tagName, "sessionLayout")) {
+				sessionLayout = (SessionLayoutType)storageManager.readTagOrAttributeValueInt();
+				storageManager.exitTag("sessionLayout");
+			}
 
-			if (!strcmp(tagName, "xZoomArrangementView")) {
+			else if (!strcmp(tagName, "songGridScrollX")) {
+				songGridScrollX = storageManager.readTagOrAttributeValueInt();
+				storageManager.exitTag("songGridScrollX");
+			}
+
+			else if (!strcmp(tagName, "songGridScrollY")) {
+				songGridScrollY = storageManager.readTagOrAttributeValueInt();
+				storageManager.exitTag("songGridScrollY");
+			}
+
+			else if (!strcmp(tagName, "xZoomArrangementView")) {
 				xZoom[NAVIGATION_ARRANGEMENT] = storageManager.readTagOrAttributeValueInt();
 				storageManager.exitTag("xZoomArrangementView");
 			}
@@ -4318,6 +4336,134 @@ bool Song::canOldOutputBeReplaced(Clip* clip, Availability* availabilityRequirem
 		// We still may as well replace the Output so long as it doesn't have any *other* Clips
 		return (getClipWithOutput(clip->output, false, clip) == NULL);
 	}
+}
+
+Output* Song::navigateThroughPresetsForInstrument(Output* output, int32_t offset) {
+	if (output->type == InstrumentType::AUDIO) {
+		return output;
+	}
+
+	actionLogger.deleteAllLogs();
+
+	Instrument* oldInstrument = (Instrument*)output;
+
+	InstrumentType instrumentType = oldInstrument->type;
+
+	currentSong->ensureAllInstrumentsHaveAClipOrBackedUpParamManager("E063", "H063");
+
+	// If we're in MIDI or CV mode, easy - just change the channel
+	if (instrumentType == InstrumentType::MIDI_OUT || instrumentType == InstrumentType::CV) {
+
+		NonAudioInstrument* oldNonAudioInstrument = (NonAudioInstrument*)oldInstrument;
+
+		int32_t oldChannel = oldNonAudioInstrument->channel;
+		int32_t newChannel = oldNonAudioInstrument->channel;
+
+		int32_t oldChannelSuffix, newChannelSuffix;
+		if (instrumentType == InstrumentType::MIDI_OUT) {
+			oldChannelSuffix = ((MIDIInstrument*)oldNonAudioInstrument)->channelSuffix;
+			newChannelSuffix = ((MIDIInstrument*)oldNonAudioInstrument)->channelSuffix;
+		}
+
+		// CV
+		if (instrumentType == InstrumentType::CV) {
+			do {
+				newChannel = (newChannel + offset) & (NUM_CV_CHANNELS - 1);
+
+				if (newChannel == oldChannel) {
+cantDoIt:
+					numericDriver.displayPopup(HAVE_OLED ? "No free channel slots available in song" : "CANT");
+					return output;
+				}
+
+			} while (currentSong->getInstrumentFromPresetSlot(instrumentType, newChannel, -1, NULL, NULL, false));
+		}
+
+		// Or MIDI
+		else {
+
+			oldNonAudioInstrument->channel = -1; // Get it out of the way
+
+			do {
+				newChannelSuffix += offset;
+
+				// Turned left
+				if (offset == -1) {
+					if (newChannelSuffix < -1) {
+						newChannel = (newChannel + offset) & 15;
+						newChannelSuffix = currentSong->getMaxMIDIChannelSuffix(newChannel);
+					}
+				}
+
+				// Turned right
+				else {
+					if (newChannelSuffix >= 26 || newChannelSuffix > currentSong->getMaxMIDIChannelSuffix(newChannel)) {
+						newChannel = (newChannel + offset) & 15;
+						newChannelSuffix = -1;
+					}
+				}
+
+				if (newChannel == oldChannel && newChannelSuffix == oldChannelSuffix) {
+					oldNonAudioInstrument->channel = oldChannel; // Put it back
+					goto cantDoIt;
+				}
+
+			} while (currentSong->getInstrumentFromPresetSlot(instrumentType, newChannel, newChannelSuffix, NULL, NULL,
+			                                                  false));
+
+			oldNonAudioInstrument->channel = oldChannel; // Put it back, before switching notes off etc
+		}
+
+		if (oldNonAudioInstrument->activeClip && (playbackHandler.playbackState & PLAYBACK_CLOCK_EITHER_ACTIVE)) {
+			oldNonAudioInstrument->activeClip->expectNoFurtherTicks(currentSong);
+		}
+
+		// Because these are just MIDI / CV instruments and we're changing them for all Clips, we can just change the existing Instrument object!
+		oldNonAudioInstrument->channel = newChannel;
+		if (instrumentType == InstrumentType::MIDI_OUT) {
+			((MIDIInstrument*)oldNonAudioInstrument)->channelSuffix = newChannelSuffix;
+		}
+
+		view.displayOutputName(oldNonAudioInstrument);
+#if HAVE_OLED
+		OLED::sendMainImage();
+#endif
+	}
+
+	// Or if we're on a Kit or Synth...
+	else {
+		PresetNavigationResult results =
+		    loadInstrumentPresetUI.doPresetNavigation(offset, oldInstrument, Availability::INSTRUMENT_UNUSED, true);
+		if (results.error == NO_ERROR_BUT_GET_OUT) {
+removeWorkingAnimationAndGetOut:
+#if HAVE_OLED
+			OLED::removeWorkingAnimation();
+#endif
+			return output;
+		}
+		else if (results.error) {
+			numericDriver.displayError(results.error);
+			goto removeWorkingAnimationAndGetOut;
+		}
+
+		Instrument* newInstrument = results.fileItem->instrument;
+		Browser::emptyFileItems();
+
+		currentSong->replaceInstrument(oldInstrument, newInstrument);
+
+		oldInstrument = newInstrument;
+#if HAVE_OLED
+		OLED::removeWorkingAnimation();
+#else
+		numericDriver.removeTopLayer();
+#endif
+	}
+
+	currentSong->instrumentSwapped(oldInstrument);
+
+	currentSong->ensureAllInstrumentsHaveAClipOrBackedUpParamManager("E064", "H064");
+
+	return oldInstrument;
 }
 
 void Song::instrumentSwapped(Instrument* newInstrument) {
