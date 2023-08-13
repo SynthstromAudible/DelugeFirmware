9a9d3deec0593b38330a2e79456925110cad7807
bobtwinkles <srkoser+github@gmail.com>
2023-08-13 16:46:00 -0700

    fix: use string_view in more parts of the menu
    Fixes #343 though probably not by actually fixing the underlying bug

--- src/deluge/gui/menu_item/source_selection.cpp
+++ src/deluge/gui/menu_item/source_selection.cpp
@@ -44,7 +44,7 @@ uint8_t SourceSelection::shouldDrawDotOnValue() {
 int32_t SourceSelection::selectedRowOnScreen;
 
 void SourceSelection::drawPixelsForOled() {
-	static_vector<std::string, kOLEDMenuNumOptionsVisible> itemNames{};
+	static_vector<std::string_view, kOLEDMenuNumOptionsVisible> itemNames{};
 
 	selectedRowOnScreen = 0;
 
