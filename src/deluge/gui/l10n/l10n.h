bd1d2d97cf8b36c001fc901897618cabf880b545
Katherine Whitlock <kate@skylinesynths.nyc>
2023-08-14 07:46:55 -0400

    Proper fallbacks

--- src/deluge/gui/l10n/l10n.h
+++ src/deluge/gui/l10n/l10n.h
@@ -6,21 +6,31 @@
 #include <array>
 #include <cstddef>
 #include <initializer_list>
+#include <optional>
 
 namespace deluge::l10n {
-constexpr std::string_view getView(size_t language_idx, l10n::String string) {
-	return languages.at(language_idx)->get(string);
+constexpr std::string_view getView(const Language& language, l10n::String string) {
+	auto string_opt = language.get(string);
+	if (string_opt.has_value()) {
+		return string_opt.value();
+	}
+
+	if (language.hasFallback()) {
+		return getView(language.fallback(), string);
+	}
+
+	return built_in::english.get(String::EMPTY_STRING).value(); // EMPTY_STRING
 }
 
 inline std::string_view getView(l10n::String string) {
-	return chosenLanguage->get(string);
+	return getView(*chosenLanguage, string);
 }
 
-constexpr const char* get(size_t language_idx, l10n::String string) {
-	return languages.at(language_idx)->get(string).data();
+constexpr const char* get(const Language& language, l10n::String string) {
+	return getView(language, string).data();
 }
 
 inline const char* get(l10n::String string) {
-	return chosenLanguage->get(string).data();
+	return getView(string).data();
 }
 } // namespace deluge::l10n
--- src/deluge/gui/l10n/language.h
+++ src/deluge/gui/l10n/language.h
@@ -3,7 +3,9 @@
 #include "util/container/static_vector.hpp"
 #include "util/misc.h"
 #include <array>
+#include <optional>
 #include <string>
+#include <string_view>
 #include <utility>
 
 namespace deluge::l10n {
@@ -14,25 +16,18 @@ extern static_vector<Language const*, kMaxNumLanguages> languages;
 extern Language const* chosenLanguage;
 
 class Language {
+	using entry_type = std::pair<l10n::String, std::string_view>;
 public:
-	using map_type = std::array<std::string_view, kNumStrings>;
+	using map_type = std::array<std::optional<std::string_view>, kNumStrings>;
 
 	Language(std::string name, Language const* fallback = languages[0]) : name_(std::move(name)) {
 		std::copy(fallback->map_.cbegin(), fallback->map_.cend(), this->map_.begin());
 	};
 
 	/** @brief Builder-style constructor for creating localization language maps (compile-time only) */
-	consteval Language(char const* name, std::initializer_list<std::pair<l10n::String, const char*>> stringmaps,
+	consteval Language(char const* name, std::initializer_list<entry_type> stringmaps,
 	                   const Language* fallback = nullptr)
-	    : name_(name) {
-
-		if (fallback != nullptr) { // copy the string pointers from the fallback
-			std::copy(fallback->map_.cbegin(), fallback->map_.cend(), this->map_.begin());
-		}
-		else { // If we have no languages yet...
-			   // default all values to empty string
-			std::fill(map_.begin(), map_.end(), "");
-		}
+	    : name_(name), fallback_(fallback) {
 
 		// Replace with stringmap values for valid entries
 		for (auto [string_id, s] : stringmaps) {
@@ -48,10 +43,14 @@ public:
 		return *this;
 	}
 
-	constexpr std::string_view name() { return name_; }
+	[[nodiscard]] constexpr std::string_view name() const { return name_; }
+
+	[[nodiscard]] constexpr bool hasFallback() const { return fallback_ != nullptr; }
+	[[nodiscard]] constexpr const Language& fallback() const { return *fallback_; }
 
 private:
 	std::string name_;
 	map_type map_{};
+	const Language *fallback_ = nullptr;
 };
 } // namespace deluge::l10n
