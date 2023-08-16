c1cd3899df6db5712098cd5cea99c7fb73dda2bf
bfredl <bjorn.linse@gmail.com>
2023-08-16 17:19:10 +0200

    backport: feature(sysex): more efficient screen updates + switch type at runtime
    Instead of having the client poll the screen with regular
    intervals, this instead has the deluge push out updates
    whenever the screen changes. This should dramatically reduce
    the CPU overhead of this feature when there is no changes to the
    OLED display.
    
    Implement throttling by checking the fullness of the sysex send
    buffer. This naturally reduces the refresh rate as needed when
    using serial MIDI, or some USB device which reading too slow
    
    he client still has to continuously ask the deluge for updates,
    to stop sending after the client disconnects. This can be done
    with a much lower rate, one short message per second. The client
    can also force a full update so a dropped delta message does
    not cause the screen to be corrupt forever.
    
    In addition, based on the recent display unification work,
    it is now possible to switch the screen type at runtime from the client.

--- src/deluge/hid/hid_sysex.h
+++ src/deluge/hid/hid_sysex.h
@@ -6,5 +6,7 @@ void requestOLEDDisplay(MIDIDevice* device, uint8_t* data, int32_t len);
 void request7SegDisplay(MIDIDevice* device, uint8_t* data, int32_t len);
 void sysexReceived(MIDIDevice* device, uint8_t* data, int32_t len);
 void sendOLEDData(MIDIDevice* device, bool rle);
-
+void sendOLEDDataDelta(MIDIDevice* device, bool force);
+void send7SegData(MIDIDevice* device);
+void sendDisplayIfChanged();
 } // namespace HIDSysex
