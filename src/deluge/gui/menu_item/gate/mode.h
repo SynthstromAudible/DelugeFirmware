f84e009c83313c24bbe54f6ebf8ef78b2d96f16d
Katherine Whitlock <kate@skylinesynths.nyc>
2023-08-18 12:09:45 -0400

    Fix mode items and title

--- src/deluge/gui/menu_item/gate/mode.h
+++ src/deluge/gui/menu_item/gate/mode.h
@@ -22,40 +22,52 @@
 #include "gui/ui/sound_editor.h"
 #include "processing/engines/cv_engine.h"
 #include "util/misc.h"
+#include <string_view>
 
 namespace deluge::gui::menu_item::gate {
 
 class Mode final : public Selection<3>, public FormattedTitle {
 
-	static_vector<std::string_view, capacity()> options_ = {
-	    l10n::getView(l10n::String::STRING_FOR_V_TRIGGER),
-	    l10n::getView(l10n::String::STRING_FOR_S_TRIGGER),
+	static_vector<l10n::String, capacity()> options_ = {
+	    l10n::String::STRING_FOR_V_TRIGGER,
+	    l10n::String::STRING_FOR_S_TRIGGER,
 	};
 
 public:
 	Mode() : Selection(), FormattedTitle(l10n::String::STRING_FOR_GATE_MODE_TITLE) {}
+	[[nodiscard]] std::string_view getTitle() const override {
+		return FormattedTitle::title();
+	}
+
 	void readCurrentValue() override { this->setValue(cvEngine.gateChannels[soundEditor.currentSourceIndex].mode); }
 	void writeCurrentValue() override {
 		cvEngine.setGateType(soundEditor.currentSourceIndex, this->getValue<GateType>());
 	}
-	static_vector<std::string_view, capacity()> getOptions() override { return options_; }
+	static_vector<std::string_view, capacity()> getOptions() override {
+		static_vector<std::string_view, capacity()> output;
+		for (l10n::String str : options_) {
+			output.push_back(l10n::getView(str));
+		}
+		return output;
+	}
 
 	void updateOptions(int32_t value) {
 		using enum l10n::String;
+		// Remove the extra entry if it's present
+		if (options_.size() > 2) {
+			options_.pop_back();
+		}
+
 		switch (value) {
 		case WHICH_GATE_OUTPUT_IS_CLOCK:
-			options_[2] = l10n::getView(STRING_FOR_CLOCK);
+			options_.push_back(STRING_FOR_CLOCK);
 			break;
 
 		case WHICH_GATE_OUTPUT_IS_RUN:
-			options_[2] = l10n::getView(STRING_FOR_RUN_SIGNAL);
+			options_.push_back(STRING_FOR_RUN_SIGNAL);
 			break;
 
 		default:
-			// Remove the extra entry if it's present
-			if (options_.size() > 2) {
-				options_.pop_back();
-			}
 			break;
 		}
 	}
