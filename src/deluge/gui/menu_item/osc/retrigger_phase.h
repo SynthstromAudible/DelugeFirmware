1fac11b78c4121628e0979b1cd1abe37faa54db4
Katherine Whitlock <kate@skylinesynths.nyc>
2023-08-23 12:44:02 -0400

    Fix some appearances of "DISABLED" in menus where it should be "OFF"

--- src/deluge/gui/menu_item/osc/retrigger_phase.h
+++ src/deluge/gui/menu_item/osc/retrigger_phase.h
@@ -66,7 +66,7 @@ public:
 
 	void drawPixelsForOled() override {
 		if (this->getValue() < 0) {
-			deluge::hid::display::OLED::drawStringCentred(l10n::get(l10n::String::STRING_FOR_DISABLED), 20,
+			deluge::hid::display::OLED::drawStringCentred(l10n::get(l10n::String::STRING_FOR_OFF), 20,
 			                                              deluge::hid::display::OLED::oledMainImage[0],
 			                                              OLED_MAIN_WIDTH_PIXELS, kTextHugeSpacingX, kTextHugeSizeY);
 		}
