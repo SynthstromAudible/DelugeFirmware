44f9771299432d48b937ab26d8e60df22dc0d286
Paul Freund <freund.paul@lvl3.org>
2023-08-24 16:18:57 +0200

    Default enable highlighting incoming notes

--- src/deluge/model/settings/runtime_feature_settings.cpp
+++ src/deluge/model/settings/runtime_feature_settings.cpp
@@ -123,7 +123,7 @@ void RuntimeFeatureSettings::init() {
 	                              "syncScalingAction", RuntimeFeatureStateSyncScalingAction::SyncScaling);
 	// HighlightIncomingNotes
 	SetupOnOffSetting(settings[RuntimeFeatureSettingType::HighlightIncomingNotes], "Highlight incoming notes",
-	                  "highlightIncomingNotes", RuntimeFeatureStateToggle::Off);
+	                  "highlightIncomingNotes", RuntimeFeatureStateToggle::On);
 	// DisplayNornsLayout
 	SetupOnOffSetting(settings[RuntimeFeatureSettingType::DisplayNornsLayout], "Display Norns layout",
 	                  "displayNornsLayout", RuntimeFeatureStateToggle::Off);
