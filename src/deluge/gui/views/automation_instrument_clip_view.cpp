6c29fb40e102b801c6b544d8b98e9c850d646f81
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2023-08-27 17:18:53 -0400

    Automation view: handle very rapid region entry
    Refactored code for a single press to its own separate function
    
    Recorded a second pad press for the second pad press in a long press which should resolve issues with deselection
    
    Also allows for further control of long presses by giving the user freedom to let go of the first pad and continue holding the second pad and still be able to edit the long press.

--- src/deluge/gui/views/automation_instrument_clip_view.cpp
+++ src/deluge/gui/views/automation_instrument_clip_view.cpp
@@ -1358,8 +1358,7 @@ void AutomationInstrumentClipView::editPadAction(bool state, uint8_t yDisplay, u
 		}
 		// If this is a automation-length-edit press...
 		//needed for Automation
-		if (clip->lastSelectedParamID != kNoLastSelectedParamID && instrumentClipView.numEditPadPresses == 1
-		    && ((int32_t)(instrumentClipView.timeLastEditPadPress + 80 * 44 - AudioEngine::audioSampleTimer) < 0)) {
+		if (clip->lastSelectedParamID != kNoLastSelectedParamID && instrumentClipView.numEditPadPresses == 1) {
 
 			int32_t firstPadX = 255;
 			int32_t firstPadY = 255;
@@ -1377,6 +1376,8 @@ void AutomationInstrumentClipView::editPadAction(bool state, uint8_t yDisplay, u
 			}
 
 			if (firstPadX != 255 && firstPadY != 255 && firstPadX != xDisplay) {
+				recordSinglePadPress(xDisplay, yDisplay);
+
 				multiPadPressSelected = true;
 				multiPadPressActive = true;
 
@@ -1420,33 +1421,9 @@ void AutomationInstrumentClipView::editPadAction(bool state, uint8_t yDisplay, u
 
 		// Or, if this is a regular create-or-select press...
 		else {
-			instrumentClipView.timeLastEditPadPress = AudioEngine::audioSampleTimer;
-			// Find an empty space in the press buffer, if there is one
-			int32_t i;
-			for (i = 0; i < kEditPadPressBufferSize; i++) {
-				if (!instrumentClipView.editPadPresses[i].isActive) {
-					break;
-				}
-			}
-			if (i < kEditPadPressBufferSize) {
+			if (recordSinglePadPress(xDisplay, yDisplay)) {
 				multiPadPressActive = false;
-
 				handleSinglePadPress(modelStack, clip, xDisplay, yDisplay);
-
-				instrumentClipView.shouldIgnoreVerticalScrollKnobActionIfNotAlsoPressedForThisNotePress = false;
-
-				// If this is the first press, record the time
-				if (instrumentClipView.numEditPadPresses == 0) {
-					instrumentClipView.timeFirstEditPadPress = AudioEngine::audioSampleTimer;
-					instrumentClipView.shouldIgnoreHorizontalScrollKnobActionIfNotAlsoPressedForThisNotePress = false;
-				}
-
-				instrumentClipView.editPadPresses[i].isActive = true;
-				instrumentClipView.editPadPresses[i].yDisplay = yDisplay;
-				instrumentClipView.editPadPresses[i].xDisplay = xDisplay;
-				instrumentClipView.numEditPadPresses++;
-				instrumentClipView.numEditPadPressesPerNoteRowOnScreen[yDisplay]++;
-				enterUIMode(UI_MODE_NOTES_PRESSED);
 			}
 		}
 	}
@@ -1478,7 +1455,7 @@ void AutomationInstrumentClipView::editPadAction(bool state, uint8_t yDisplay, u
 		}
 		//switch from long press selection to short press selection in pad selection mode
 		else if ((clip->lastSelectedParamID != kNoLastSelectedParamID) && padSelectionOn && multiPadPressSelected
-		         && !multiPadPressActive
+		         && !multiPadPressActive && (currentUIMode != UI_MODE_NOTES_PRESSED)
 		         && ((AudioEngine::audioSampleTimer - instrumentClipView.timeLastEditPadPress) < kShortPressTime)) {
 
 			multiPadPressSelected = false;
@@ -1489,6 +1466,37 @@ void AutomationInstrumentClipView::editPadAction(bool state, uint8_t yDisplay, u
 			uiNeedsRendering(this);
 		}
 	}
+}
+
+bool AutomationInstrumentClipView::recordSinglePadPress(int32_t xDisplay, int32_t yDisplay) {
+
+	instrumentClipView.timeLastEditPadPress = AudioEngine::audioSampleTimer;
+	// Find an empty space in the press buffer, if there is one
+	int32_t i;
+	for (i = 0; i < kEditPadPressBufferSize; i++) {
+		if (!instrumentClipView.editPadPresses[i].isActive) {
+			break;
+		}
+	}
+	if (i < kEditPadPressBufferSize) {
+		instrumentClipView.shouldIgnoreVerticalScrollKnobActionIfNotAlsoPressedForThisNotePress = false;
+
+		// If this is the first press, record the time
+		if (instrumentClipView.numEditPadPresses == 0) {
+			instrumentClipView.timeFirstEditPadPress = AudioEngine::audioSampleTimer;
+			instrumentClipView.shouldIgnoreHorizontalScrollKnobActionIfNotAlsoPressedForThisNotePress = false;
+		}
+
+		instrumentClipView.editPadPresses[i].isActive = true;
+		instrumentClipView.editPadPresses[i].yDisplay = yDisplay;
+		instrumentClipView.editPadPresses[i].xDisplay = xDisplay;
+		instrumentClipView.numEditPadPresses++;
+		instrumentClipView.numEditPadPressesPerNoteRowOnScreen[yDisplay]++;
+		enterUIMode(UI_MODE_NOTES_PRESSED);
+
+		return true;
+	}
+	return false;
 }
 
 //audition pad action
