d6faab91e5a7e06ef75d3095a3d8b9a7ca488b62
Paul Freund <freund.paul@lvl3.org>
2023-09-05 15:46:25 +0200

    Finished defaults menus

--- src/deluge/gui/ui/menus.cpp
+++ src/deluge/gui/ui/menus.cpp
@@ -30,6 +30,8 @@
 #include "gui/menu_item/defaults/magnitude.h"
 #include "gui/menu_item/defaults/scale.h"
 #include "gui/menu_item/defaults/velocity.h"
+#include "gui/menu_item/defaults/session_layout.h"
+#include "gui/menu_item/defaults/keyboard_layout.h"
 #include "gui/menu_item/delay/analog.h"
 #include "gui/menu_item/delay/ping_pong.h"
 #include "gui/menu_item/delay/sync.h"
@@ -809,6 +811,31 @@ Submenu triggerClockMenu{
 };
 
 // Defaults menu
+defaults::KeyboardLayout defaultKeyboardLayoutMenu{STRING_FOR_DEFAULT_UI_LAYOUT, STRING_FOR_DEFAULT_UI_LAYOUT};
+
+Submenu defaultUIKeyboard{
+    STRING_FOR_DEFAULT_UI_KEYBOARD,
+    {
+		&defaultKeyboardLayoutMenu
+    },
+};
+
+defaults::SessionLayout defaultSessionLayoutMenu{STRING_FOR_DEFAULT_UI_LAYOUT, STRING_FOR_DEFAULT_UI_LAYOUT};
+Submenu defaultUISession{
+    STRING_FOR_DEFAULT_UI_SONG,
+    {
+		&defaultSessionLayoutMenu
+    },
+};
+
+Submenu defaultUI{
+    STRING_FOR_DEFAULT_UI,
+    {
+        &defaultUISession,
+		&defaultUIKeyboard
+    },
+};
+
 IntegerRange defaultTempoMenu{STRING_FOR_TEMPO, STRING_FOR_DEFAULT_TEMPO, 60, 240};
 IntegerRange defaultSwingMenu{STRING_FOR_SWING, STRING_FOR_DEFAULT_SWING, 1, 99};
 KeyRange defaultKeyMenu{STRING_FOR_KEY, STRING_FOR_DEFAULT_KEY};
@@ -820,7 +847,8 @@ defaults::BendRange defaultBendRangeMenu{STRING_FOR_BEND_RANGE, STRING_FOR_DEFAU
 Submenu defaultsSubmenu{
     STRING_FOR_DEFAULTS,
     {
-        &defaultTempoMenu,
+        &defaultUI,
+		&defaultTempoMenu,
         &defaultSwingMenu,
         &defaultKeyMenu,
         &defaultScaleMenu,
