f850b0ba066299c2c94213c497f7526e978384a2
Paul Freund <freund.paul@lvl3.org>
2023-09-07 21:46:29 +0200

    Fixed switching from arrangement playing to session playing

--- src/deluge/gui/views/session_view.cpp
+++ src/deluge/gui/views/session_view.cpp
@@ -3557,8 +3557,14 @@ ActionResult SessionView::gridHandlePadsLaunch(int32_t x, int32_t y, int32_t on,
 		return ActionResult::DEALT_WITH;
 	}
 
+	if (playbackHandler.playbackState && currentPlaybackMode == &arrangement) {
+		if (currentUIMode == UI_MODE_NONE) {
+			playbackHandler.switchToSession();
+		}
+	}
+
 	// Normal arming, handle cases normally in View::clipStatusPadAction
-	if (!Buttons::isButtonPressed(deluge::hid::button::SHIFT)) {
+	else if (!Buttons::isButtonPressed(deluge::hid::button::SHIFT)) {
 		if (currentUIMode == UI_MODE_VIEWING_RECORD_ARMING) {
 			// Here I removed the overdubbing settings
 			clip->armedForRecording = !clip->armedForRecording;
