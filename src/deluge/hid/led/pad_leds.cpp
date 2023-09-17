26ac7de424816db3bc9b21620b623565fccbc979
Katherine Whitlock <kate@skylinesynths.nyc>
2023-09-17 00:52:15 -0400

    Fix vertical scroll direction issue #503 (#551)

--- src/deluge/hid/led/pad_leds.cpp
+++ src/deluge/hid/led/pad_leds.cpp
@@ -1328,7 +1328,7 @@ void vertical::renderScroll() {
 	for (int32_t x = 0; x < kDisplayWidth + kSideBarWidth; x++) {
 		colours[x] = prepareColour(x, endSquare, Colour::fromArray(image[endSquare][x]));
 	}
-	PIC::doVerticalScroll(scrollDirection <= 0, colours);
+	PIC::doVerticalScroll(scrollDirection > 0, colours);
 	PIC::flush();
 }
 
