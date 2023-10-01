6227a7035339c73178b261e775cde139989ef60b
m-m-adams <mark.adams@queensu.ca>
2023-10-01 00:00:21 -0400

    debug mode for test bin

--- tests/RunAllTests.cpp
+++ tests/RunAllTests.cpp
@@ -1,5 +1,6 @@
 #include "CppUTest/CommandLineTestRunner.h"
-
+#include <iostream>
 int main(int argc, char** argv) {
+	std::cout << "hello";
 	return CommandLineTestRunner::RunAllTests(argc, argv);
 }
--- tests/memory_tests.cpp
+++ tests/memory_tests.cpp
@@ -1,18 +1,6 @@
 #include "CppUTest/TestHarness.h"
-#include "CppUTestExt/MockSupport.h"
-#include "memory/general_memory_allocator.h"
-#include "memory/memory_region.h"
 
-class MemoryRegionMock : public MemoryRegion {
-public:
-	virtual void dbgprint(const char* string) { printf(string); }
-	virtual void freeze(const char* string) { printf(string); }
-};
-
-TEST_GROUP(MemoryAllocation){void setup(){MemoryRegionMock memreg;
-}
-}
-;
+TEST_GROUP(MemoryAllocation){};
 
 TEST(MemoryAllocation, FirstTest) {
 	FAIL("Fail me!");
