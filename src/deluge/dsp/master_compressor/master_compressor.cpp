ed2fd93385e6e39441f40cb7b422d0c05e157a98
m-m-adams <mark.adams@queensu.ca>
2023-10-20 20:13:47 -0400

    fix gain scaling properly

--- src/deluge/dsp/master_compressor/master_compressor.cpp
+++ src/deluge/dsp/master_compressor/master_compressor.cpp
@@ -62,7 +62,7 @@ void MasterCompressor::render(StereoSample* buffer, uint16_t numSamples) {
 	float reduction = 21 * (out / ONE_Q31f);
 	//this lowers the volume so we'll increase the levels afterwards
 
-	finalVolume = exp(std::max<float>(er + reduction, 1)) * float(1 << 29);
+	finalVolume = exp(std::min<float>(er + reduction, 1)) * float(1 << 29);
 
 	amplitudeIncrement = (int32_t)(finalVolume - currentVolume) / numSamples;
 
