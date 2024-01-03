3dea7770bfeaa649f1a3e2d4ec44381905a55931
m-m-adams <mark.adams@queensu.ca>
2024-01-02 22:22:00 -0500

    implement sbrk and provide explicit stubs for other syscalls

---
+++ src/sys_stubs.c
@@ -0,0 +1,64 @@
+#include <stdint.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+//doing the minimal amount possible to not break
+
+void* delugeAlloc(unsigned int requiredSize, bool mayUseOnChipRam);
+
+void* _sbrk(int incr) {
+	static unsigned char* heap = NULL;
+	static unsigned char* end_heap = NULL;
+	unsigned char* prev_heap;
+
+	if (heap == NULL) {
+		heap = delugeAlloc(10000, false);
+		end_heap = heap + 10000;
+	}
+	prev_heap = heap;
+
+	heap += incr;
+	if (heap < end_heap) {
+		return prev_heap;
+	} else {
+		heap = prev_heap;
+		return 0;
+	}
+
+}
+
+int _close(int file) {
+	return -1;
+}
+
+int _fstat(int file, struct stat* st) {
+	st->st_mode = S_IFCHR;
+	return 0;
+}
+
+int _isatty(int file) {
+	return 1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+	return 0;
+}
+
+void _exit(int status) {
+	__asm("BKPT #0");
+}
+
+void _kill(int pid, int sig) {
+	return;
+}
+
+int _getpid(void) {
+	return -1;
+}
+
+int _write(int file, char* ptr, int len) {
+	return 0;
+}
+
+int _read(int file, char* ptr, int len) {
+	return 0;
+}
