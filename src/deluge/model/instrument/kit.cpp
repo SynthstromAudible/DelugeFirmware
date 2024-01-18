b303e1c42fa7a420038751151c78736b7b21853c
bobtwinkles <srkoser+github@gmail.com>
2024-01-18 01:23:11 -0800

    fixup: use correct constant for max patched params in sound context

--- src/deluge/model/instrument/kit.cpp
+++ src/deluge/model/instrument/kit.cpp
@@ -584,7 +584,7 @@ yesTickParamManager:
 
 				// No time to call the proper function and do error checking, sorry.
 				ParamCollectionSummary* unpatchedParamsSummary = &thisNoteRow->paramManager.summaries[0];
-				if constexpr (params::kMaxNumUnpatchedParams > 32) {
+				if constexpr (params::UNPATCHED_SOUND_MAX_NUM > 32) {
 					if (unpatchedParamsSummary->whichParamsAreInterpolating[0]
 					    || unpatchedParamsSummary->whichParamsAreInterpolating[1]) {
 						goto yesTickParamManager;
