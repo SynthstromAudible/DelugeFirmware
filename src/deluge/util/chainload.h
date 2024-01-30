b819a74bf34920edc970d34703493367181c7c95
Reed Koser <srkoser+GitHub@gmail.com>
2024-01-29 21:35:08 -0800

    Fix sysex loader v2 (final) (Really Final) (FINAL FINAL) (oops deleted old branch) (#1126)
    * ramloader: fix sysez flash
    
    Fixes sysex flashing by disabling the MMU, and using a ASM chainload
    implementation instead of sketchy byte copies.
    
    * address feedback

--- src/deluge/util/chainload.h
+++ src/deluge/util/chainload.h
@@ -5,4 +5,12 @@
 #define OFF_USER_CODE_EXECUTE 0x28
 #define OFF_USER_SIGNATURE 0x2c
 
+/// Chainload from a buffer.
+///
+/// The buffer is expected to be in the same format as a Deluge firmware .bin. Namely:
+///   - The code start address is located at buffer + OFF_USER_CODE_START
+///   - The code end address (inclusive) is at buffer + OFF_USER_CODE_END
+///   - The entrypoint address is at a buffer + OFF_USER_CODE_EXECUTE
+///
+/// Currently only implemented for ARM.
 void chainload_from_buf(uint8_t* buffer, int buf_size);
