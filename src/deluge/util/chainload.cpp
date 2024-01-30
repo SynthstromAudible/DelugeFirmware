3019da385cddc26de11187b6d223c1bfee8e4961
Reed Koser <srkoser+GitHub@gmail.com>
2024-01-29 21:35:08 -0800

    Fix sysex loader v2 (final) (Really Final) (FINAL FINAL) (oops deleted old branch) (#1126)
    * ramloader: fix sysez flash
    
    Fixes sysex flashing by disabling the MMU, and using a ASM chainload
    implementation instead of sketchy byte copies.
    
    * address feedback

--- src/deluge/util/chainload.cpp
+++ src/deluge/util/chainload.cpp
@@ -3,36 +3,27 @@
  *
  * This file is part of The Synthstrom Audible Deluge Firmware.
  *
- * The Synthstrom Audible Deluge Firmware is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * The Synthstrom Audible Deluge Firmware is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License along with this program.
- * If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "chainload.h"
+#include "RZA1/intc/devdrv_intc.h"
 #include "RZA1/mtu/mtu.h"
 #include "definitions_cxx.hpp"
+#include <cstddef>
 
-static void chainloader(uint32_t code_start, uint32_t code_size, uint32_t code_exec, char* buf) {
-	int32_t loop_num = (((uint32_t)code_size + 3) / (sizeof(uint32_t)));
-
-	uint32_t* psrc = (uint32_t*)buf;
-	uint32_t* pdst = (uint32_t*)code_start;
-	uint32_t* pdst2 = (uint32_t*)(code_start + UNCACHED_MIRROR_OFFSET);
-
-	for (int32_t i = 0; i < loop_num; i++) {
-		pdst[i] = psrc[i];
-		pdst2[i] = psrc[i];
-	}
-
-	((void (*)(void))code_exec)(); // lessgo
-}
+extern uint32_t spareRenderingBuffer[][SSI_TX_BUFFER_NUM_SAMPLES];
 
 void chainload_from_buf(uint8_t* buffer, int buf_size) {
 	uint32_t user_code_start = *(uint32_t*)(buffer + OFF_USER_CODE_START);
@@ -44,17 +35,27 @@ void chainload_from_buf(uint8_t* buffer, int buf_size) {
 		return;
 	}
 
-	char* funcbuf = (char*)user_code_end; // this should be a nice spot
-	char* from = (char*)chainloader;
-	for (int i = 0; i < 128; i++) {
-		(funcbuf + UNCACHED_MIRROR_OFFSET)[i] = from[i];
-	}
-
+	// Disable interrupts so we don't get interrupted during the chainload
+#if defined(__arm__)
+	asm volatile("CPSID   i");
+#endif
+	// Disable timers
 	disableTimer(TIMER_MIDI_GATE_OUTPUT);
 	disableTimer(TIMER_SYSTEM_SLOW);
 	disableTimer(TIMER_SYSTEM_FAST);
 	disableTimer(TIMER_SYSTEM_SUPERFAST);
+	uint8_t* funcbuf = reinterpret_cast<uint8_t*>(spareRenderingBuffer);
 
-	auto ptr = (void (*)(uint32_t, uint32_t, uint32_t, char*))funcbuf;
-	ptr(user_code_start, code_size, user_code_exec, (char*)buffer);
+#if defined(__arm__)
+	// Jump to the chainloader
+	asm volatile("mov r0,%0\n\t"
+	             "mov r1,%1\n\t"
+	             "mov r2,%2\n\t"
+	             "mov r3,%3\n\t"
+	             "mov r4,%4\n\t"
+	             "blx deluge_chainload"
+	             :
+	             : "r"(user_code_start), "r"(code_size), "r"(user_code_exec), "r"(buffer), "r"(funcbuf)
+	             : "r0", "r1", "r2", "r3", "r4");
+#endif
 }
