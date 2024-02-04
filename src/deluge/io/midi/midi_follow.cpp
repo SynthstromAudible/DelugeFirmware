8d00dac6e3cd1c2000cc284810e6fabd75f43747
m-m-adams <mark.adams@queensu.ca>
2024-02-03 23:04:47 -0500

    update compressor shape param to sidechain shape

--- src/deluge/io/midi/midi_follow.cpp
+++ src/deluge/io/midi/midi_follow.cpp
@@ -191,7 +191,7 @@ MidiFollow::getModelStackWithParamForSong(ModelStackWithThreeMainThings* modelSt
 
 	if (paramID != kNoParamID) {
 		// can't control Pitch or Sidechain params in Song view
-		if ((paramID != params::UNPATCHED_PITCH_ADJUST) && (paramID != params::UNPATCHED_COMPRESSOR_SHAPE)
+		if ((paramID != params::UNPATCHED_PITCH_ADJUST) && (paramID != params::UNPATCHED_SIDECHAIN_SHAPE)
 		    && (paramID != params::UNPATCHED_SIDECHAIN_VOLUME)) {
 			modelStackWithParam = currentSong->getModelStackWithParam(modelStackWithThreeMainThings, paramID);
 		}
