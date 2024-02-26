5765be776aaa48b2899efaa8cbe151fb5c6c9fdd
pmatila <152684196+pmatila@users.noreply.github.com>
2024-02-26 22:25:40 +0200

    Make number of count-in bars configurable (#1305)
    Repurpose the count-in on/off toggle into number of count-in bars,
    allow up to four bars. This should be entirely backwards compatible
    as older firmware only checks for a non-zero value for enabled/disabled
    so if going backwards in firmware it counts as enabled and on update
    a previous enabled state gives one bar count-in as before.

--- src/deluge/gui/ui/menus.cpp
+++ src/deluge/gui/ui/menus.cpp
@@ -100,6 +100,7 @@
 #include "gui/menu_item/patched_param/integer_non_fm.h"
 #include "gui/menu_item/patched_param/pan.h"
 #include "gui/menu_item/performance_session_view/editing_mode.h"
+#include "gui/menu_item/record/countin.h"
 #include "gui/menu_item/record/quantize.h"
 #include "gui/menu_item/reverb/damping.h"
 #include "gui/menu_item/reverb/model.h"
@@ -818,7 +819,7 @@ Submenu padsSubmenu{
 // Record submenu
 record::Quantize recordQuantizeMenu{STRING_FOR_QUANTIZATION};
 ToggleBool recordMarginsMenu{STRING_FOR_LOOP_MARGINS, STRING_FOR_LOOP_MARGINS, FlashStorage::audioClipRecordMargins};
-ToggleBool recordCountInMenu{STRING_FOR_COUNT_IN, STRING_FOR_REC_COUNT_IN, playbackHandler.countInEnabled};
+record::CountIn recordCountInMenu{STRING_FOR_COUNT_IN, STRING_FOR_REC_COUNT_IN};
 monitor::Mode monitorModeMenu{STRING_FOR_SAMPLING_MONITORING, STRING_FOR_MONITORING};
 
 Submenu recordSubmenu{
