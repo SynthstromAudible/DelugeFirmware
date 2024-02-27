dda4d082f77cde976f03810fa26502ddd13a19b5
Katherine Whitlock <kate@skylinesynths.nyc>
2024-02-26 20:05:27 -0500

    Add new test case

--- tests/spec/semver_spec.cpp
+++ tests/spec/semver_spec.cpp
@@ -13,7 +13,7 @@ describe semver("SemVer", $ {
 		expect(SemVer::parse("7.2.6-a92c49").value()).to_equal(SemVer{7, 2, 6, "a92c49"});
 	});
 
-	context("An alpha pre-release version", _{
+	context("with an alpha pre-release version number", _{
 		auto pre_release = SemVer{1,5,2, "alpha"};
 		it("is less than its full release", _ {
 			expect(pre_release).to_be_less_than(SemVer{1,5,2});
@@ -21,6 +21,10 @@ describe semver("SemVer", $ {
 
 		it("is less than its beta release", _ {
 			expect(pre_release).to_be_less_than(SemVer{1,5,2, "beta"});
+		});
+
+		it ("is greater than the previous core version", _ {
+			expect(pre_release).to_be_greater_than(SemVer{1,5,1});
 		});
 	});
 });
