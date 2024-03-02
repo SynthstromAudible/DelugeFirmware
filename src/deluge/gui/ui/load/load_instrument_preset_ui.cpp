12f6dca637310a48ec33668a6094296113410c60
m-m-adams <mark.adams@queensu.ca>
2024-03-02 17:20:39 -0500

    check for drum before setting path

--- src/deluge/gui/ui/load/load_instrument_preset_ui.cpp
+++ src/deluge/gui/ui/load/load_instrument_preset_ui.cpp
@@ -189,7 +189,7 @@ Error LoadInstrumentPresetUI::setupForOutputType() {
 
 	// Or if the Instruments are different types...
 	else {
-		if (loadingSynthToKitRow) {
+		if (loadingSynthToKitRow && soundDrumToReplace) {
 
 			if (&soundDrumToReplace->name) {
 				String* name = &soundDrumToReplace->name;
