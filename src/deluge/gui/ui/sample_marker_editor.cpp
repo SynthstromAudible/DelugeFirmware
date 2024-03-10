f0fe228ec553f173ff82b3d2a5ff3cd666ac410b
Sapphire Koser <srkoser+GitHub@gmail.com>
2024-03-10 12:52:49 -0700

    loop lock: prevent start point disable during locked loop movement (#1461)
    Fixes #1042

--- src/deluge/gui/ui/sample_marker_editor.cpp
+++ src/deluge/gui/ui/sample_marker_editor.cpp
@@ -157,7 +157,8 @@ void SampleMarkerEditor::writeValue(uint32_t value, MarkerType markerTypeNow) {
 	else if (markerTypeNow == MarkerType::LOOP_END) {
 		if (loopLocked) {
 			int32_t intendedLoopStartPos = static_cast<int32_t>(value) - loopLength;
-			if (intendedLoopStartPos >= 0
+			// pos == 0 would disable the loop, so the smallest legal start position is sample 1
+			if (intendedLoopStartPos >= 1
 			    && static_cast<uint64_t>(intendedLoopStartPos) >= getCurrentSampleHolder()->startPos) {
 				getCurrentMultisampleRange()->sampleHolder.loopEndPos = value;
 				getCurrentMultisampleRange()->sampleHolder.loopStartPos = intendedLoopStartPos;
