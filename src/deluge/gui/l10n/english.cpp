9176b480aa71a439a17149dddcd6f79682b0c7c2
bfredl <bjorn.linse@gmail.com>
2024-03-12 14:17:10 +0100

    fix: 7SEG emulator doesn't display community features correctly
    deluge::l10n::getView() gets called and cached too early, assuming
    language cannot change at runtime.

--- src/deluge/gui/l10n/english.cpp
+++ src/deluge/gui/l10n/english.cpp
@@ -538,6 +538,9 @@ PLACE_SDRAM_DATA Language english{
         {STRING_FOR_COMMUNITY_FEATURE_HIGHLIGHT_INCOMING_NOTES, "Highlight Incoming Notes"},
         {STRING_FOR_COMMUNITY_FEATURE_NORNS_LAYOUT, "Display Norns Layout"},
         {STRING_FOR_COMMUNITY_FEATURE_GRAIN_FX, "Enable Grain FX"},
+        {STRING_FOR_COMMUNITY_FEATURE_STICKY_SHIFT, "Sticky Shift"},
+        {STRING_FOR_COMMUNITY_FEATURE_LIGHT_SHIFT, "Light Shift"},
+        {STRING_FOR_COMMUNITY_FEATURE_EMULATED_DISPLAY, "Emulated Display"},
 
         {STRING_FOR_TRACK_STILL_HAS_CLIPS_IN_SESSION, "Track still has clips in session"},
         {STRING_FOR_DELETE_ALL_TRACKS_CLIPS_FIRST, "Delete all track's clips first"},
