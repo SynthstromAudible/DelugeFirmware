e27fbdf0f05781f3572f47d6ba35f3d3040779c4
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2024-03-20 19:23:20 -0400

    Adjust display of negative values (#1530)
    Adjusted display of negative values in mod matrix menu so that 1 decimal place is not cut off.

--- src/deluge/gui/menu_item/patch_cables.cpp
+++ src/deluge/gui/menu_item/patch_cables.cpp
@@ -80,7 +80,10 @@ void PatchCables::renderOptions() {
 		int32_t param_value = cable->param.getCurrentValue();
 		int32_t level = ((int64_t)param_value * kMaxMenuPatchCableValue + (1 << 29)) >> 30;
 
-		floatToString((float)level / 100, buf + off + 5, 2, 2);
+		float floatLevel = (float)level / 100;
+		int floatoff = floatLevel < 0 ? 1 : 0;
+
+		floatToString(floatLevel, buf + off + 5 - floatoff, 2, 2);
 		// fmt::vformat_to_n(buf + off + 5, 5, "{:4}", fmt::make_format_args();
 
 		buf[off + 9] = ' ';
