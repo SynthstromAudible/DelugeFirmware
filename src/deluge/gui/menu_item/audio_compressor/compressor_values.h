dc96b84fba6427543d8f294b5eb04d062146be7d
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2024-03-23 16:19:39 -0400

    UI Consistency - Align compressor display values (#1557)
    * Align comp menu values
    
    Updated values displayed in comp values to align them with the pop-up values displayed in the Song Master Compressor
    
    Still some work to do as values aren't exactly the same
    
    * rename ratio
    
    * simplify display variables
    
    * Align menu values with gold knob values
    
    Updated menu's to use the 0-128 value range so that values displayed are equal to those values displayed with gold knobs in song view
    
    * Display comp param units on OLED
    
    On OLED, all comp params (except threshold) now display the units (e.g. MS, HZ, or : 1)
    
    * Updated strncat char's
    
    ---------
    
    Co-authored-by: m-m-adams <mark.adams@queensu.ca>

--- src/deluge/gui/menu_item/audio_compressor/compressor_values.h
+++ src/deluge/gui/menu_item/audio_compressor/compressor_values.h
@@ -12,71 +12,73 @@ public:
 	using Integer::Integer;
 	void readCurrentValue() override {
 		auto value = (uint64_t)soundEditor.currentModControllable->compressor.getAttack();
-		this->setValue((value * (kMaxMenuValue * 2) + 2147483648) >> 32);
+		this->setValue(value >> 24);
 	}
 	void writeCurrentValue() override {
-		if (this->getValue() == kMaxMenuValue) {
-			soundEditor.currentModControllable->compressor.setAttack(ONE_Q31);
-		}
-		else {
-			q31_t knobPos = (uint32_t)this->getValue() * (2147483648 / kMidMenuValue) >> 1;
+		auto value = this->getValue();
+		if (value < kMaxKnobPos) {
+			q31_t knobPos = lshiftAndSaturate<24>(value);
 			soundEditor.currentModControllable->compressor.setAttack(knobPos);
 		}
 	}
-	[[nodiscard]] int32_t getMaxValue() const override { return kMaxMenuValue; }
+	int32_t getDisplayValue() override { return soundEditor.currentModControllable->compressor.getAttackMS(); }
+	const char* getUnit() override { return " MS"; }
+	[[nodiscard]] int32_t getMaxValue() const override { return kMaxKnobPos; }
 };
 class Release final : public Integer {
 public:
 	using Integer::Integer;
 	void readCurrentValue() override {
 		auto value = (uint64_t)soundEditor.currentModControllable->compressor.getRelease();
-		this->setValue((value * (kMaxMenuValue * 2) + 2147483648) >> 32);
+		this->setValue(value >> 24);
 	}
 	void writeCurrentValue() override {
-		if (this->getValue() == kMaxMenuValue) {
-			soundEditor.currentModControllable->compressor.setRelease(ONE_Q31);
-		}
-		else {
-			q31_t knobPos = (uint32_t)this->getValue() * (2147483648 / kMidMenuValue) >> 1;
+		auto value = this->getValue();
+		if (value < kMaxKnobPos) {
+			q31_t knobPos = lshiftAndSaturate<24>(value);
 			soundEditor.currentModControllable->compressor.setRelease(knobPos);
 		}
 	}
-	[[nodiscard]] int32_t getMaxValue() const override { return kMaxMenuValue; }
+	int32_t getDisplayValue() override { return soundEditor.currentModControllable->compressor.getReleaseMS(); }
+	const char* getUnit() override { return " MS"; }
+	[[nodiscard]] int32_t getMaxValue() const override { return kMaxKnobPos; }
 };
 class Ratio final : public Integer {
 public:
 	using Integer::Integer;
 	void readCurrentValue() override {
 		auto value = (uint64_t)soundEditor.currentModControllable->compressor.getRatio();
-		this->setValue((value * (kMaxMenuValue * 2) + 2147483648) >> 32);
+		this->setValue(value >> 24);
 	}
 	void writeCurrentValue() override {
-		if (this->getValue() == kMaxMenuValue) {
-			soundEditor.currentModControllable->compressor.setRatio(ONE_Q31);
-		}
-		else {
-			q31_t knobPos = (uint32_t)this->getValue() * (2147483648 / kMidMenuValue) >> 1;
+		auto value = this->getValue();
+		if (value < kMaxKnobPos) {
+			q31_t knobPos = lshiftAndSaturate<24>(value);
 			soundEditor.currentModControllable->compressor.setRatio(knobPos);
 		}
 	}
-	[[nodiscard]] int32_t getMaxValue() const override { return kMaxMenuValue; }
+	int32_t getDisplayValue() override { return soundEditor.currentModControllable->compressor.getRatioForDisplay(); }
+	const char* getUnit() override { return " : 1"; }
+	[[nodiscard]] int32_t getMaxValue() const override { return kMaxKnobPos; }
 };
 class SideHPF final : public Integer {
 public:
 	using Integer::Integer;
 	void readCurrentValue() override {
 		auto value = (uint64_t)soundEditor.currentModControllable->compressor.getSidechain();
-		this->setValue((value * (kMaxMenuValue * 2) + 2147483648) >> 32);
+		this->setValue(value >> 24);
 	}
 	void writeCurrentValue() override {
-		if (this->getValue() == kMaxMenuValue) {
-			soundEditor.currentModControllable->compressor.setSidechain(ONE_Q31);
-		}
-		else {
-			q31_t knobPos = (uint32_t)this->getValue() * (2147483648 / kMidMenuValue) >> 1;
+		auto value = this->getValue();
+		if (value < kMaxKnobPos) {
+			q31_t knobPos = lshiftAndSaturate<24>(value);
 			soundEditor.currentModControllable->compressor.setSidechain(knobPos);
 		}
 	}
-	[[nodiscard]] int32_t getMaxValue() const override { return kMaxMenuValue; }
+	int32_t getDisplayValue() override {
+		return soundEditor.currentModControllable->compressor.getSidechainForDisplay();
+	}
+	const char* getUnit() override { return " HZ"; }
+	[[nodiscard]] int32_t getMaxValue() const override { return kMaxKnobPos; }
 };
 } // namespace deluge::gui::menu_item::audio_compressor
