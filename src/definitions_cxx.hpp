886b0ccbd6d0aa0713773b7ffae8348becbd8e02
m-m-adams <mark.adams@queensu.ca>
2024-03-23 14:42:41 -0400

    add to global param array and automation view

--- src/definitions_cxx.hpp
+++ src/definitions_cxx.hpp
@@ -328,7 +328,7 @@ constexpr int32_t kMinMenuMetronomeVolumeValue = 1;
 
 // Automation View constants
 constexpr int32_t kNumNonGlobalParamsForAutomation = 60;
-constexpr int32_t kNumGlobalParamsForAutomation = 23;
+constexpr int32_t kNumGlobalParamsForAutomation = 25;
 constexpr int32_t kNoSelection = 255;
 constexpr int32_t kKnobPosOffset = 64;
 constexpr int32_t kMaxKnobPos = 128;
