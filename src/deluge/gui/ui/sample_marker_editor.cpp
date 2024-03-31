fdbef1daa2a9f8ef82ffba47ec65b721203ec9c7
bobtwinkles <srkoser+github@gmail.com>
2024-03-30 20:00:23 -0700

    remove extra row from lock icon

--- src/deluge/gui/ui/sample_marker_editor.cpp
+++ src/deluge/gui/ui/sample_marker_editor.cpp
@@ -1154,7 +1154,7 @@ void SampleMarkerEditor::renderOLED(uint8_t image[][OLED_MAIN_WIDTH_PIXELS]) {
 	if (isLoopLocked()) {
 		OLED::drawGraphicMultiLine(OLED::lockIcon, OLED_MAIN_WIDTH_PIXELS - 10, OLED_MAIN_TOPMOST_PIXEL + 1, 7,
 		                           OLED::oledMainImage[0]);
-		OLED::invertArea(OLED_MAIN_WIDTH_PIXELS - 10, 7, OLED_MAIN_TOPMOST_PIXEL + 9, OLED_MAIN_TOPMOST_PIXEL + 10,
+		OLED::invertArea(OLED_MAIN_WIDTH_PIXELS - 10, 7, OLED_MAIN_TOPMOST_PIXEL + 9, OLED_MAIN_TOPMOST_PIXEL + 9,
 		                 &OLED::oledMainImage[0]);
 	}
 
