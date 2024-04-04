83eaca0b600ad9c0ae3026e92108c0d0b94fd8c0
m-m-adams <mark.adams@queensu.ca>
2024-04-04 17:15:02 -0400

    use new cases

--- .github/scripts/validate_build_string.js
+++ .github/scripts/validate_build_string.js
@@ -1,4 +1,4 @@
-if (!['debug', 'relwithdebinfo', 'release'].includes(process.env.buildtype)) {
+if (!['Debug', 'Relwithdebinfo', 'Release'].includes(process.env.buildtype)) {
     console.log("build string must be either 'debug', 'relwithdebinfo' or 'release'");
     process.exit(1);
   }
