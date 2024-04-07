78ced00606aa9207facdeb8d201478b6e691d52a
Sapphire Koser <srkoser+GitHub@gmail.com>
2024-04-07 04:58:21 -0700

    Keep git commit up-to-date during build (#1680)
    * delete debug message
    
    * Build-time generation of version string

---
+++ src/deluge/version/version.cpp
@@ -0,0 +1,5 @@
+// This is a stub to pull the generated code from the build directory in to the
+// program, because CMake can't handle targets who have source files that don't
+// exist at configure time.
+
+#include "version.cpp.gen"
