0bb1312faf41e4ab1880dba58d71d2711a0f90c6
Mark Adams <mark.adams@queensu.ca>
2024-05-12 17:09:43 -0400

    Os stuff/interrupt disabling (#1892)
    * new disable and enable interrupt functions
    
    * don't grab usr mode stack since we never enter usr mode
    
    * cleanup other interrupt disabling, force re enable at boot to handle restarts without power cycle
    
    * stubs for 32 bit tests
    
    * improve doc comment

---
+++ tests/32bit_unit_tests/mocks/timers_interrupts/timers_interrupts.h
@@ -0,0 +1,13 @@
+//
+// Created by Mark Adams on 2024-05-11.
+//
+
+#ifndef DELUGE_TIMERS_INTERRUPTS_H
+#define DELUGE_TIMERS_INTERRUPTS_H
+
+#endif // DELUGE_TIMERS_INTERRUPTS_H
+static inline __attribute__((no_instrument_function)) void DISABLE_ALL_INTERRUPTS() {
+}
+
+static inline __attribute__((no_instrument_function)) void ENABLE_INTERRUPTS() {
+}
