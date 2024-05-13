613121560d925fdbc3d6206cf38e3a7609466562
Nikodemus Siivola <nikodemus@random-state.net>
2024-05-13 21:50:34 +0300

    tests: adjust LFO tests

--- tests/unit/lfo_tests.cpp
+++ tests/unit/lfo_tests.cpp
@@ -14,9 +14,9 @@ TEST(LFOTest, renderGlobalTriangle) {
 	LFOType type = LFOType::TRIANGLE;
 	// as per resyncGlobalLFO()
 	lfo.phase = getLFOInitialPhaseForZero(type);
-	CHECK_EQUAL(0, lfo.render(0, type, 0));
-	lfo.tick(10, 100);
-	CHECK_EQUAL(2000, lfo.render(0, type, 0));
+	int32_t numSamples = 10, phaseIncrement = 100;
+	CHECK_EQUAL(0, lfo.render(10, type, 100));
+	CHECK_EQUAL(numSamples*phaseIncrement*2, lfo.render(0, type, 0));
 }
 
 TEST(LFOTest, renderLocalTriangle) {
@@ -24,10 +24,8 @@ TEST(LFOTest, renderLocalTriangle) {
 	LFOType type = LFOType::TRIANGLE;
 	// as per Voice::noteOn()
 	lfo.phase = getLFOInitialPhaseForNegativeExtreme(type);
-	CHECK_EQUAL(-2147483648, lfo.render(0, type, 0));
-
-	lfo.tick(10, 100);
-	CHECK_EQUAL(-2147481648, lfo.render(0, type, 0));
+	CHECK_EQUAL(INT32_MIN, lfo.render(10, type, 100));
+	CHECK_EQUAL(INT32_MIN+2000, lfo.render(0, type, 0));
 }
 
 TEST(LFOTest, renderGlobalSine) {
@@ -35,10 +33,11 @@ TEST(LFOTest, renderGlobalSine) {
 	LFOType type = LFOType::SINE;
 	// as per resyncGlobalLFO()
 	lfo.phase = getLFOInitialPhaseForZero(type);
-	CHECK_EQUAL(0, lfo.render(0, type, 0));
-
-	lfo.tick(10, 100);
-	CHECK_EQUAL(2412, lfo.render(0, type, 0));
+	// sin(0) == 0
+	CHECK_EQUAL(0, lfo.phase);
+	lfo.phase = 1024;
+	// (2^31)*sin(2*pi*1024/(2^32)) = 3216.99
+	CHECK_EQUAL(3216, lfo.render(0, type, 0));
 }
 
 TEST(LFOTest, renderLocalSine) {
@@ -46,9 +45,13 @@ TEST(LFOTest, renderLocalSine) {
 	LFOType type = LFOType::SINE;
 	// as per Voice::noteOn()
 	lfo.phase = getLFOInitialPhaseForNegativeExtreme(type);
-	CHECK_EQUAL(-2147418112, lfo.render(0, type, 0));
-
-	lfo.tick(10, 100);
+	CHECK_EQUAL(3221225472, lfo.phase);
+	// These are nasty numbers, but the first one represents the
+	// initial value for a local sine LFO, and a second one is a
+	// an arbitray step forward.
+	//
+	// (2^31)*sin(2*pi*3221225472/(2^32)) = -2147483648 ... close?
+	CHECK_EQUAL(-2147418112, lfo.render(10, type, 100));
 	CHECK_EQUAL(-2147418082, lfo.render(0, type, 0));
 }
 
@@ -57,10 +60,8 @@ TEST(LFOTest, renderSaw) {
 	LFOType type = LFOType::SAW;
 	// as per resyncGlobalLFO() AND Voice::noteOn()
 	lfo.phase = getLFOInitialPhaseForNegativeExtreme(type);
-	CHECK_EQUAL(-2147483648, lfo.render(0, type, 0));
-
-	lfo.tick(10, 100);
-	CHECK_EQUAL(-2147482648, lfo.render(0, type, 0));
+	CHECK_EQUAL(INT32_MIN, lfo.render(10, type, 100));
+	CHECK_EQUAL(INT32_MIN+1000, lfo.render(0, type, 0));
 }
 
 TEST(LFOTest, renderSquare) {
@@ -68,10 +69,11 @@ TEST(LFOTest, renderSquare) {
 	LFOType type = LFOType::SQUARE;
 	// as per resyncGlobalLFO() AND Voice::noteOn()
 	lfo.phase = getLFOInitialPhaseForNegativeExtreme(type);
-	CHECK_EQUAL(2147483647, lfo.render(0, type, 0));
-
-	lfo.tick(1328979, 193287); // need a big shift to see the jump
-	CHECK_EQUAL(-2147483648, lfo.render(0, type, 0));
+	// ...it's interesting that our initial phase for "negative extreme"
+	// for square LFO actually gets the positive extreme...
+	CHECK_EQUAL(INT32_MAX, lfo.render(0, type, 0));
+	lfo.phase = 0x80000001; // force over phase width
+	CHECK_EQUAL(INT32_MIN, lfo.render(0, type, 0));
 }
 
 TEST(LFOTest, renderSampleAndHold) {
@@ -79,19 +81,20 @@ TEST(LFOTest, renderSampleAndHold) {
 	LFOType type = LFOType::SAMPLE_AND_HOLD;
 	// as per resyncGlobalLFO() AND Voice::noteOn()
 	lfo.phase = getLFOInitialPhaseForNegativeExtreme(type);
+	CHECK_EQUAL(0, lfo.phase);
 	// CHECK_EQUAL evaluates multiple times: get the value just once.
-	int32_t value = lfo.render(0, type, 0);
+	int32_t value = lfo.render(10, type, 10);
 	CHECK_EQUAL(-1392915738, value);
+	CHECK_EQUAL(100, lfo.phase);
 
-	lfo.tick(1328979, 193287); // no change...
-	value = lfo.render(0, type, 0);
+	value = lfo.render(10, type, 10); // no change
 	CHECK_EQUAL(-1392915738, value);
-	CHECK(lfo.phase != 0);
+	CHECK_EQUAL(200, lfo.phase);
 
 	lfo.phase = 0; // force reset
-	value = lfo.render(0, type, 0);
+	value = lfo.render(10, type, 10);
 	CHECK_EQUAL(-28442955, value);
-	CHECK(lfo.phase == 0);
+	CHECK_EQUAL(100, lfo.phase);
 }
 
 TEST(LFOTest, renderRandomWalk) {
@@ -99,17 +102,18 @@ TEST(LFOTest, renderRandomWalk) {
 	LFOType type = LFOType::RANDOM_WALK;
 	// as per resyncGlobalLFO() AND Voice::noteOn()
 	lfo.phase = getLFOInitialPhaseForNegativeExtreme(type);
+	CHECK_EQUAL(0, lfo.phase);
 	// CHECK_EQUAL evaluates multiple times: get the value just once.
-	int32_t value = lfo.render(0, type, 0);
+	int32_t value = lfo.render(10, type, 10);
 	CHECK_EQUAL(-2948644, value);
-	CHECK(lfo.phase == 0);
+	CHECK_EQUAL(100, lfo.phase);
+
+	value = lfo.render(10, type, 10); // no change
+	CHECK_EQUAL(-2948644, value);
+	CHECK_EQUAL(200, lfo.phase);
 
 	lfo.phase = 0; // force reset
-	value = lfo.render(1, type, 1);
+	value = lfo.render(10, type, 10);
 	CHECK_EQUAL(-78931243, value);
-
-	lfo.tick(10, 10); // no change...
-	value = lfo.render(1, type, 1);
-	CHECK_EQUAL(-78931243, value);
-	CHECK_EQUAL(102, lfo.phase);
+	CHECK_EQUAL(100, lfo.phase);
 }
