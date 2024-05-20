81de9447dc96a7d2f1f9aebf65f1d734b144143d
Nikodemus Siivola <nikodemus@random-state.net>
2024-05-20 19:46:23 +0300

    continuous getTriangle() with tests (#1968)
    * tests: add test for getTriangle()
    
    * Test at turnover points and near zero. Demonstrates discontinuiety.
    
    * Sketch of alternative implementation which is continuous.
    
    * getTriangle: continuous version
    
    * The original had a discontinuiety at the peak, due
      to an overflow to the sign bit.

--- src/deluge/util/waves.h
+++ src/deluge/util/waves.h
@@ -51,6 +51,11 @@ extern uint32_t z, w, jcong;
 }
 
 [[gnu::always_inline]] inline int32_t getTriangle(uint32_t phase) {
-	return ((phase < 2147483648u) ? 2 : -2) * phase + 2147483648u;
-	// return getTriangleSmall(phase) << 1;
+	int32_t slope = 2;
+	int32_t offset = 0x80000000u;
+	if (phase >= 0x80000000u) {
+		slope = -2;
+		offset = 0x80000000u - 1;
+	}
+	return slope * phase + offset;
 }
