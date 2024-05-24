9fbfb141beda89f72d47ebd9763ee46c74515636
Nikodemus Siivola <nikodemus@random-state.net>
2024-05-24 22:03:00 +0300

    NoteSet: explicit representation for sets of chromatic notes (#2014)
    * NoteSet initially replaces ad-hoc arrays of 12 booleans used
      in scale switching code.
    
    * This commit also introduces model/scales directory, intended to hold
      abstractions dealing with scales.

---
+++ tests/unit/scale_tests.cpp
@@ -0,0 +1,38 @@
+#include "CppUTest/TestHarness.h"
+#include "model/scale/note_set.h"
+
+TEST_GROUP(NoteSetTest) {
+};
+
+TEST(NoteSetTest, init) {
+	NoteSet notes;
+	CHECK(notes.size == 12);
+	for (int i = 0; i < notes.size; i++) {
+		CHECK(!notes.has(i));
+	}
+}
+
+TEST(NoteSetTest, add) {
+	NoteSet notes;
+	notes.add(7);
+	for (int i = 0; i < notes.size; i++) {
+		CHECK(notes.has(i) == (i == 7));
+	}
+}
+
+TEST(NoteSetTest, fill) {
+	NoteSet notes;
+	notes.fill();
+	for (int i = 0; i < notes.size; i++) {
+		CHECK(notes.has(i));
+	}
+}
+
+TEST(NoteSetTest, count) {
+	NoteSet notes;
+	CHECK(notes.count() == 0);
+	notes.add(3);
+	CHECK(notes.count() == 1);
+	notes.fill();
+	CHECK(notes.count() == 12);
+}
