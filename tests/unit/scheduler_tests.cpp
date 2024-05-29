5982b2c1bfac9a9cc3a1e5e4e0cc2e4ef0b98379
m-m-adams <mark.adams@queensu.ca>
2024-05-29 18:49:51 -0400

    name function in test

--- tests/unit/scheduler_tests.cpp
+++ tests/unit/scheduler_tests.cpp
@@ -92,7 +92,7 @@ TEST(Scheduler, backOffTime) {
 	mock().clear();
 	// will be called one less time due to the time the sleep takes not being zero
 	mock().expectNCalls(9, "sleep_50ns");
-	taskManager.addRepeatingTask(sleep_50ns, 1, 0.01, 0.001, 1);
+	taskManager.addRepeatingTask(sleep_50ns, 1, 0.01, 0.001, 1, "sleep_50ns");
 	// run the scheduler for just under 10ms, calling the function to sleep 50ns every 1ms
 	taskManager.start(0.1);
 	std::cout << "ending tests at " << getTimerValueSeconds(0) << std::endl;
