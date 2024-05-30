77f55f4bc936eedd94b53ae3c14181e6c918e9ee
Nikodemus Siivola <nikodemus@random-state.net>
2024-05-30 18:11:01 +0300

    tests: unpatched_param::Pan::readCurrentValue() & getFinalValue()
    - Pull out guts for testing. The other Pan variants coming up
      separately.

--- src/deluge/gui/menu_item/value_scaling.cpp
+++ src/deluge/gui/menu_item/value_scaling.cpp
@@ -11,6 +11,10 @@ int32_t computeCurrentValueForHalfPrecisionMenuItem(int32_t value) {
 	return ((int64_t)value * (kMaxMenuValue * 2) + 2147483648) >> 32;
 }
 
+int32_t computeCurrentValueForPan(int32_t value) {
+	return ((int64_t)value * (kMaxMenuRelativeValue * 2) + 2147483648) >> 32;
+}
+
 int32_t computeFinalValueForStandardMenuItem(int32_t value) {
 	if (value == kMaxMenuValue) {
 		return 2147483647;
@@ -46,5 +50,17 @@ int32_t computeFinalValueForHalfPrecisionMenuItem(int32_t value) {
 	}
 	else {
 		return (uint32_t)value * (2147483648 / kMidMenuValue) >> 1;
+	}
+}
+
+int32_t computeFinalValueForPan(int32_t value) {
+	if (value == kMaxMenuRelativeValue) {
+		return 2147483647;
+	}
+	else if (value == kMinMenuRelativeValue) {
+		return -2147483648;
+	}
+	else {
+		return ((int32_t)value * (2147483648 / (kMaxMenuRelativeValue * 2)) * 2);
 	}
 }
