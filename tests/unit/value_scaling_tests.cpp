5bf8f814bf6bcf8995495bfa901215a181ea3a17
Nikodemus Siivola <nikodemus@random-state.net>
2024-06-02 04:07:34 +0300

    tests: CompParam::readCurrentValue() & getFinalValue() (#2068)
    - Pull out the guts for testing.
    
    - Rename standard menu item roundtrip test to "scaling test",
      since it tests a few specific values in addition to roundtripping.

--- tests/unit/value_scaling_tests.cpp
+++ tests/unit/value_scaling_tests.cpp
@@ -6,7 +6,7 @@
 
 TEST_GROUP(ValueScalingTest) {};
 
-TEST(ValueScalingTest, standardMenuItemValueRoundTrip) {
+TEST(ValueScalingTest, standardMenuItemValueScaling) {
 	for (int i = kMinMenuValue; i <= kMaxMenuValue; i++) {
 		int32_t finalValue = computeFinalValueForStandardMenuItem(i);
 		int32_t currentValue = computeCurrentValueForStandardMenuItem(finalValue);
@@ -15,4 +15,15 @@ TEST(ValueScalingTest, standardMenuItemValueRoundTrip) {
 	CHECK_EQUAL(INT32_MIN, computeFinalValueForStandardMenuItem(0));
 	CHECK_EQUAL(-23,       computeFinalValueForStandardMenuItem(25));
 	CHECK_EQUAL(INT32_MAX, computeFinalValueForStandardMenuItem(50));
+}
+
+TEST(ValueScalingTest, compParamMenuItemValueScaling) {
+	for (int i = kMinMenuValue; i <= kMaxMenuValue; i++) {
+		int32_t finalValue = computeFinalValueForCompParam(i);
+		int32_t currentValue = computeCurrentValueForCompParam(finalValue);
+		CHECK_EQUAL(i, currentValue);
+	}
+	CHECK_EQUAL(0,          computeFinalValueForCompParam(0));
+	CHECK_EQUAL(1073741812, computeFinalValueForCompParam(25));
+	CHECK_EQUAL(INT32_MAX,  computeFinalValueForCompParam(50));
 }
