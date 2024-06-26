1c4f40313356fcaa9bc76699d9d046014b5dc0ed
m-m-adams <mark.adams@queensu.ca>
2024-06-26 11:58:11 -0400

    catch error before infinit copy

--- src/deluge/hid/display/oled.cpp
+++ src/deluge/hid/display/oled.cpp
@@ -309,7 +309,14 @@ void copyRowWithMask(uint8_t destMask, uint8_t sourceRow[], uint8_t destRow[], i
 
 void copyBackgroundAroundForeground(ImageStore backgroundImage, ImageStore foregroundImage, int32_t minX, int32_t minY,
                                     int32_t maxX, int32_t maxY) {
-
+	if (maxX < 0 || minX < 0 || maxX < minX || minX > OLED_MAIN_WIDTH_PIXELS || maxX > OLED_MAIN_WIDTH_PIXELS)
+	    [[unlikely]] {
+		FREEZE_WITH_ERROR("C001");
+	}
+	if (maxY < 0 || minY < 0 || maxY < minY || minY > OLED_MAIN_HEIGHT_PIXELS || maxY > OLED_MAIN_HEIGHT_PIXELS)
+	    [[unlikely]] {
+		FREEZE_WITH_ERROR("C002");
+	}
 	// Copy everything above
 	int32_t firstRow = minY >> 3;
 	int32_t lastRow = maxY >> 3;
