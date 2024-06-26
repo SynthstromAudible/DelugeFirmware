58c8f63a5fcb564407b97feaee0d825b1dee3ea5
Nikodemus Siivola <nikodemus@random-state.net>
2024-06-26 14:18:55 +0300

    refactor/test: extract is-same-note logic from InstrumentClipView
    - There were a couple of variations, both seemingly needlessly complex,
      and the second one buggy. Test case includes the verbatim versions
      of the old code, and demonstrates bug.
    
    - Drop a couple of comments that aren't needed given a descriptive
      function name.

--- tests/unit/scale_tests.cpp
+++ tests/unit/scale_tests.cpp
@@ -1,5 +1,6 @@
 #include "CppUTest/TestHarness.h"
 #include "model/scale/note_set.h"
+#include "model/scale/utils.h"
 
 TEST_GROUP(NoteSetTest){};
 
@@ -34,4 +35,31 @@ TEST(NoteSetTest, count) {
 	CHECK(notes.count() == 1);
 	notes.fill();
 	CHECK(notes.count() == 12);
+}
+
+TEST_GROUP(UtilTest){};
+
+TEST(UtilTest, isSameNote) {
+	// Exhaustive test for a reasonable input range
+	for (int a = -200; a <= 200; a++) {
+		for (int b = -200; b <= 200; b++) {
+			// Different variations in the codebase that were replaced by isSameNote()
+			int legacy1 = (int32_t)std::abs(a - b) % 12 == 0;
+			int legacy2 = (uint16_t)(a - b + 120) % (uint8_t)12 == 0;
+			bool same = isSameNote(a, b);
+			// The first variation matches always.
+			CHECK_EQUAL(legacy1, same);
+			// The second variation returns bogus values if (a-b+120) goes negative,
+			// due to the cast to u16.
+			if (a - b + 120 >= 0) {
+				CHECK_EQUAL(legacy2, same);
+			}
+		}
+	}
+	// Explicit demo of the legacy2 code misbehaving: both numbers are 2 modulo 12.
+	int a = 2;
+	int b = 146;
+	int legacy2 = (uint16_t)(a - b + 120) % (uint8_t)12 == 0;
+	CHECK(!legacy2);
+	CHECK(isSameNote(a, b));
 }
