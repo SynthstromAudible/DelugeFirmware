d08553a65a4e2325d006d281f1562c6f8c542dfd
Nikodemus Siivola <nikodemus@random-state.net>
2024-07-11 03:22:43 +0300

    refactor: audio clip attack value scaling (#2249)
    * refactor: audio clip attack value scaling
    
    - fix wrong value in computeFinalValueForArpMidiCvRate() comment
    
    - math is same as in with arp::midi_cv::Gate, rename the shared
      logic to compute(Current|Final)ValueForSemiStandardMenuItem().
    
    - move the thin wrappers to be inline functions
    
    - delete dead code: computeFinalValueForCompParam()
    
    * document assumption of accidental difference in math
    
    * rename compute*ForSemiStandardMenuItem -> compute*WithoutRoundingToOne
    
    * more StandardMenuItem scaling
    
    - arpeggiator::midi_cv::Gate: difference between the non-standard and this
      is unmeasurable, still a 2.5ms down time between two 16th notes with Gate 50.
    
    - arpeggiator::midi_cv::Rate: also unmeasurable difference
    
    - audio_clip::Attack difference is even more subtle, if possible

--- tests/unit/value_scaling_tests.cpp
+++ tests/unit/value_scaling_tests.cpp
@@ -39,26 +39,6 @@ TEST(ValueScalingTest, panValueScaling) {
 	CHECK_EQUAL(INT32_MAX, computeFinalValueForPan(25));
 }
 
-TEST(ValueScalingTest, arpMidiCvGateValueScaling) {
-	for (int i = kMinMenuValue; i <= kMaxMenuValue; i++) {
-		int32_t finalValue = computeFinalValueForArpMidiCvGate(i);
-		int32_t currentValue = computeCurrentValueForArpMidiCvGate(finalValue);
-		CHECK_EQUAL(i, currentValue);
-	}
-	CHECK_EQUAL(INT32_MIN, computeFinalValueForArpMidiCvGate(0));
-	CHECK_EQUAL(-23, computeFinalValueForArpMidiCvGate(25));
-	// As seen here, we diverge _slightly_ from the standard
-	// menu item scaling when computing final values, despite roundtripping
-	// and using the identical current value computation.
-	//
-	// See computeFinalValueForArpMidiCvGate()'s comment for possible
-	// motivation.
-	CHECK_EQUAL(INT32_MAX - 45, computeFinalValueForArpMidiCvGate(50));
-	// while 50 doesn't quite get to INT32_MAX, make sure the current value math
-	// behaves well on the whole range
-	CHECK_EQUAL(50, computeCurrentValueForArpMidiCvGate(INT32_MAX));
-}
-
 TEST(ValueScalingTest, consistentArpAndMenuMaxValues) {
 	// See comment above definition of kMaxPresetArpRhythm
 	CHECK_EQUAL(kMaxMenuValue, kMaxPresetArpRhythm);
@@ -77,18 +57,4 @@ TEST(ValueScalingTest, arpMidiCvRatchetOrRhytmValueScaling) {
 	// while 50 doesn't quite get to UINT32_MAX, make sure the current value math
 	// behaves well on the whole range
 	CHECK_EQUAL(50, computeCurrentValueForArpMidiCvRatchetsOrRhythm(UINT32_MAX));
-}
-
-TEST(ValueScalingTest, arpMidiCvRate) {
-	for (int i = kMinMenuValue; i <= kMaxMenuValue; i++) {
-		int32_t finalValue = computeFinalValueForArpMidiCvRate(i);
-		int32_t currentValue = computeCurrentValueForArpMidiCvRate(finalValue);
-		CHECK_EQUAL(i, currentValue);
-	}
-	CHECK_EQUAL(INT32_MIN, computeFinalValueForArpMidiCvRate(0));
-	CHECK_EQUAL(0, computeFinalValueForArpMidiCvRate(25));
-	CHECK_EQUAL(INT32_MAX - 45, computeFinalValueForArpMidiCvRate(50));
-	// while 50 doesn't quite get to INT32_MAX, make sure the current value math
-	// behaves well on the whole range
-	CHECK_EQUAL(50, computeCurrentValueForArpMidiCvRate(INT32_MAX));
 }
