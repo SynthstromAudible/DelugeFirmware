SynthstromAudible/DelugeFirmware

commit 149d907773be27aa5b2cf1485012404355de0005

Os/task handler (#1708)
* folder restructure

* scheduler api

* task handler framework

* rename unit to memory for clarity on 32 bit

* basic task scheduler tests

* switch to seconds for timer api

* fix names for workflow

* init clock time for linux

* cleanup types for better accuracy

* remove extra defines

* fix up volatile reads etc.

* play with scheduling for better real time performance

* improve mock timer accuracy

* refactor mainloop to function and add macro to choose between loop and task manager

* start splitting audio routine up

* clean up API

* use task manager by default, increase ui timer call rate

* fix variable style

* remove binary dir

* use constant for max number of tasks

src/deluge/deluge.cpp
@@ -62,6 +62,7 @@
 #include "storage/audio/audio_file_manager.h"
 #include "storage/flash_storage.h"
 #include "storage/storage_manager.h"
+#include "task_scheduler.h"
 #include "util/misc.h"
 #include "util/pack.h"
 #include <stdlib.h>
@@ -388,7 +389,9 @@ bool readButtonsAndPads() {
 
 	return anything;
 }
-
+void readButtonsAndPadsOnce() {
+	readButtonsAndPads();
+}
 void setUIForLoadedSong(Song* song) {
 
 	UI* newUI;
@@ -544,6 +547,77 @@ extern "C" volatile uint32_t usbLock;
 
 extern "C" void usb_main_host(void);
 
+void registerTasks() {
+	addRepeatingTask([]() { uiTimerManager.routine(); }, 101, 0.001, 0.005, 0.010);
+	if (hid::display::have_oled_screen) {
+		addRepeatingTask(&(oledRoutine), 100, 0.01, 0.01, 0.02);
+	}
+	addRepeatingTask(&(PIC::flush), 99, 0.001, 0.001, 0.002);
+	addRepeatingTask(&(readButtonsAndPadsOnce), 20, 0.005, 0.005, 0.01);
+	// 30 Hz update desired?
+	addRepeatingTask(&doAnyPendingUIRendering, 50, 0.02, 0.02, 0.03);
+
+	// this one runs quickly and frequently to check for encoder changes
+	addRepeatingTask([]() { encoders::readEncoders(); }, 49, 0.001, 0.001, 0.001);
+	addRepeatingTask([]() { encoders::interpretEncoders(true); }, 10, 0.001, 0.001, 0.005);
+	// this one actually actions them
+	addRepeatingTask([]() { encoders::interpretEncoders(false); }, 48, 0.005, 0.005, 0.025);
+
+	// addRepeatingTask([]() { AudioEngine::routineWithClusterLoading(true); }, 0, 1 / 44100., 16 / 44100., 32 / 44100.,
+	// true);
+	addRepeatingTask([]() { audioFileManager.loadAnyEnqueuedClusters(8, false); }, 5, 0.00001, 0.00001, 0.00002);
+	addRepeatingTask(&(AudioEngine::routine), 0, 1 / 44100., 16 / 44100., 32 / 44100.);
+	// addRepeatingTask(&(AudioEngine::routine), 0, 16 / 44100., 64 / 44100., true);
+
+	// formerly part of audio routine
+	addRepeatingTask([]() { playbackHandler.routine(); }, 10, 4 / 44100., 16 / 44100, 64 / 44100.);
+	addRepeatingTask([]() { audioFileManager.slowRoutine(); }, 60, 0.1, 0.1, 0.2);
+	addRepeatingTask([]() { audioRecorder.slowRoutine(); }, 61, 0.01, 0.1, 0.1);
+	addRepeatingTask(&AudioEngine::slowRoutine, 70, 0.01, 0.05, 0.1);
+}
+void mainLoop() {
+	while (1) {
+
+		uiTimerManager.routine();
+
+		// Flush stuff - we just have to do this, regularly
+		if (hid::display::have_oled_screen) {
+			oledRoutine();
+		}
+		PIC::flush();
+
+		AudioEngine::routineWithClusterLoading(true); // -----------------------------------
+
+		int32_t count = 0;
+		while (readButtonsAndPads() && count < 16) {
+			if (!(count & 3)) {
+				AudioEngine::routineWithClusterLoading(true); // -----------------------------------
+			}
+			count++;
+		}
+
+		encoders::readEncoders();
+		bool anything = encoders::interpretEncoders();
+		if (anything) {
+			AudioEngine::routineWithClusterLoading(true); // -----------------------------------
+		}
+
+		doAnyPendingUIRendering();
+
+		AudioEngine::routineWithClusterLoading(true); // -----------------------------------
+
+		// Only actually needs calling a couple of times per second, but we can't put it in uiTimerManager cos that gets
+		// called in card routine
+		audioFileManager.slowRoutine();
+		AudioEngine::slowRoutine();
+
+		audioRecorder.slowRoutine();
+
+#if AUTOPILOT_TEST_ENABLED
+		autoPilotStuff();
+#endif
+	}
+}
 extern "C" int32_t deluge_main(void) {
 	// Piggyback off of bootloader DMA setup.
 	uint32_t oledSPIDMAConfig = (0b1101000 | (OLED_SPI_DMA_CHANNEL & 7));
@@ -827,48 +901,12 @@ extern "C" int32_t deluge_main(void) {
 	D_PRINTLN("going into main loop");
 	sdRoutineLock = false; // Allow SD routine to start happening
 
-	while (1) {
-
-		uiTimerManager.routine();
-
-		// Flush stuff - we just have to do this, regularly
-		if (deluge::hid::display::have_oled_screen) {
-			oledRoutine();
-		}
-		PIC::flush();
-
-		AudioEngine::routineWithClusterLoading(true); // -----------------------------------
-
-		int32_t count = 0;
-		while (readButtonsAndPads() && count < 16) {
-			if (!(count & 3)) {
-				AudioEngine::routineWithClusterLoading(true); // -----------------------------------
-			}
-			count++;
-		}
-
-		encoders::readEncoders();
-		bool anything = encoders::interpretEncoders();
-		if (anything) {
-			AudioEngine::routineWithClusterLoading(true); // -----------------------------------
-		}
-
-		doAnyPendingUIRendering();
-
-		AudioEngine::routineWithClusterLoading(true); // -----------------------------------
-
-		// Only actually needs calling a couple of times per second, but we can't put it in uiTimerManager cos that gets
-		// called in card routine
-		audioFileManager.slowRoutine();
-		AudioEngine::slowRoutine();
-
-		audioRecorder.slowRoutine();
-
-#if AUTOPILOT_TEST_ENABLED
-		autoPilotStuff();
+#ifdef USE_TASK_MANAGER
+	registerTasks();
+	startTaskManager();
+#else
+	mainLoop();
 #endif
-	}
-
 	return 0;
 }
 

.github/workflows/tests.yml
@@ -34,10 +34,14 @@ jobs:
       - name: Run CTest
         run: ctest --test-dir build --output-junit results/ctest_results.xml
 
-      - name: Run CppUTest
+      - name: Run 32 bit CppUTest
         run: |
           cd build/results
-          ../unit/RunAllTests -ojunit
+          ../32bit_unit_tests/32BitTests -ojunit
+      - name: Run main CppUTest
+        run: |
+          cd build/results
+          ../unit/UnitTests -ojunit
 
       - name: Upload Test Results
         uses: actions/upload-artifact@v4

CMakeLists.txt
@@ -232,7 +232,7 @@ list(APPEND DELUGE_COMMON_COMPILE_OPTIONS
     $<$<CONFIG:DEBUG>:-Wno-unused-parameter>
 
     $<$<CONFIG:DEBUG>:-Werror=write-strings>
-
+    -DUSE_TASK_MANAGER
     # Offsetof for non standard types is supported in GCC,
     # if another compiler does not support it they are obligated to error
 

src/CMakeLists.txt
@@ -1,7 +1,8 @@
 cmake_path(SET SHARED_INCLUDE ${CMAKE_CURRENT_LIST_DIR})
 
-target_sources(deluge PUBLIC main.c resetprg.c fault_handler.c c_lib_alternatives.S sys_stubs.c timers_interrupts.c)
+target_sources(deluge PUBLIC main.c resetprg.c c_lib_alternatives.S sys_stubs.c)
 
+add_subdirectory(OSLikeStuff)
 add_subdirectory(deluge)
 add_subdirectory(RZA1)
 add_subdirectory(RTT)

src/OSLikeStuff/CMakeLists.txt
@@ -1,0 +1,13 @@
+# Add the sources to the target
+file(GLOB_RECURSE OS_SOURCES CONFIGURE_DEPENDS
+        ${CMAKE_CURRENT_LIST_DIR}/*.S
+        ${CMAKE_CURRENT_LIST_DIR}/*.c
+        ${CMAKE_CURRENT_LIST_DIR}/*.cpp
+)
+
+target_sources(deluge PUBLIC ${OS_SOURCES})
+
+target_include_directories(deluge PUBLIC
+        ${CMAKE_CURRENT_LIST_DIR}
+        ${SHARED_INCLUDE}
+)

src/OSLikeStuff/task_scheduler.cpp
@@ -1,0 +1,180 @@
+/*
+ * Copyright © 2014-2023 Mark Adams
+ *
+ * This file is part of The Synthstrom Audible Deluge Firmware.
+ *
+ * The Synthstrom Audible Deluge Firmware is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "task_scheduler.h"
+#include "RZA1/ostm/ostm.h"
+#include "util/container/static_vector.hpp"
+#include <algorithm>
+#include <iostream>
+// currently 14 are in use
+constexpr int kMaxTasks = 20;
+struct Task {
+	TaskHandle handle;
+	uint8_t priority;
+	double lastCallTime;
+	double averageDuration;
+	double minTimeBetweenCalls;
+	double targetTimeBetweenCalls;
+	double maxTimeBetweenCalls;
+};
+
+struct SortedTask {
+	uint8_t priority = UINT8_MAX;
+	TaskID task = -1;
+	// priorities are descending, so this puts low pri tasks first
+	bool operator<(const SortedTask& another) const { return priority > another.priority; }
+};
+
+/// internal only to the task scheduler, hence all public. External interaction to use the api
+struct TaskManager {
+
+	std::array<Task, kMaxTasks> list;
+	std::array<SortedTask, kMaxTasks> sortedList;
+	TaskID index = 0;
+	double mustEndBefore = 128;
+	void start(double duration = 0);
+	void removeTask(TaskID id);
+	void runTask(TaskID id);
+	TaskID chooseBestTask(double deadline);
+	TaskID addRepeatingTask(TaskHandle task, uint8_t priority, double minTimeBetweenCalls,
+	                        double targetTimeBetweenCalls, double maxTimeBetweenCalls);
+	void createSortedList();
+	void clockRolledOver();
+};
+
+TaskManager taskManager;
+
+void TaskManager::createSortedList() {
+	for (TaskID i = 0; i <= index; i++) {
+		sortedList[i] = (SortedTask{list[i].priority, i});
+	}
+	std::sort(sortedList.begin(), &sortedList[index + 1]);
+}
+
+TaskID TaskManager::chooseBestTask(double deadline) {
+	double currentTime = getTimerValueSeconds(0);
+	double nextFinishTime = currentTime;
+	TaskID bestTask = -1;
+	uint8_t bestPriority = INT8_MAX;
+	/// Go through all tasks. If a task needs to be called before the current best task finishes, and has a higher
+	/// priority than the current best task, it becomes the best task
+
+	for (int i = 0; i < index; i++) {
+		struct Task t = list[sortedList[i].task];
+		double timeToCall = t.lastCallTime + t.targetTimeBetweenCalls - t.averageDuration;
+		double maxTimeToCall = t.lastCallTime + t.maxTimeBetweenCalls - t.averageDuration;
+		double timeSinceCall = currentTime - t.lastCallTime;
+		// ensure every routine is within its target
+		if (timeSinceCall > t.maxTimeBetweenCalls) {
+			uint8_t next = sortedList[i].task;
+			return next;
+		}
+
+		if (timeToCall < currentTime || maxTimeToCall < nextFinishTime) {
+			if (currentTime + t.averageDuration < deadline) {
+
+				if (t.priority < bestPriority && t.handle) {
+					if (timeSinceCall > t.minTimeBetweenCalls) {
+						bestTask = sortedList[i].task;
+					}
+					else {
+						bestTask = -1;
+					}
+					bestPriority = t.priority;
+					nextFinishTime = currentTime + t.averageDuration;
+				}
+			}
+		}
+	}
+	return bestTask;
+}
+
+TaskID TaskManager::addRepeatingTask(TaskHandle task, uint8_t priority, double minTimeBetweenCalls,
+                                     double targetTimeBetweenCalls, double maxTimeBetweenCalls) {
+	list[index] = (Task{task, priority, 0, 0, minTimeBetweenCalls, targetTimeBetweenCalls, maxTimeBetweenCalls});
+	createSortedList();
+	return index++;
+}
+
+void TaskManager::removeTask(TaskID id) {
+	list[id] = Task{0, 0, 0, 0, 0, 0};
+	return;
+}
+constexpr double rollTime = ((double)(UINT32_MAX) / DELUGE_CLOCKS_PERf);
+
+void TaskManager::runTask(TaskID id) {
+	list[id].lastCallTime = getTimerValueSeconds(0);
+	list[id].handle();
+	double runtime = (getTimerValueSeconds(0) - list[id].lastCallTime);
+	if (runtime < 0) {
+		runtime += rollTime;
+	}
+	if (list[id].averageDuration < 0.000001 || list[id].averageDuration > 0.01) {
+		list[id].averageDuration = runtime;
+	}
+	else {
+		list[id].averageDuration = (list[id].averageDuration + runtime) / 2;
+	}
+}
+void TaskManager::clockRolledOver() {
+	for (int i = 0; i < index; i++) {
+		list[i].lastCallTime -= rollTime;
+	}
+}
+
+/// default duration of 0 signifies infinite loop, intended to be specified only for testing
+void TaskManager::start(double duration) {
+	// set up os timer 0 as a free running timer
+
+	disableTimer(0);
+	setTimerValue(0, 0);
+	// just let it count - a full loop is 2 minutes or so and we'll handle that case manually
+	setOperatingMode(0, FREE_RUNNING, false);
+	enableTimer(0);
+	double startTime = getTimerValueSeconds(0);
+	double lastLoop = startTime;
+	if (duration != 0) {
+		mustEndBefore = startTime + duration;
+	}
+
+	while (duration == 0 || getTimerValueSeconds(0) < startTime + duration) {
+		double newTime = getTimerValueSeconds(0);
+		if (newTime < lastLoop) {
+			clockRolledOver();
+		}
+		else {}
+		lastLoop = newTime;
+		TaskID task = chooseBestTask(mustEndBefore);
+		if (task >= 0) {
+			runTask(task);
+		}
+	}
+}
+
+// C api starts here
+void startTaskManager() {
+	taskManager.start();
+}
+
+uint8_t addRepeatingTask(TaskHandle task, uint8_t priority, double minTimeBetweenCalls, double targetTimeBetweenCalls,
+                         double maxTimeBetweenCalls) {
+	return taskManager.addRepeatingTask(task, priority, minTimeBetweenCalls, targetTimeBetweenCalls,
+	                                    maxTimeBetweenCalls);
+}
+void removeTask(uint8_t id) {
+	return taskManager.removeTask(id);
+}

commit 1673cb1d82281d9701050ae6585793aeb82aadfe

task manager: allow speedy handling of sysex messages
The loading time of firmware over USB regressed in 149d907
From ~2.5 seconds to ~6.1 seconds (regardless if debug or release build).
This restores the old performance.

It seems to be enough to _allow_ playbackHandler.routine() to run more
often without increasing the target time, so this should have no
negative effects when there is contention with other tasks, I guess.

src/deluge/deluge.cpp
@@ -572,7 +572,7 @@ void registerTasks() {
 	// this one runs quickly and frequently to check for encoder changes
 	addRepeatingTask([]() { encoders::readEncoders(); }, p++, 0.0005, 0.001, 0.001, "read encoders");
 	// formerly part of audio routine, updates midi and clock
-	addRepeatingTask([]() { playbackHandler.routine(); }, p++, 8 / 44100., 16 / 44100, 32 / 44100., "playback routine");
+	addRepeatingTask([]() { playbackHandler.routine(); }, p++, 2 / 44100., 16 / 44100, 32 / 44100., "playback routine");
 	addRepeatingTask([]() { audioFileManager.loadAnyEnqueuedClusters(128, false); }, p++, 0.00001, 0.00001, 0.00002,
 	                 "load clusters");
 	// handles sd card recorders