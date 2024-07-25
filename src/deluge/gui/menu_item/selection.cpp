29ee977ffe72ad1f95589c42b4dcab0952d6a1c2
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2024-07-25 17:35:51 -0400

    Update Menu UI for interacting with toggle menu's
    Updated UI for interacting with toggle (ON/OFF) menu's on OLED and 7SEG
    - OLED renders a checkbox that shows current ON/OFF status. Selecting that menu with select encoder will toggle the checkbox as opposed to entering the menu
    - 7SEG renders a dot at the end of the menu item to show current ON/OFF status. Selecting that menu with select encoder will toggle the dot as opposed to entering the menu.
    
    Submenu's are also now rendered with a ">" at the end to indicate that it is a submenu.
    
    There are a couple edge case menu's (e.g. CV / Gate Output, Community Features) that do not inherit these changes but I think those can be addressed as part of a separate PR as they are built differently (don't use toggle or submenu classes).
    
    Fix PR links
    
    Render more menu's as toggles
    
    Some selection class menu's are actually toggles.
    
    This PR enables those selection class menu's to behave like toggles and thus display a checkbox (on OLED) and dot (on 7SEG)

--- src/deluge/gui/menu_item/selection.cpp
+++ src/deluge/gui/menu_item/selection.cpp
@@ -2,6 +2,7 @@
 #include "gui/menu_item/menu_item.h"
 #include "gui/ui/sound_editor.h"
 #include "hid/display/display.h"
+#include "hid/display/oled.h"
 
 namespace deluge::gui::menu_item {
 void Selection::drawValue() {
@@ -29,4 +30,36 @@ void Selection::drawPixelsForOled() {
 	MenuItem::drawItemsForOled(std::span{getOptions().data(), getOptions().size()}, selectedOption,
 	                           soundEditor.menuCurrentScroll);
 }
+
+// renders check box on OLED and dot on 7seg
+void Selection::displayToggleValue() {
+	if (display->haveOLED()) {
+		renderUIsForOled();
+	}
+	else {
+		drawName();
+	}
+}
+
+// handles rendering of the community features menu items that are identified as toggles
+void Selection::renderSubmenuItemTypeForOled(int32_t xPixel, int32_t yPixel) {
+	deluge::hid::display::oled_canvas::Canvas& image = deluge::hid::display::OLED::main;
+
+	std::string stringForSubmenuItemType;
+
+	if (isToggle()) {
+		if (getToggleValue()) {
+			stringForSubmenuItemType.append("[x]");
+		}
+		else {
+			stringForSubmenuItemType.append("[ ]");
+		}
+	}
+	else {
+		stringForSubmenuItemType.append("  >");
+	}
+
+	image.drawString(stringForSubmenuItemType, xPixel, yPixel, kTextSpacingX, kTextSpacingY);
+}
+
 } // namespace deluge::gui::menu_item
