5f59ac22d82037f91696c9cd63dc1d79089bbd8a
Jamie Fenton <jamie@fentonia.com>
2024-07-29 12:16:46 -0500

    Fix conflict with ToT in StorageManager.

--- src/deluge/storage/storage_manager.cpp
+++ src/deluge/storage/storage_manager.cpp
@@ -249,7 +249,7 @@ Error StorageManager::initSD() {
 	// If we know the SD card is still initialised, no need to actually initialise
 	DSTATUS status = disk_status(SD_PORT);
 	if ((status & STA_NOINIT) == 0) {
-		auto _ = fileSystemStuff.fileSystem.mount(0); // check that it's mounted but don't block if not
+		auto _ = fileSystem.mount(0); // check that it's mounted but don't block if not
 		return Error::NONE;
 	}
 
