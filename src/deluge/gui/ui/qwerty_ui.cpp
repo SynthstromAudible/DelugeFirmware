7ea257f2c720662692021c0f33ff45ad913e8a9e
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2024-10-06 16:38:11 -0400

    Update qwerty_ui.cpp

--- src/deluge/gui/ui/qwerty_ui.cpp
+++ src/deluge/gui/ui/qwerty_ui.cpp
@@ -125,13 +125,12 @@ void QwertyUI::drawTextForOLEDEditing(int32_t xPixel, int32_t xPixelMax, int32_t
 	canvas.drawString(&displayName[scrollPosHorizontal], xPixel, yPixel, kTextSpacingX, kTextSpacingY, 0,
 	                  xPixel + maxNumChars * kTextSpacingX);
 
-	int32_t highlightStartX;
 	int32_t scrollAmount = enteredTextEditPos - scrollPosHorizontal;
 	
 	// if we're at the beginning of the string, we want to show an extra inverted pixel before
 	// if we're somewhere inside the string, we want to start highlight right on the character
 	int32_t startAdjustment = scrollAmount ? 0 : 1;
-	highlightStartX = xPixel + kTextSpacingX * scrollAmount - startAdjustment;
+	int32_t highlightStartX = xPixel + kTextSpacingX * scrollAmount - startAdjustment;
 
 	int32_t highlightWidth = xPixelMax - highlightStartX;
 
