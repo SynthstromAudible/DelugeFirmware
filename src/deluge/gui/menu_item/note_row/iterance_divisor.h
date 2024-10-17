5cfeb17353cb9e1d7351e4490394bb704957c99d
Raúl Muñoz <raul@mothership.com>
2024-10-17 16:06:52 +0200

    Feature // Custom iterance for notes and note rows (#2751)
    * Implemented custom iterance
    
    * Cleanup
    
    * Implement submenu under iterance’s CUSTOM value
    
    * Fix
    
    * Documentation
    
    * Cleanup and documentation
    
    * Fixed reading custom iterance
    
    * Fixed sanitizeIterance method. Typo when calculating the divisor
    
    * fix
    
    * When custom iterance is modified in note row, apply same iternace to notes
    
    * Refactor code to avoid repetition
    
    * dbt format
    
    * fix
    
    * Re-enable editing iterance with select encoder
    
    Re-enabled editing note / note row iterance using note / audition pad + turning select encoder
    
    - To edit probability, hold a note / audition pad and turn the select encoder to the left to display current probability value / set new probability value.
    
    - To edit iterance, hold a note / audition pad and turn the select encoder to the right to display current iterance value / set new iterance value.
    
    * Fix bug with blurred notes
    
    * Allow editing iterance turning the select encoder right while holding the note
    
    * Fix merge resolution mistake
    
    * Misc fixes
    
    - Moving knob right when you are at CUSTOM iterance will not reset the custom iterance set. It respects what is there (see code around “originalParameter”)
    - Changing the iterance directly turning select knob while holding note will now NOT allow moving past 8 of 8, and if already on Custom, it won’t let you go left to the presets
    
    # Conflicts:
    #	src/deluge/gui/views/instrument_clip_view.cpp
    
    * Fixed bug with blurred notes
    
    * docs
    
    * Save Iterance as an struct instead of a plain uint16
    
    * Renaming
    
    * renaming stuff
    
    * Refactor some methods inside the Iterance struct
    
    * Refactor to include methods as part of the struct
    
    * Added menu hierarchies
    
    * update community_features.md
    
    Co-authored-by: Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>

--- src/deluge/gui/menu_item/note_row/iterance_divisor.h
+++ src/deluge/gui/menu_item/note_row/iterance_divisor.h
@@ -15,24 +15,22 @@
  * If not, see <https://www.gnu.org/licenses/>.
  */
 #pragma once
-#include "definitions_cxx.hpp"
-#include "gui/menu_item/integer.h"
 #include "gui/menu_item/note_row/selected_note_row.h"
-#include "gui/ui/sound_editor.h"
 #include "gui/views/instrument_clip_view.h"
 #include "model/clip/instrument_clip.h"
 #include "model/instrument/kit.h"
 #include "model/model_stack.h"
 #include "model/note/note_row.h"
 #include "model/song/song.h"
+#include <cstdint>
 
 namespace deluge::gui::menu_item::note_row {
-class Fill final : public SelectedNoteRow {
+class IteranceDivisor final : public SelectedNoteRow {
 public:
 	using SelectedNoteRow::SelectedNoteRow;
 
-	[[nodiscard]] int32_t getMaxValue() const override { return FillMode::FILL; }
-	[[nodiscard]] int32_t getMinValue() const override { return FillMode::OFF; }
+	[[nodiscard]] int32_t getMaxValue() const override { return 8; }
+	[[nodiscard]] int32_t getMinValue() const override { return 1; }
 
 	/// @brief Begin an editing session with this menu item.
 	///
@@ -46,30 +44,36 @@ public:
 
 		if (modelStackWithNoteRow->getNoteRowAllowNull() != nullptr) {
 			NoteRow* noteRow = modelStackWithNoteRow->getNoteRowAllowNull();
-			this->setValue(noteRow->fillValue);
-			updateDisplay();
-		}
-	}
-
-	void selectEncoderAction(int32_t offset) final override {
-		int32_t newValue = instrumentClipView.setNoteRowFillWithOffset(offset);
-		if (newValue != -1) {
-			this->setValue(newValue);
-			updateDisplay();
-			if (currentSong->isFillModeActive()) {
-				uiNeedsRendering(&instrumentClipView);
+			Iterance iterance = noteRow->iteranceValue;
+			if (iterance == kDefaultIteranceValue) {
+				// if we end up here in this menu, convert OFF to the default CUSTOM value 1of1
+				// so we can make edits from here
+				iterance = kCustomIteranceValue;
 			}
+			int32_t divisor = iterance.divisor;
+			this->setValue(std::clamp<int32_t>(divisor, 1, 8));
+			updateDisplay();
 		}
 	}
+	void writeCurrentValue() override {
+		int32_t val = this->getValue();
+		char modelStackMemory[MODEL_STACK_MAX_SIZE];
+		ModelStackWithTimelineCounter* modelStack = currentSong->setupModelStackWithCurrentClip(modelStackMemory);
+		ModelStackWithNoteRow* modelStackWithNoteRow = getIndividualNoteRow(modelStack);
 
-	void drawPixelsForOled() final override {
-		deluge::hid::display::OLED::main.drawStringCentred(instrumentClipView.getFillString(this->getValue()),
-		                                                   18 + OLED_MAIN_TOPMOST_PIXEL, kTextHugeSpacingX,
-		                                                   kTextHugeSizeY);
+		if (modelStackWithNoteRow->getNoteRowAllowNull() != nullptr) {
+			NoteRow* noteRow = modelStackWithNoteRow->getNoteRowAllowNull();
+			Iterance iterance = noteRow->iteranceValue;
+			if (iterance == kDefaultIteranceValue) {
+				// if we end up here in this menu, convert OFF to the default CUSTOM value 1of1
+				// so we can make edits from here
+				iterance = kCustomIteranceValue;
+			}
+			int32_t mask = (1 << val) - 1; // Creates a mask where the first 'divisor' bits are 1
+			// Wipe the bits whose index is greater than the current divisor value
+			int32_t newIteranceSteps = ((iterance.toInt() & 0xFF) & mask);
+			instrumentClipView.setNoteRowIteranceWithFinalValue(Iterance{(uint8_t)val, newIteranceSteps});
+		}
 	}
-
-	void drawValue() final override { display->setText(instrumentClipView.getFillString(this->getValue())); }
-
-	void writeCurrentValue() override { ; }
 };
 } // namespace deluge::gui::menu_item::note_row
---
+++ src/deluge/gui/menu_item/note_row/iterance_step_toggle.h
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2014-2023 Synthstrom Audible Limited
+ *
+ * This file is part of The Synthstrom Audible Deluge Firmware.
+ *
+ * The Synthstrom Audible Deluge Firmware is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include "definitions_cxx.hpp"
+#include "gui/menu_item/toggle.h"
+#include "gui/views/instrument_clip_view.h"
+#include "model/clip/instrument_clip.h"
+#include "model/note/note_row.h"
+
+namespace deluge::gui::menu_item::note_row {
+class IteranceStepToggle : public Toggle {
+public:
+	using Toggle::Toggle;
+
+	IteranceStepToggle(l10n::String newName, l10n::String title, uint8_t newIndex) : Toggle(newName, title) {
+		this->index = newIndex;
+	}
+
+	ModelStackWithNoteRow* getIndividualNoteRow(ModelStackWithTimelineCounter* modelStack) {
+		auto* clip = static_cast<InstrumentClip*>(modelStack->getTimelineCounter());
+		ModelStackWithNoteRow* modelStackWithNoteRow =
+		    clip->getNoteRowOnScreen(instrumentClipView.lastAuditionedYDisplay,
+		                             modelStack); // don't create
+
+		bool isKit = clip->output->type == OutputType::KIT;
+
+		if (!isKit) {
+			if (!modelStackWithNoteRow->getNoteRowAllowNull()) { // if note row doesn't exist yet, create it
+				modelStackWithNoteRow =
+				    instrumentClipView.createNoteRowForYDisplay(modelStack, instrumentClipView.lastAuditionedYDisplay);
+			}
+		}
+
+		return modelStackWithNoteRow;
+	}
+
+	void updateDisplay() {
+		if (display->haveOLED()) {
+			renderUIsForOled();
+		}
+		else {
+			drawValue();
+		}
+	}
+
+	void readCurrentValue() override {
+		char modelStackMemory[MODEL_STACK_MAX_SIZE];
+		ModelStackWithTimelineCounter* modelStack = currentSong->setupModelStackWithCurrentClip(modelStackMemory);
+		ModelStackWithNoteRow* modelStackWithNoteRow = getIndividualNoteRow(modelStack);
+
+		if (modelStackWithNoteRow->getNoteRowAllowNull() != nullptr) {
+			NoteRow* noteRow = modelStackWithNoteRow->getNoteRowAllowNull();
+			Iterance iterance = noteRow->iteranceValue;
+			if (iterance == kDefaultIteranceValue) {
+				// if we end up here in this menu, convert OFF to the default CUSTOM value 1of1
+				// so we can make edits from here
+				iterance = kCustomIteranceValue;
+			}
+			this->setValue(iterance.iteranceStep[index]);
+			updateDisplay();
+		}
+	}
+	void writeCurrentValue() override {
+		bool value = this->getValue();
+		char modelStackMemory[MODEL_STACK_MAX_SIZE];
+		ModelStackWithTimelineCounter* modelStack = currentSong->setupModelStackWithCurrentClip(modelStackMemory);
+		ModelStackWithNoteRow* modelStackWithNoteRow = getIndividualNoteRow(modelStack);
+
+		if (modelStackWithNoteRow->getNoteRowAllowNull() != nullptr) {
+			NoteRow* noteRow = modelStackWithNoteRow->getNoteRowAllowNull();
+			Iterance iterance = noteRow->iteranceValue;
+			if (iterance == kDefaultIteranceValue) {
+				// if we end up here in this menu, convert OFF to the default CUSTOM value 1of1
+				// so we can make edits from here
+				iterance = kCustomIteranceValue;
+			}
+			int32_t newIteranceSteps = iterance.toInt() & 0xFF;
+			if (value) {
+				newIteranceSteps |= (1 << index);
+			}
+			else {
+				newIteranceSteps &= ~(1 << index);
+			}
+			instrumentClipView.setNoteRowIteranceWithFinalValue(Iterance{iterance.divisor, newIteranceSteps});
+		}
+	}
+
+	uint8_t index;
+
+	bool isRelevant(ModControllableAudio* modControllable, int32_t whichThing) override {
+		char modelStackMemory[MODEL_STACK_MAX_SIZE];
+		ModelStackWithTimelineCounter* modelStack = currentSong->setupModelStackWithCurrentClip(modelStackMemory);
+		ModelStackWithNoteRow* modelStackWithNoteRow = getIndividualNoteRow(modelStack);
+
+		if (modelStackWithNoteRow->getNoteRowAllowNull() != nullptr) {
+			NoteRow* noteRow = modelStackWithNoteRow->getNoteRowAllowNull();
+			Iterance iterance = noteRow->iteranceValue;
+			// Only show this iteration step if its index is smaller than current divisor value
+			return (iterance == kDefaultIteranceValue && index == 0) || iterance.divisor > index;
+		}
+		return false;
+	}
+};
+} // namespace deluge::gui::menu_item::note_row
