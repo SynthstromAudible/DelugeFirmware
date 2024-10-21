9f5ebeb7ba0858018bb4fa73d9850072619f0937
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2024-10-20 20:41:26 -0400

    Remove reset call
    Removed reset call as its unnecessary

--- src/deluge/model/sample/sample_recorder.cpp
+++ src/deluge/model/sample/sample_recorder.cpp
@@ -1109,7 +1109,6 @@ doFinishCapturing:
 					writePos = writePosNow;
 					numSamplesCaptured += numSamplesThisCycle;
 					sample->audioStartDetected = true;
-					envelopeFollower.reset();
 				}
 			}
 		}
