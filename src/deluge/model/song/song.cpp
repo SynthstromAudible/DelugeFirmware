dcc17ed982d21a88fafe1cd68a2593070c4d1b04
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2024-10-26 23:17:00 -0400

    Address feedback
    Changed Sound* to ModControllableAudio*

--- src/deluge/model/song/song.cpp
+++ src/deluge/model/song/song.cpp
@@ -41,6 +41,7 @@
 #include "model/consequence/consequence_clip_existence.h"
 #include "model/instrument/cv_instrument.h"
 #include "model/instrument/midi_instrument.h"
+#include "model/mod_controllable/mod_controllable_audio.h"
 #include "model/sample/sample_recorder.h"
 #include "model/scale/preset_scales.h"
 #include "model/scale/scale_change.h"
@@ -3312,7 +3313,7 @@ void Song::replaceInstrument(Instrument* oldOutput, Instrument* newOutput, bool 
 	// if we're replacing the same output type
 	if (newOutput->type == oldOutput->type) {
 		// exclude MIDI and CV instruments from this
-		// only sound instruments have a midi knob array
+		// only audio instruments have a midi knob array
 		if (newOutput->type == OutputType::SYNTH || newOutput->type == OutputType::KIT) {
 			// Migrate any midi learned params for synth and kit clip preset changes
 			// For kit clips it will migrate only kit affect entire midi learned params
@@ -3321,14 +3322,15 @@ void Song::replaceInstrument(Instrument* oldOutput, Instrument* newOutput, bool 
 			// - you midi learn a controller to that clip's params
 			// - you then go to change the preset for that clip
 			// - you expect that you can continue controlling the same params for the new preset
-			Sound* oldSound = (Sound*)oldOutput->toModControllable();
-			if (oldSound) {
-				int32_t numKnobs = oldSound->midiKnobArray.getNumElements();
+			ModControllableAudio* oldModControllableAudio = (ModControllableAudio*)oldOutput->toModControllable();
+			if (oldModControllableAudio) {
+				int32_t numKnobs = oldModControllableAudio->midiKnobArray.getNumElements();
 				if (numKnobs) {
-					Sound* newSound = (Sound*)newOutput->toModControllable();
-					newSound->midiKnobArray.cloneFrom(&oldSound->midiKnobArray);
-					oldSound->midiKnobArray.deleteAtIndex(0, numKnobs);
-					oldSound->ensureInaccessibleParamPresetValuesWithoutKnobsAreZero(this);
+					ModControllableAudio* newModControllableAudio =
+					    (ModControllableAudio*)newOutput->toModControllable();
+					newModControllableAudio->midiKnobArray.cloneFrom(&oldModControllableAudio->midiKnobArray);
+					oldModControllableAudio->midiKnobArray.deleteAtIndex(0, numKnobs);
+					oldModControllableAudio->ensureInaccessibleParamPresetValuesWithoutKnobsAreZero(this);
 				}
 			}
 		}
