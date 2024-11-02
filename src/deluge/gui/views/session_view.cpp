b034ff6a0902623c7772a6ba3e2ac26e140719c8
RJ-Eckie <121803687+RJ-Eckie@users.noreply.github.com>
2024-11-02 01:46:10 +0100

    Fix: Correct issue in previous commit

--- src/deluge/gui/views/session_view.cpp
+++ src/deluge/gui/views/session_view.cpp
@@ -4127,7 +4127,6 @@ ActionResult SessionView::gridHandlePadsLaunch(int32_t x, int32_t y, int32_t on,
 
 	// Release first pad, while two pads are held
 	if (!on && x == gridFirstPressedX && y == gridFirstPressedY && gridSecondPressedX != -1 && gridSecondPressedY != -1) {
-		gridResetPresses(true, true);
 		display->popupTextTemporary("COPY CANCELLED");
 	}
 
