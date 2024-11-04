de86c80274342b0e6761facf4a3a6c023d61434a
RJ-Eckie <121803687+RJ-Eckie@users.noreply.github.com>
2024-11-04 10:53:45 +0100

    Enter clip view after new clip on new track

--- src/deluge/gui/views/session_view.cpp
+++ src/deluge/gui/views/session_view.cpp
@@ -3900,6 +3900,7 @@ ActionResult SessionView::gridHandlePadsEdit(int32_t x, int32_t y, int32_t on, C
 					// don't update clip selection if we didn't create a clip
 					if (clip != nullptr && (x != gridFirstPressedX || y != gridFirstPressedY)) {
 						currentSong->setCurrentClip(clip);
+						transitionToViewForClip(clip);
 						return ActionResult::ACTIONED_AND_CAUSED_CHANGE;
 					}
 				}
@@ -4073,6 +4074,7 @@ ActionResult SessionView::gridHandlePadsLaunch(int32_t x, int32_t y, int32_t on,
 				gridFirstPressedY = y;
 				// will create the track if it doesn't exist
 				clip = gridCreateClip(gridSectionFromY(y), track, nullptr);
+				transitionToViewForClip(clip);
 				// If playing and Rec enabled, selecting an empty clip creates a new clip and starts it playing
 				// (depending on setting)
 				if (clip != nullptr && playbackHandler.playbackState
