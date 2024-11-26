3b7c65349bce2ad83cf794c12c0f495c84775c62
Katherine Whitlock <kate@skylinesynths.nyc>
2024-11-26 12:47:08 -0500

    Remove macros from voice (#2966)
    * Move WAVETABLE_EXTRA_INSTRUCTIONS_FOR_CROSSOVER_SAMPLE_REDO to lambda
    
    * change how render loops are passed
    
    * render functions
    
    * More lambdas
    
    * renderOscSync extraction
    
    * inline CREATE_WAVE_RENDER_FUNCTION_INSTANCE

--- src/deluge/processing/render_wave.h
+++ src/deluge/processing/render_wave.h
@@ -15,98 +15,95 @@
  * If not, see <https://www.gnu.org/licenses/>.
  */
 #include "processing/vector_rendering_function.h"
+#include "util/fixedpoint.h"
+#include "util/waves.h"
 #include <argon.hpp>
 #include <arm_neon.h>
 
-#define RENDER_OSC_SYNC(storageFunctionName, valueFunctionName, extraInstructionsForCrossoverSampleRedo,               \
-                        startRenderingASyncLabel)                                                                      \
-                                                                                                                       \
-	bool renderedASyncFromItsStartYet = false;                                                                         \
-	int32_t crossoverSampleBeforeSync;                                                                                 \
-	int32_t fadeBetweenSyncs;                                                                                          \
-                                                                                                                       \
-	/* Do a bunch of samples until we get to the next crossover sample */                                              \
-	uint32_t samplesIncludingNextCrossoverSample =                                                                     \
-	    1; /* A starting value that'll be added to. It's 1 because we want to include the 1 extra sample at the end -  \
-	          the crossover sample. */                                                                                 \
-startRenderingASyncLabel:                                                                                              \
-	uint32_t distanceTilNextCrossoverSample = -resetterPhase - (resetterPhaseIncrement >> 1);                          \
-	samplesIncludingNextCrossoverSample += (uint32_t)(distanceTilNextCrossoverSample - 1) / resetterPhaseIncrement;    \
-	bool shouldBeginNextSyncAfter = (numSamplesThisOscSyncSession >= samplesIncludingNextCrossoverSample);             \
-	int32_t numSamplesThisSyncRender = shouldBeginNextSyncAfter                                                        \
-	                                       ? samplesIncludingNextCrossoverSample                                       \
-	                                       : numSamplesThisOscSyncSession; /* Just limit it, basically. */             \
-                                                                                                                       \
-	int32_t const* const bufferEndThisSyncRender = bufferStartThisSync + numSamplesThisSyncRender;                     \
-	uint32_t phaseTemp = phase;                                                                                        \
-	int32_t* __restrict__ writePos = bufferStartThisSync;                                                              \
-                                                                                                                       \
-	storageFunctionName(valueFunctionName);                                                                            \
-                                                                                                                       \
-	/* Sort out the crossover sample at the *start* of that window we just did, if there was one. */                   \
-	if (renderedASyncFromItsStartYet) {                                                                                \
-		int32_t average = (*bufferStartThisSync >> 1) + (crossoverSampleBeforeSync >> 1);                              \
-		int32_t halfDifference = (*bufferStartThisSync >> 1) - (crossoverSampleBeforeSync >> 1);                       \
-		int32_t sineValue = getSine(fadeBetweenSyncs >> 1);                                                            \
-		*bufferStartThisSync = average + (multiply_32x32_rshift32(halfDifference, sineValue) << 1);                    \
-	}                                                                                                                  \
-                                                                                                                       \
-	if (shouldBeginNextSyncAfter) {                                                                                    \
-		/* We've just done a crossover (i.e. hit a sync point) at the end of that window, so start thinking about that \
-		 * and planning the next window. */                                                                            \
-		bufferStartThisSync += samplesIncludingNextCrossoverSample - 1;                                                \
-		crossoverSampleBeforeSync = *bufferStartThisSync;                                                              \
-		numSamplesThisOscSyncSession -= samplesIncludingNextCrossoverSample - 1;                                       \
-		extraInstructionsForCrossoverSampleRedo;                                                                       \
-                                                                                                                       \
-		resetterPhase += resetterPhaseIncrement                                                                        \
-		                 * (samplesIncludingNextCrossoverSample                                                        \
-		                    - renderedASyncFromItsStartYet); /* We want this to always show one sample late at this    \
-		                                                        point (why again?). */                                 \
-		/* The first time we get here, it won't yet be, so make it so. */                                              \
-                                                                                                                       \
-		fadeBetweenSyncs =                                                                                             \
-		    multiply_32x32_rshift32((int32_t)resetterPhase,                                                            \
-		                            resetterDivideByPhaseIncrement) /* The result of that comes out as between "-0.5   \
-		                                                               and 0.5", represented as +-(1<<14) */           \
-		    << 17; /* And this makes it "full-scale", so "1" is 1<<32. */                                              \
-		phase = multiply_32x32_rshift32(fadeBetweenSyncs, phaseIncrement) + retriggerPhase;                            \
-                                                                                                                       \
-		phase -= phaseIncrement; /* Because we're going back and redoing the last sample. */                           \
-		renderedASyncFromItsStartYet = true;                                                                           \
-		samplesIncludingNextCrossoverSample = 2; /* Make this 1 higher now, because resetterPhase's value is 1 sample  \
-		                                            later than what it "is in reality". */                             \
-		goto startRenderingASyncLabel;                                                                                 \
-	}                                                                                                                  \
-                                                                                                                       \
-	/* We're not beginning a next sync, so are not going to reset phase, so need to update (increment) it to keep it   \
-	 * valid. */                                                                                                       \
+[[gnu::always_inline]] static inline void //<
+renderOscSync(auto storageFunction, auto extraInstructionsForCrossoverSampleRedo,
+              // Params
+              uint32_t phase, uint32_t phaseIncrement, uint32_t& resetterPhase, uint32_t resetterPhaseIncrement,
+              uint32_t resetterDivideByPhaseIncrement, uint32_t retriggerPhase, int32_t numSamplesThisOscSyncSession,
+              int32_t*& bufferStartThisSync) {
+
+	bool renderedASyncFromItsStartYet = false;
+	int32_t crossoverSampleBeforeSync;
+	int32_t fadeBetweenSyncs;
+
+	/* Do a bunch of samples until we get to the next crossover sample */
+	/* A starting value that'll be added to. It's 1 because we want to include the 1 extra sample at the end - the
+	 * crossover sample. */
+	uint32_t samplesIncludingNextCrossoverSample = 1;
+startRenderingASync:
+	uint32_t distanceTilNextCrossoverSample = -resetterPhase - (resetterPhaseIncrement >> 1);
+	samplesIncludingNextCrossoverSample += (uint32_t)(distanceTilNextCrossoverSample - 1) / resetterPhaseIncrement;
+	bool shouldBeginNextSyncAfter = (numSamplesThisOscSyncSession >= samplesIncludingNextCrossoverSample);
+	int32_t numSamplesThisSyncRender = shouldBeginNextSyncAfter
+	                                       ? samplesIncludingNextCrossoverSample
+	                                       : numSamplesThisOscSyncSession; /* Just limit it, basically. */
+
+	int32_t const* const bufferEndThisSyncRender = bufferStartThisSync + numSamplesThisSyncRender;
+	uint32_t phaseTemp = phase;
+	int32_t* __restrict__ writePos = bufferStartThisSync;
+
+	storageFunction(bufferEndThisSyncRender, phaseTemp, writePos);
+
+	/* Sort out the crossover sample at the *start* of that window we just did, if there was one. */
+	if (renderedASyncFromItsStartYet) {
+		int32_t average = (*bufferStartThisSync >> 1) + (crossoverSampleBeforeSync >> 1);
+		int32_t halfDifference = (*bufferStartThisSync >> 1) - (crossoverSampleBeforeSync >> 1);
+		int32_t sineValue = getSine(fadeBetweenSyncs >> 1);
+		*bufferStartThisSync = average + (multiply_32x32_rshift32(halfDifference, sineValue) << 1);
+	}
+
+	if (shouldBeginNextSyncAfter) {
+		/* We've just done a crossover (i.e. hit a sync point) at the end of that window, so start thinking about that
+		 * and planning the next window. */
+		bufferStartThisSync += samplesIncludingNextCrossoverSample - 1;
+		crossoverSampleBeforeSync = *bufferStartThisSync;
+		numSamplesThisOscSyncSession -= samplesIncludingNextCrossoverSample - 1;
+		extraInstructionsForCrossoverSampleRedo(samplesIncludingNextCrossoverSample);
+
+		/* We want this to always show one sample late at this point (why again?). */
+		resetterPhase += resetterPhaseIncrement * (samplesIncludingNextCrossoverSample - renderedASyncFromItsStartYet);
+		/* The first time we get here, it won't yet be, so make it so. */
+
+		/* The result of that comes out as between "-0.5 and 0.5", represented as +-(1<<14) */
+		fadeBetweenSyncs = multiply_32x32_rshift32((int32_t)resetterPhase, resetterDivideByPhaseIncrement)
+		                   << 17; /* And this makes it "full-scale", so "1" is 1<<32. */
+		phase = multiply_32x32_rshift32(fadeBetweenSyncs, phaseIncrement) + retriggerPhase;
+
+		phase -= phaseIncrement; /* Because we're going back and redoing the last sample. */
+		renderedASyncFromItsStartYet = true;
+		samplesIncludingNextCrossoverSample = 2; /* Make this 1 higher now, because resetterPhase's value is 1 sample
+		                                            later than what it "is in reality". */
+		goto startRenderingASync;
+	}
+
+	/* We're not beginning a next sync, so are not going to reset phase, so need to update (increment) it to keep it
+	 * valid. */
 	phase += phaseIncrement * numSamplesThisSyncRender;
+}
 
-#define RENDER_WAVETABLE_LOOP(dontCare)                                                                                \
-	{                                                                                                                  \
-		doRenderingLoop(bufferStartThisSync, bufferEndThisSyncRender, firstCycleNumber, bandHere, phaseTemp,           \
-		                phaseIncrement, crossCycleStrength2, crossCycleStrength2Increment, kernel);                    \
-	}
+auto renderWavetableLoop(auto bufferStartThisSync, auto firstCycleNumber, auto bandHere, auto phaseIncrement,
+                         auto crossCycleStrength2, auto crossCycleStrength2Increment, auto kernel) {
+	return [&](int32_t const* const bufferEndThisSyncRender, uint32_t phaseTemp, int32_t* __restrict__ writePos) {
+		doRenderingLoop(bufferStartThisSync, bufferEndThisSyncRender, firstCycleNumber, bandHere, phaseTemp,
+		                phaseIncrement, crossCycleStrength2, crossCycleStrength2Increment, kernel);
+	};
+}
 
-#define RENDER_SINGLE_CYCLE_WAVEFORM_LOOP(dontCare)                                                                    \
-	{                                                                                                                  \
-		doRenderingLoopSingleCycle(bufferStartThisSync, bufferEndThisSyncRender, bandHere, phaseTemp, phaseIncrement,  \
-		                           kernel);                                                                            \
-	}
-
-#define WAVETABLE_EXTRA_INSTRUCTIONS_FOR_CROSSOVER_SAMPLE_REDO                                                         \
-	{ crossCycleStrength2 += crossCycleStrength2Increment * (samplesIncludingNextCrossoverSample - 1); }
-
-#define STORE_VECTOR_WAVE_FOR_ONE_SYNC(vectorValueFunctionName)                                                        \
-	{                                                                                                                  \
-		do {                                                                                                           \
-			int32x4_t valueVector;                                                                                     \
-			vectorValueFunctionName();                                                                                 \
-			vst1q_s32(writePos, valueVector);                                                                          \
-			writePos += 4;                                                                                             \
-		} while (writePos < bufferEndThisSyncRender);                                                                  \
-	}
+auto storeVectorWaveForOneSync(auto vectorValueFunction) {
+	return [&](int32_t const* const bufferEndThisSyncRender, uint32_t phaseTemp, int32_t* __restrict__ writePos) {
+		do {
+			int32x4_t valueVector;
+			vectorValueFunction();
+			vst1q_s32(writePos, valueVector);
+			writePos += 4;
+		} while (writePos < bufferEndThisSyncRender);
+	};
+}
 
 /// @note amplitude and amplitudeIncrement multiplied by two before being passed to this function
 inline int32x4_t createAmplitudeVector(int32_t amplitude, int32_t amplitudeIncrement) {
@@ -119,40 +116,3 @@ inline int32x4_t createAmplitudeVector(int32_t amplitude, int32_t amplitudeIncre
 	// TODO(@stellar-aria): investigate where the doubling comes from (likely an unshifted smmul)
 	return vshrq_n_s32(amplitudeVector, 1); // halve amplitude
 }
-
-/* Before calling, you must:
-    amplitude <<= 1;
-    amplitudeIncrement <<= 1;
-*/
-#define CREATE_WAVE_RENDER_FUNCTION_INSTANCE(thisFunctionInstanceName, vectorValueFunctionName)                        \
-                                                                                                                       \
-	__attribute__((optimize("unroll-loops"))) void thisFunctionInstanceName(                                           \
-	    const int16_t* __restrict__ table, int32_t tableSizeMagnitude, int32_t amplitude,                              \
-	    int32_t* __restrict__ outputBuffer, int32_t* bufferEnd, uint32_t phaseIncrement, uint32_t phase,               \
-	    bool applyAmplitude, uint32_t phaseToAdd, int32_t amplitudeIncrement) {                                        \
-                                                                                                                       \
-		int16x4_t const32767 = vdup_n_s16(32767);                                                                      \
-		int32_t* __restrict__ outputBufferPos = outputBuffer;                                                          \
-                                                                                                                       \
-		int32x4_t amplitudeVector = createAmplitudeVector(amplitude, amplitudeIncrement);                              \
-		int32x4_t amplitudeIncrementVector = vdupq_n_s32(amplitudeIncrement << 1);                                     \
-                                                                                                                       \
-		uint32_t phaseTemp = phase;                                                                                    \
-                                                                                                                       \
-		do {                                                                                                           \
-			int32x4_t valueVector;                                                                                     \
-                                                                                                                       \
-			vectorValueFunctionName();                                                                                 \
-                                                                                                                       \
-			if (applyAmplitude) {                                                                                      \
-				int32x4_t existingDataInBuffer = vld1q_s32(outputBufferPos);                                           \
-				valueVector = vqdmulhq_s32(amplitudeVector, valueVector);                                              \
-				amplitudeVector = vaddq_s32(amplitudeVector, amplitudeIncrementVector);                                \
-				valueVector = vaddq_s32(valueVector, existingDataInBuffer);                                            \
-			}                                                                                                          \
-                                                                                                                       \
-			vst1q_s32(outputBufferPos, valueVector);                                                                   \
-                                                                                                                       \
-			outputBufferPos += 4;                                                                                      \
-		} while (outputBufferPos < bufferEnd);                                                                         \
-	};
