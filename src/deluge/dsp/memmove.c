5b3a2f727c30466e0d74a20e899a6714a2aa107f
Katherine Whitlock <kate@skylinesynths.nyc>
2024-11-28 15:19:09 -0500

    Fix comments

--- src/deluge/dsp/memmove.c
+++ src/deluge/dsp/memmove.c
@@ -2,47 +2,37 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-
-static inline bool isAligned(const void* __restrict__ pointer, size_t byte_count) {
-	return (uintptr_t)pointer % byte_count == 0;
-}
+#include <stdlib.h>
 
 void* memmove(void* dst, const void* src, size_t len) {
-	if ((intptr_t)dst - (intptr_t)src >= (ptrdiff_t)len) {
+	ptrdiff_t result;
+	asm("sub %0, %1, %2" : "=r"(result) : "r"(dst), "r"(src));
+	if (abs(result) >= (ptrdiff_t)len) {
 		/* destination not in source data, so can safely use memcpy */
 		return memcpy(dst, src, len);
 	}
 
 	// For the remaining cases, the destination range is _guaranteed_ to include the source range
-
-	if (isAligned(src, 4) && isAligned(dst, 4) && len % 4 == 0) {
-		/* copy backwards... */
-		const uint32_t* end = (uint32_t*)dst;
-		const uint32_t* s = (const uint32_t*)src + (len / 4);
-		uint32_t* d = (uint32_t*)dst + (len / 4);
-		while (d != end) {
-			*--d = *--s;
-		}
-		return dst;
-	}
-
-	if (isAligned(src, 2) && isAligned(dst, 2) && len % 2 == 0) {
-		/* copy backwards... */
-		const uint16_t* end = (uint16_t*)dst;
-		const uint16_t* s = (const uint16_t*)src + (len / 2);
-		uint16_t* d = (uint16_t*)dst + (len / 2);
-		while (d != end) {
-			*--d = *--s;
-		}
-		return dst;
-	}
-
-	/* copy backwards... */
-	const char* end = (char*)dst;
-	const char* s = (const char*)src + len;
-	char* d = (char*)dst + len;
-	while (d != end) {
+	// single byte to do
+	const char* s = (const char*)(src + len);
+	char* d = (char*)(dst + len);
+	if (len % 2) {
 		*--d = *--s;
 	}
+
+	// halfword to do
+	const uint16_t* s_16 = (const uint16_t*)s;
+	uint16_t* d_16 = (uint16_t*)d;
+	if (len % 4) {
+		*--d_16 = *--s_16;
+	}
+
+	// copy backwards by word...
+	const uint32_t* s_32 = (const uint32_t*)s_16;
+	uint32_t* d_32 = (uint32_t*)d_16;
+	while ((intptr_t)d_32 > (intptr_t)dst) {
+		*--d_32 = *--s_32;
+	}
+
 	return dst;
 }
