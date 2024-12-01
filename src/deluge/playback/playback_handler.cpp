2cd7ca0169801beb3b77b9f07f710718dda3885a
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2024-11-30 21:30:56 -0500

    Bugfix Keyboard Playback Crash (#2993)
    Fixed playback crash bug in keyboard view

--- src/deluge/playback/playback_handler.cpp
+++ src/deluge/playback/playback_handler.cpp
@@ -369,14 +369,13 @@ void PlaybackHandler::setupPlaybackUsingInternalClock(int32_t buttonPressLatency
 
 	int32_t navSys;
 	// we might not need this but just always grab it for simplicity
-	if (rootUI) {
-		if (auto* timelineView = rootUI->toTimelineView()) {
-			navSys = timelineView->getNavSysId();
-		}
+	if (rootUI && rootUI->toTimelineView()) {
+		navSys = ((TimelineView*)rootUI)->getNavSysId();
 	}
 	else {
 		navSys = NAVIGATION_CLIP; // Keyboard view will cause this case
 	}
+
 	// playback restart command - restart should always restart, regardless of what else you're doing. playback
 	// restart command follows play button logic
 	if (restartingPlaybackAtBeginning) {
