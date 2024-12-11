6922e501c7eec48ab7da0df943b73e2dff36a8ee
Katherine Whitlock <kate@skylinesynths.nyc>
2024-12-10 21:31:46 -0500

    Add temporary multiload/store intrinsics

--- src/memmove.c
+++ src/memmove.c
@@ -5,7 +5,35 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-void* my_memmove(void* dst, const void* src, size_t len) {
+[[gnu::always_inline]] inline uint8x16x2_t vld1q_u8_x2(unsigned char const* ptr) {
+	uint8x16x2_t output;
+	output.val[0] = vld1q_u8(ptr);
+	output.val[1] = vld1q_u8(ptr + 16);
+	return output;
+}
+
+[[gnu::always_inline]] inline uint8x16x4_t vld1q_u8_x4(unsigned char const* ptr) {
+	uint8x16x4_t output;
+	output.val[0] = vld1q_u8(ptr);
+	output.val[1] = vld1q_u8(ptr + 16);
+	output.val[2] = vld1q_u8(ptr + 32);
+	output.val[3] = vld1q_u8(ptr + 48);
+	return output;
+}
+
+[[gnu::always_inline]] inline void vst1q_u8_x2(unsigned char* ptr, uint8x16x2_t output) {
+	vst1q_u8(ptr, output.val[0]);
+	vst1q_u8(ptr + 16, output.val[1]);
+}
+
+[[gnu::always_inline]] inline void vst1q_u8_x4(unsigned char* ptr, uint8x16x4_t output) {
+	vst1q_u8(ptr, output.val[0]);
+	vst1q_u8(ptr + 16, output.val[1]);
+	vst1q_u8(ptr + 32, output.val[2]);
+	vst1q_u8(ptr + 48, output.val[3]);
+}
+
+void* memmove(void* dst, const void* src, size_t len) {
 	ptrdiff_t result;
 	asm("sub %0, %1, %2" : "=r"(result) : "r"(dst), "r"(src));
 	if (abs(result) >= (ptrdiff_t)len) {
@@ -51,21 +79,19 @@ void* my_memmove(void* dst, const void* src, size_t len) {
 		vst1q_u8(d, vld1q_u8(s));
 	}
 
-	// quadword x2
 	if (len % 64) {
 		s -= 32;
 		d -= 32;
 		vst1q_u8_x2(d, vld1q_u8_x2(s));
 	}
 
-	// quadword x4
 	if (len % 128) {
 		s -= 64;
 		d -= 64;
 		vst1q_u8_x4(d, vld1q_u8_x4(s));
 	}
 
-	// quadword x8
+	// max number of quadwords is 16
 	while ((intptr_t)d > (intptr_t)dst) {
 		s -= 64;
 		uint8x16x4_t ld1 = vld1q_u8_x4(s);
