62cbbb2b04dfd0d4c2e09ef5eb5a2090af79cdcb
Katherine Whitlock <kate@skylinesynths.nyc>
2024-12-21 11:17:07 -0500

    Fix alignment issue for interpolator (#3110)
    * Fix bug with auditioning INPUT osc type
    
    * Simplify interpolator buffer access
    
    * format

--- src/deluge/dsp/interpolate/interpolate.h
+++ src/deluge/dsp/interpolate/interpolate.h
@@ -1,50 +1,45 @@
 #pragma once
 #include "definitions_cxx.hpp"
 #include "deluge/dsp/stereo_sample.h"
-#include <arm_neon_shim.h>
 #include <array>
 #include <cstdint>
 
 extern const int16_t windowedSincKernel[][17][16];
 
 namespace deluge::dsp {
-class Interpolator {
-	using buffer_t = std::array<int16x4_t, kInterpolationMaxNumSamples / 4>;
+struct Interpolator {
+	using buffer_t = std::array<int16_t, kInterpolationMaxNumSamples>;
 
-public:
 	Interpolator() = default;
 	StereoSample interpolate(size_t channels, int32_t whichKernel, uint32_t oscPos);
 	StereoSample interpolateLinear(size_t channels, uint32_t oscPos);
 
-	constexpr int16_t& bufferL(size_t idx) { return interpolation_buffer_l_[idx / 4][idx % 4]; }
-	constexpr int16_t& bufferR(size_t idx) { return interpolation_buffer_r_[idx / 4][idx % 4]; }
-
 	constexpr void pushL(int16_t value) {
 #pragma unroll
 		for (int32_t i = kInterpolationMaxNumSamples - 1; i >= 1; i--) {
-			bufferL(i) = bufferL(i - 1);
+			buffer_l[i] = buffer_l[i - 1];
 		}
-		interpolation_buffer_l_[0][0] = value;
+		buffer_l[0] = value;
 	}
 
 	inline void pushR(int16_t value) {
 #pragma unroll
 		for (int32_t i = kInterpolationMaxNumSamples - 1; i >= 1; i--) {
-			bufferR(i) = bufferR(i - 1);
+			buffer_r[i] = buffer_r[i - 1];
 		}
-		interpolation_buffer_r_[0][0] = value;
+		buffer_r[0] = value;
 	}
 
 	constexpr void jumpForward(size_t num_samples) {
 #pragma unroll
 		for (int32_t i = kInterpolationMaxNumSamples - 1; i >= num_samples; i--) {
-			bufferL(i) = bufferL(i - num_samples);
-			bufferR(i) = bufferR(i - num_samples);
+			buffer_l[i] = buffer_l[i - num_samples];
+			buffer_r[i] = buffer_r[i - num_samples];
 		}
 	}
 
-private:
-	buffer_t interpolation_buffer_l_{};
-	buffer_t interpolation_buffer_r_{};
+	// These are the state buffers
+	buffer_t buffer_l;
+	buffer_t buffer_r;
 };
 } // namespace deluge::dsp
