9aac6e22ac7b34f3c30b6b0ece8c16365bca5a67
Katherine Whitlock <kate@skylinesynths.nyc>
2024-12-31 12:04:15 -0500

    Add SDRAM allocator

--- src/deluge/memory/sdram_allocator.h
+++ src/deluge/memory/sdram_allocator.h
@@ -10,24 +10,26 @@ namespace deluge::memory {
 /**
  * @brief A simple GMA wrapper that conforms to the C++ Allocator trait spec
  * (see: https://en.cppreference.com/w/cpp/named_req/Allocator)
+ * @note The sdram_allocator allows allocation to _either_ the specially reserved "external" 8MiB region of SDRAM _OR_
+ *       the remaining 56MiB "stealable" region of SDRAM
  *
  * @tparam T The type to allocate for
  */
 template <typename T>
-class fast_allocator {
+class sdram_allocator {
 public:
 	using value_type = T;
 
-	constexpr fast_allocator() noexcept = default;
+	constexpr sdram_allocator() noexcept = default;
 
 	template <typename U>
-	constexpr fast_allocator(const fast_allocator<U>&) noexcept {};
+	constexpr sdram_allocator(const sdram_allocator<U>&) noexcept {};
 
 	[[nodiscard]] T* allocate(std::size_t n) noexcept(false) {
 		if (n == 0) {
 			return nullptr;
 		}
-		void* addr = GeneralMemoryAllocator::get().allocMaxSpeed(n * sizeof(T));
+		void* addr = GeneralMemoryAllocator::get().allocLowSpeed(n * sizeof(T));
 		if (addr == nullptr) [[unlikely]] {
 			throw deluge::exception::BAD_ALLOC;
 		}
@@ -37,12 +39,12 @@ public:
 	void deallocate(T* p, std::size_t n) { GeneralMemoryAllocator::get().dealloc(p); }
 
 	template <typename U>
-	bool operator==(const fast_allocator<U>& o) {
+	bool operator==(const sdram_allocator<U>& o) {
 		return true;
 	}
 
 	template <typename U>
-	bool operator!=(const fast_allocator<U>& o) {
+	bool operator!=(const sdram_allocator<U>& o) {
 		return false;
 	}
 };
