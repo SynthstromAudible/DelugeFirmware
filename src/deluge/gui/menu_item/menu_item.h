deb9dd86f84ba0d9ae01d149136a91b7f2aa9325
Raúl Muñoz <raul@mothership.com>
2025-01-08 12:22:53 +0100

    Prefer the 7SEG string for the horizontal menu titles

--- src/deluge/gui/menu_item/menu_item.h
+++ src/deluge/gui/menu_item/menu_item.h
@@ -244,7 +244,7 @@ public:
 	/// @brief Get the name for use on horizontal menus.
 	///
 	/// By default this redirects to getName(), but can be overriden.
-	virtual void getColumnLabel(StringBuf& label) { label.append(getName().data()); }
+	virtual void getColumnLabel(StringBuf& label) { label.append(deluge::l10n::getView(deluge::l10n::built_in::seven_segment, name).data()); }
 
 	/// @brief Check if this MenuItem should show up in a containing deluge::gui::menu_item::Submenu.
 	///
