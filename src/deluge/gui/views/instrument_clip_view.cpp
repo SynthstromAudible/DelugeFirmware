fab7b2375783b814adf4bb7760701a0f00a18feb
Katherine Whitlock <kate@skylinesynths.nyc>
2025-01-15 18:50:47 -0500

    Maybe fix ghost notes issue?

--- src/deluge/gui/views/instrument_clip_view.cpp
+++ src/deluge/gui/views/instrument_clip_view.cpp
@@ -4668,12 +4668,10 @@ void InstrumentClipView::recordNoteOnEarly(int32_t velocity, int32_t yDisplay, I
 	else {
 		// NoteRow is allowed to be NULL in this case.
 		int32_t yNote = getCurrentInstrumentClip()->getYNoteFromYDisplay(yDisplay, currentSong);
-		((MelodicInstrument*)instrument)
-		    ->earlyNotes.emplace(yNote,
-		                         MelodicInstrument::EarlyNoteInfo{
-		                             instrument->defaultVelocity,
-		                             getCurrentInstrumentClip()->allowNoteTails(modelStackWithNoteRowOnCurrentClip),
-		                         });
+		static_cast<MelodicInstrument*>(instrument)->earlyNotes[yNote] = {
+		    .velocity = instrument->defaultVelocity,
+		    .still_active = getCurrentInstrumentClip()->allowNoteTails(modelStackWithNoteRowOnCurrentClip),
+		};
 	}
 }
 
