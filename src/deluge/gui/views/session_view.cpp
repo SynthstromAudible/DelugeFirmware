41c66c9ff9b6d1d7e210d3208f07eb88160bf6ba
Mark Adams <mark.adams@queensu.ca>
2025-01-26 16:36:58 -0500

    reset the string to the  new value (#3315)

--- src/deluge/gui/views/session_view.cpp
+++ src/deluge/gui/views/session_view.cpp
@@ -3596,6 +3596,7 @@ void SessionView::copyClipName(Clip* source, Clip* target, Output* targetOutput)
 	while (targetOutput->getClipFromName(&newNameString) != nullptr) {
 		newName.truncate(end);
 		newName.appendInt(counter++);
+		newNameString.set(newName.data());
 	}
 	target->name.set(newName.data());
 }
