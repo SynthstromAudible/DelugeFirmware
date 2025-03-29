53e6d8e32ef7fe79025f8a3ddb44fc8271e99ff0
Katherine Whitlock <kate@skylinesynths.nyc>
2025-03-29 13:24:12 -0400

    Make integers in tests floats

--- tests/qemu/spec/fixedpoint_spec.cpp
+++ tests/qemu/spec/fixedpoint_spec.cpp
@@ -59,7 +59,7 @@ describe fixedpoint("FixedPoint", ${
 	context("==", _ {
 		it("the original integer", _ {
 			FixedPoint<16> fp{42};
-			expect(fp).to_equal(42);
+			expect(fp).to_equal(42.f);
 		});
 
 		it("the original float", _ {
@@ -118,7 +118,7 @@ describe fixedpoint("FixedPoint", ${
 		it("adds another FixedPoint with an integral value", _ {
 			FixedPoint<16> fp1{42};
 			FixedPoint<16> fp2{2};
-			expect(fp1 + fp2).to_equal(44);
+			expect(fp1 + fp2).to_equal(44.f);
 		});
 
 		it("adds another FixedPoint with a fractional value", _ {
@@ -132,7 +132,7 @@ describe fixedpoint("FixedPoint", ${
 		it("subtracts another FixedPoint with an integral value", _ {
 			FixedPoint<16> fp1{42};
 			FixedPoint<16> fp2{2};
-			expect(fp1 - fp2).to_equal(40);
+			expect(fp1 - fp2).to_equal(40.f);
 		});
 
 		it("subtracts another FixedPoint with a fractional value", _ {
@@ -148,13 +148,13 @@ describe fixedpoint("FixedPoint", ${
 				it("multiplies by another FixedPoint with an integral value", _ {
 					FixedPoint<16> fp1{42};
 					FixedPoint<16> fp2{2};
-					expect(fp1 * fp2).to_equal(84);
+					expect(fp1 * fp2).to_equal(84.f);
 				});
 
 				it("multiplies by another FixedPoint with a fractional value", _ {
 					FixedPoint<16> fp1{42};
 					FixedPoint<16> fp2{2.5f};
-					expect(fp1 * fp2).to_equal(105);
+					expect(fp1 * fp2).to_equal(105.f);
 				});
 			});
 
@@ -162,13 +162,13 @@ describe fixedpoint("FixedPoint", ${
 				it("multiplies by another FixedPoint with an integral value", _ {
 					FixedPoint<16> fp1{42};
 					FixedPoint<28> fp2{2};
-					expect(fp1 * fp2).to_equal(84);
+					expect(fp1 * fp2).to_equal(84.f);
 				});
 
 				it("multiplies by another FixedPoint with a fractional value", _ {
 					FixedPoint<16> fp1{42};
 					FixedPoint<28> fp2{2.5f};
-					expect(fp1 * fp2).to_equal(105);
+					expect(fp1 * fp2).to_equal(105.f);
 				});
 			});
 		});
@@ -178,13 +178,13 @@ describe fixedpoint("FixedPoint", ${
 				it("multiplies by another FixedPoint with an integral value", _ {
 					FixedPointAccurate<16> fp1{42};
 					FixedPointAccurate<16> fp2{2};
-					expect(fp1 * fp2).to_equal(84);
+					expect(fp1 * fp2).to_equal(84.f);
 				});
 
 				it("multiplies by another FixedPoint with a fractional value", _ {
 					FixedPointAccurate<16> fp1{42};
 					FixedPointAccurate<16> fp2{2.5f};
-					expect(fp1 * fp2).to_equal(105);
+					expect(fp1 * fp2).to_equal(105.f);
 				});
 			});
 
@@ -192,13 +192,13 @@ describe fixedpoint("FixedPoint", ${
 				it("multiplies by another FixedPoint with an integral value", _ {
 					FixedPointAccurate<16> fp1{42};
 					FixedPointAccurate<28> fp2{2};
-					expect(fp1 * fp2).to_equal(84);
+					expect(fp1 * fp2).to_equal(84.f);
 				});
 
 				it("multiplies by another FixedPoint with a fractional value", _ {
 					FixedPointAccurate<16> fp1{42};
 					FixedPointAccurate<28> fp2{2.5f};
-					expect(fp1 * fp2).to_equal(105);
+					expect(fp1 * fp2).to_equal(105.f);
 				});
 			});
 		});
@@ -208,7 +208,7 @@ describe fixedpoint("FixedPoint", ${
 		it("divides by another FixedPoint with an integral value", _ {
 			FixedPoint<16> fp1{42};
 			FixedPoint<16> fp2{2};
-			expect(fp1 / fp2).to_equal(21);
+			expect(fp1 / fp2).to_equal(21.f);
 		});
 
 		it("divides by another FixedPoint with a fractional value", _ {
@@ -224,7 +224,7 @@ describe fixedpoint("FixedPoint", ${
 				FixedPoint<16> fp1{42};
 				FixedPoint<17> fp2{2};
 				FixedPoint<17> fp3{3};
-				expect(fp1.MultiplyAdd(fp2, fp3)).to_equal(48);
+				expect(fp1.MultiplyAdd(fp2, fp3)).to_equal(48.f);
 			});
 
 			it("multiplies and adds another FixedPoint with a fractional value", _ {
