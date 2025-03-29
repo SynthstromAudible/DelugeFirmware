e62c5625490c35f59d9e2ac40900674aa542ee94
mgabor <9047995+mgabor3141@users.noreply.github.com>
2025-03-29 18:47:19 +0100

    Port GitHub pages content

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -6,11 +6,17 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Deluge Community',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/SynthstromAudible/DelugeFirmware',
+        discord: 'https://discord.gg/s2MnkFqZgj',
+        patreon: 'https://www.patreon.com/Synthstrom'
 			},
 			sidebar: [
+        {
+          label: 'Downloads',
+          slug: 'downloads'
+        },
 				{
 					label: 'Guides',
 					items: [
