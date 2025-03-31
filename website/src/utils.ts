7c48ea15634636a6e1a3440f76e2973daa59a2b4
mgabor <9047995+mgabor3141@users.noreply.github.com>
2025-03-31 02:47:09 +0200

    Introduce eslint violation to test lint workflow

--- website/src/utils.ts
+++ website/src/utils.ts
@@ -1,5 +1,8 @@
 import config from "../astro.config.mjs"
 
 export const withBase = (path: string) => {
+  if (true)
+    console.log("I hope nobody notices")
+
   return path.startsWith("/") ? `${config.base}${path}` : path
 }
