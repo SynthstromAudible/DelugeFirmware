86c9d4e7b320e6a056e44867d31095773a810bbc
mgabor <9047995+mgabor3141@users.noreply.github.com>
2025-03-31 02:55:16 +0200

    Revert "Introduce eslint violation to test lint workflow"
    This reverts commit 7c48ea15634636a6e1a3440f76e2973daa59a2b4.

--- website/src/utils.ts
+++ website/src/utils.ts
@@ -1,8 +1,5 @@
 import config from "../astro.config.mjs"
 
 export const withBase = (path: string) => {
-  if (true)
-    console.log("I hope nobody notices")
-
   return path.startsWith("/") ? `${config.base}${path}` : path
 }
