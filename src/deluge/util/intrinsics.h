1a59a68757e9c351fdbe9f3fca3372d63c4c8d45
m-m-adams <mark.adams@queensu.ca>
2025-04-06 02:01:18 -0400

    make them good again

--- src/deluge/util/intrinsics.h
+++ src/deluge/util/intrinsics.h
@@ -43,71 +43,80 @@ constexpr bool kCompilerClang = COMPILER_CLANG;
 // This multiplies two numbers in signed Q31 fixed point as if they were q32, so the return value is half what it should
 // be. Use this when several corrective shifts can be accumulated and then combined
 [[gnu::always_inline]] static constexpr int32_t multiply_32x32_rshift32(int32_t a, int32_t b) {
-	if constexpr (std::is_constant_evaluated() || !ARMv7a || kCompilerClang) {
-		return (int32_t)(((int64_t)a * b) >> 32);
+	if !consteval {
+		if constexpr (ARMv7a || !kCompilerClang) {
+			int32_t out;
+			asm("smmul %0, %1, %2" : "=r"(out) : "r"(a), "r"(b));
+			return out;
+		}
 	}
+
+	return (int32_t)(((int64_t)a * b) >> 32);
 }
 
 // This multiplies two numbers in signed Q31 fixed point and rounds the result
 [[gnu::always_inline]] static constexpr int32_t multiply_32x32_rshift32_rounded(int32_t a, int32_t b) {
-	if constexpr (std::is_constant_evaluated() || !ARMv7a || kCompilerClang) {
-		return (int32_t)(((int64_t)a * b + 0x80000000) >> 32);
+	if !consteval {
+		if constexpr (ARMv7a || !kCompilerClang) {
+			int32_t out;
+			asm("smmulr %0, %1, %2" : "=r"(out) : "r"(a), "r"(b));
+			return out;
+		}
 	}
-	else {
-		int32_t out;
-		asm("smmulr %0, %1, %2" : "=r"(out) : "r"(a), "r"(b));
-		return out;
-	}
+
+	return (int32_t)(((int64_t)a * b + 0x80000000) >> 32);
 }
 
 /// @brief This multiplies two numbers in signed Q31 fixed point, returning the result in Q31.
 /// @deprecated This function is deprecated. Use FixedPoint<31>::multiply instead.
-/// @note deprecated because the two branches perform different operations and cannot be relied on for testing
 [[gnu::always_inline]] static constexpr int32_t q31_mult(int32_t a, int32_t b) {
-	if constexpr (std::is_constant_evaluated() || !ARMv7a) {
-		return (int32_t)(((int64_t)a * b) >> 31);
+	if !consteval {
+		if constexpr (ARMv7a) {
+			return multiply_32x32_rshift32(a, b) << 1;
+		}
 	}
-	else {
-		return multiply_32x32_rshift32(a, b) << 1;
-	}
+	return (int32_t)(((int64_t)a * b) >> 32) << 1;
 }
 
 // Multiplies A and B, adds to sum, and returns output
 [[gnu::always_inline]] static constexpr int32_t multiply_accumulate_32x32_rshift32_rounded(int32_t sum, int32_t a,
                                                                                            int32_t b) {
-	if constexpr (std::is_constant_evaluated() || !ARMv7a || kCompilerClang) {
-		return (int32_t)(((((int64_t)sum) << 32) + ((int64_t)a * b) + 0x80000000) >> 32);
+	if !consteval {
+		if constexpr (ARMv7a && !kCompilerClang) {
+			int32_t out;
+			asm("smmlar %0, %1, %2, %3" : "=r"(out) : "r"(a), "r"(b), "r"(sum));
+			return out;
+		}
 	}
-	else {
-		int32_t out;
-		asm("smmlar %0, %1, %2, %3" : "=r"(out) : "r"(a), "r"(b), "r"(sum));
-		return out;
-	}
+
+	return (int32_t)(((((int64_t)sum) << 32) + ((int64_t)a * b) + 0x80000000) >> 32);
 }
 
 // Multiplies A and B, adds to sum, and returns output
 [[gnu::always_inline]] static constexpr int32_t multiply_accumulate_32x32_rshift32(int32_t sum, int32_t a, int32_t b) {
-	if constexpr (std::is_constant_evaluated() || !ARMv7a || kCompilerClang) {
-		return (int32_t)(((((int64_t)sum) << 32) + ((int64_t)a * b)) >> 32);
+	if !consteval {
+		if constexpr (ARMv7a && !kCompilerClang) {
+			int32_t out;
+			asm("smmla %0, %1, %2, %3" : "=r"(out) : "r"(a), "r"(b), "r"(sum));
+			return out;
+		}
 	}
-	else {
-		int32_t out;
-		asm("smmla %0, %1, %2, %3" : "=r"(out) : "r"(a), "r"(b), "r"(sum));
-		return out;
-	}
+
+	return (int32_t)(((((int64_t)sum) << 32) + ((int64_t)a * b)) >> 32);
 }
 
 // Multiplies A and B, subtracts from sum, and returns output
 [[gnu::always_inline]] static constexpr int32_t multiply_subtract_32x32_rshift32_rounded(int32_t sum, int32_t a,
                                                                                          int32_t b) {
-	if constexpr (std::is_constant_evaluated() || !ARMv7a || kCompilerClang) {
-		return (int32_t)((((((int64_t)sum) << 32) - ((int64_t)a * b)) + 0x80000000) >> 32);
+	if !consteval {
+		if constexpr (ARMv7a && !kCompilerClang) {
+			int32_t out;
+			asm("smmlsr %0, %1, %2, %3" : "=r"(out) : "r"(a), "r"(b), "r"(sum));
+			return out;
+		}
 	}
-	else {
-		int32_t out;
-		asm("smmlsr %0, %1, %2, %3" : "=r"(out) : "r"(a), "r"(b), "r"(sum));
-		return out;
-	}
+
+	return (int32_t)((((((int64_t)sum) << 32) - ((int64_t)a * b)) + 0x80000000) >> 32);
 }
 
 // computes limit((val >> rshift), 2**bits)
@@ -125,62 +134,64 @@ template <size_t bits>
 requires(bits <= 32)
 [[gnu::always_inline]] static constexpr uint32_t unsigned_saturate(uint32_t val) {
 	/// https://developer.arm.com/documentation/ddi0406/c/Application-Level-Architecture/Instruction-Details/Alphabetical-list-of-instructions/USAT
-	if constexpr (std::is_constant_evaluated() || !ARMv7a) {
-		return std::clamp<uint32_t>(val, 0u, (1uL << bits) - 1);
+	if !consteval {
+		if constexpr (ARMv7a) {
+			int32_t out;
+			asm("usat %0, %1, %2" : "=r"(out) : "I"(bits), "r"(val));
+			return out;
+		}
 	}
-	else {
-		uint32_t out;
-		asm("usat %0, %1, %2" : "=r"(out) : "I"(bits), "r"(val));
-		return out;
-	}
+
+	return std::clamp<uint32_t>(val, 0u, (1uL << bits) - 1);
 }
 
 template <size_t shift, size_t bits = 32>
 requires(shift < 32 && bits <= 32)
 [[gnu::always_inline]] static constexpr int32_t shift_left_saturate(int32_t val) {
-	if constexpr (std::is_constant_evaluated() || !ARMv7a) {
-		return std::clamp<int64_t>((int64_t)val << shift, -(1LL << (bits - 1)), (1LL << (bits - 1)) - 1);
+	if !consteval {
+		if constexpr (ARMv7a) {
+			int32_t out;
+			asm("ssat %0, %1, %2, LSL %3" : "=r"(out) : "I"(bits), "r"(val), "I"(shift));
+			return out;
+		}
 	}
-	else {
-		int32_t out;
-		asm("ssat %0, %1, %2, LSL %3" : "=r"(out) : "I"(bits), "r"(val), "I"(shift));
-		return out;
-	}
+
+	return std::clamp<int64_t>((int64_t)val << shift, -(1LL << (bits - 1)), (1LL << (bits - 1)) - 1);
 }
 
 template <size_t shift, size_t bits = 32>
 requires(shift < 32 && bits <= 32)
 [[gnu::always_inline]] static constexpr uint32_t shift_left_saturate(uint32_t val) {
-	if constexpr (std::is_constant_evaluated() || !ARMv7a) {
-		return std::clamp<uint64_t>((uint64_t)val << shift, 0u, (1uLL << bits) - 1);
+	if !consteval {
+		if constexpr (ARMv7a) {
+			int32_t out;
+			asm("usat %0, %1, %2, LSL %3" : "=r"(out) : "I"(bits), "r"(val), "I"(shift));
+			return out;
+		}
 	}
-	else {
-		uint32_t out;
-		asm("usat %0, %1, %2, LSL %3" : "=r"(out) : "I"(bits), "r"(val), "I"(shift));
-		return out;
-	}
+	return std::clamp<uint64_t>((uint64_t)val << shift, 0u, (1uLL << bits) - 1);
 }
 
 [[gnu::always_inline]] static constexpr int32_t add_saturate(int32_t a, int32_t b) {
-	if constexpr (std::is_constant_evaluated() || !ARMv7a) {
-		return std::clamp<int64_t>(a + b, -(1LL << 31), (1LL << 31) - 1);
+	if !consteval {
+		if constexpr (ARMv7a) {
+			int32_t out;
+			asm("qadd %0, %1, %2" : "=r"(out) : "r"(a), "r"(b));
+			return out;
+		}
 	}
-	else {
-		int32_t out;
-		asm("qadd %0, %1, %2" : "=r"(out) : "r"(a), "r"(b));
-		return out;
-	}
+	return std::clamp<int64_t>(a + b, -(1LL << 31), (1LL << 31) - 1);
 }
 
 [[gnu::always_inline]] static constexpr int32_t subtract_saturate(int32_t a, int32_t b) {
-	if constexpr (std::is_constant_evaluated() || !ARMv7a) {
-		return std::clamp<int64_t>(a - b, -(1LL << 31), (1LL << 31) - 1);
+	if !consteval {
+		if constexpr (ARMv7a) {
+			int32_t out;
+			asm("qsub %0, %1, %2" : "=r"(out) : "r"(a), "r"(b));
+			return out;
+		}
 	}
-	else {
-		int32_t out;
-		asm("qsub %0, %1, %2" : "=r"(out) : "r"(a), "r"(b));
-		return out;
-	}
+	return std::clamp<int64_t>(a - b, -(1LL << 31), (1LL << 31) - 1);
 }
 
 /// clz removed as std::countl_zero reduces to the same instruction
