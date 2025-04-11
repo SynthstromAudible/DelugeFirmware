69f4494ba062aa66c25736834d7ab45a60fef50a
Katherine Whitlock <kate@skylinesynths.nyc>
2025-04-11 11:51:15 -0400

    Oscillators

---
+++ src/deluge/dsp/oscillators/legacy/oscillator.h
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2025 Katherine Whitlock
+ *
+ * This file is part of The Synthstrom Audible Deluge Firmware.
+ *
+ * The Synthstrom Audible Deluge Firmware is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "dsp/core/generator.h"
+#include "dsp/core/periodic.h"
+#include "dsp/core/types.h"
+#include <cstddef>
+#include <cstdint>
+#include <limits>
+
+namespace deluge::dsp::oscillator {
+class LegacyOscillator : public SIMDGenerator<fixed_point::Sample> {
+	Periodic<Argon<uint32_t>> periodic_component_; ///< The periodic component of the oscillator
+	/// @note this can't be inherited due to being a SIMDGenerator<uint32_t>, which makes the return types for derived
+	/// class' render() functions non-matching
+public:
+	constexpr LegacyOscillator() = default; ///< Default constructor
+	constexpr LegacyOscillator(uint32_t phase, uint32_t increment) { setPhaseAndIncrement(phase, increment); }
+
+	/// @brief Set the phase of the oscillator.
+	/// @param phase The new phase value to set.
+	constexpr void setPhaseAndIncrement(uint32_t phase, uint32_t increment) {
+		periodic_component_.setPhase(Argon{phase}.MultiplyAdd(Argon<uint32_t>{1U, 2U, 3U, 4U}, increment));
+		periodic_component_.setPhaseIncrement(increment);
+	};
+
+	constexpr void setPhase(uint32_t phase) {
+		periodic_component_.setPhase(Argon{phase}.MultiplyAdd(Argon<uint32_t>{1U, 2U, 3U, 4U}, getPhaseIncrement()));
+	}
+
+	/// @brief Advance the phase of the oscillator by one step.
+	constexpr void advance() { periodic_component_.advance(); }
+
+	/// @brief Get the current phase of the oscillator.
+	/// @return The current phase value.
+	[[nodiscard]] constexpr Argon<uint32_t> getPhase() const { return periodic_component_.getPhase(); }
+	[[nodiscard]] constexpr uint32_t getPhaseIncrement() const { return periodic_component_.getPhaseIncrement(); }
+
+	/// @brief Get the number of samples remaining until the next reset of the oscillator.
+	/// @param offset An optional offset to include in the calculation, commonly used for phase adjustments.
+	/// @return An Argon object containing the number of samples remaining until the next reset.
+	[[nodiscard]] constexpr Argon<uint32_t> samplesRemaining() const {
+		uint32_t increment = getPhaseIncrement();
+		Argon<uint32_t> phase = std::numeric_limits<uint32_t>::max() - getPhase(); // Remaining phase
+		return phase * Argon{increment}.ReciprocalEstimate();
+	}
+};
+
+class SimpleOscillatorFor final : public LegacyOscillator {
+	using FunctionType = Argon<q31_t> (*)(Argon<uint32_t>);
+	FunctionType func_;
+
+public:
+	SimpleOscillatorFor(FunctionType func) : func_(func) {}
+	Argon<q31_t> render() final {
+		Argon<q31_t> output = func_(getPhase());
+		this->advance();
+		return output;
+	}
+};
+
+class PWMOscillator : public SIMDGenerator<fixed_point::Sample> {
+	uint32_t pulse_width_ = 0x80000000; ///< The width of the pulse in the PWM waveform
+public:
+	/// @brief Get the current phase width of the PWM oscillator.
+	[[nodiscard]] constexpr uint32_t getPulseWidth() const { return pulse_width_; }
+
+	/// @brief Set the phase width of the PWM oscillator.
+	void setPulseWidth(uint32_t width) { pulse_width_ = width; }
+};
+} // namespace deluge::dsp::oscillator
---
+++ src/deluge/dsp/oscillators/legacy/table_oscillator.h
@@ -0,0 +1,100 @@
+
+/**
+ * Copyright (c) 2025 Katherine Whitlock
+ *
+ * This file is part of The Synthstrom Audible Deluge Firmware.
+ *
+ * The Synthstrom Audible Deluge Firmware is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "dsp/core/generator.h"
+#include "oscillator.h"
+#include "util/fixedpoint.h"
+#include <argon.hpp>
+#include <cstdint>
+#include <limits>
+#include <span>
+
+namespace deluge::dsp::oscillator {
+
+struct Table {
+	const int16_t* data;
+	int32_t size_magnitude;
+	Table(const int16_t* table, int32_t size_magnitude) : data{table}, size_magnitude{size_magnitude} {}
+};
+
+/// @brief A base class for oscillators that use a lookup table for waveform generation.
+/// @details This class provides a mechanism to generate waveforms using a pre-defined table of values.
+/// It does linear interpolation between table values for smoother output.
+class TableOscillator final : public LegacyOscillator {
+	Table table_;
+
+public:
+	TableOscillator(const int16_t* table, int32_t table_size_magnitude) : table_{table, table_size_magnitude} {}
+
+	[[gnu::always_inline]] Argon<q31_t> render() final {
+		Argon<uint32_t> indices = getPhase() >> (32 - table_.size_magnitude);
+
+		ArgonHalf<int16_t> fractional = (indices.ShiftRightNarrow<16>() >> 1).As<int16_t>();
+		auto [value1, value2] = ArgonHalf<int16_t>::LoadGatherInterleaved<2>(table_.data, indices);
+
+		// this is a standard linear interpolation of a + (b - a) * fractional
+		Argon<q31_t> output = value1.ShiftLeftLong<16>().MultiplyDoubleAddSaturateLong(value2 - value1, fractional);
+
+		this->advance(); // advance the phasor
+
+		return output;
+	}
+};
+
+class PWMTableOscillator final : public PWMOscillator, public LegacyOscillator {
+	Table table_;
+
+public:
+	PWMTableOscillator(const int16_t* table, int32_t table_size_magnitude) : table_{table, table_size_magnitude} {}
+
+	[[gnu::always_inline]] Argon<q31_t> render() final {
+		auto phase_to_add = -(getPulseWidth() >> 1);
+		Argon<uint32_t> phase_later = getPhase() + phase_to_add;
+		Argon<uint32_t> indices_a = getPhase() >> (32 - table_.size_magnitude);
+		ArgonHalf<int16_t> rshifted_a =
+		    indices_a.ShiftRightNarrow<16>().BitwiseAnd(std::numeric_limits<int16_t>::max()).As<int16_t>();
+		auto [value_a1, value_a2] = ArgonHalf<int16_t>::LoadGatherInterleaved<2>(table_.data, indices_a);
+
+		Argon<uint32_t> indices_b = phase_later >> (32 - table_.size_magnitude);
+		ArgonHalf<int16_t> rshifted_b =
+		    indices_b.ShiftRightNarrow<16>().BitwiseAnd(std::numeric_limits<int16_t>::max()).As<int16_t>();
+		auto [value_b1, value_b2] = ArgonHalf<int16_t>::LoadGatherInterleaved<2>(table_.data, indices_b);
+
+		// Sneakily do this backwards to flip the polarity of the output, which we need to do anyway
+		ArgonHalf<int16_t> strength_a1 = rshifted_a | std::numeric_limits<int16_t>::min();
+		ArgonHalf<int16_t> strength_a2 = std::numeric_limits<int16_t>::min() - strength_a1;
+
+		Argon<int32_t> output_a = strength_a2                               //<
+		                              .MultiplyDoubleSaturateLong(value_a2) //<
+		                              .MultiplyDoubleAddSaturateLong(strength_a1, value_a1);
+
+		ArgonHalf<int16_t> strength_b2 = rshifted_b & std::numeric_limits<int16_t>::max();
+		ArgonHalf<int16_t> strength_b1 = std::numeric_limits<int16_t>::max() - strength_b2;
+
+		Argon<int32_t> output_b = strength_b2                               //<
+		                              .MultiplyDoubleSaturateLong(value_b2) //<
+		                              .MultiplyDoubleAddSaturateLong(strength_b1, value_b1);
+
+		Argon<q31_t> output = output_a.MultiplyRoundFixedPoint(output_b) << 1; // (a *. b) << 1 (average?)
+		this->advance();                                                       // advance the phase component
+		return output;
+	}
+};
+} // namespace deluge::dsp::oscillator
