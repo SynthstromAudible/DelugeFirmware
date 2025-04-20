ab18de61d000df1c146c8362735ba2fc02e0b2f8
Katherine Whitlock <kate@skylinesynths.nyc>
2025-04-20 17:10:32 -0400

    Upgrade Argon version (#3612)

--- src/deluge/dsp/reverb/cosine_oscillator.hpp
+++ src/deluge/dsp/reverb/cosine_oscillator.hpp
@@ -12,7 +12,7 @@ public:
 	enum class Mode { APPROX, EXACT };
 
 	template <Mode mode = Mode::APPROX>
-	constexpr DualCosineOscillator(std::array<float, 2> frequencies) : frequencies_(frequencies) {
+	constexpr DualCosineOscillator(std::array<float, 2> frequencies) : frequencies_(frequencies[0], frequencies[1]) {
 		Init<mode>();
 	}
 	~DualCosineOscillator() = default;
