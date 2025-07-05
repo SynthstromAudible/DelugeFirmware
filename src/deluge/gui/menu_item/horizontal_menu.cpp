SynthstromAudible/DelugeFirmware

commit 0ab60b9781adef09dec68b98b40b0337eccb9a9c

Horizontal menus/ Various adjustments & improvements (#3900)
* Horizontal menus/ Various adjustments & improvements

Further refinement of the horizontal menus.
Many small things were tweaked, graphics were added for some menus, etc

* arp menu tweaks

* center frequency sliders

src/deluge/gui/menu_item/horizontal_menu.cpp
@@ -13,6 +13,8 @@
 
 namespace deluge::gui::menu_item {
 
+using namespace hid::display;
+
 ActionResult HorizontalMenu::buttonAction(hid::Button b, bool on, bool inCardRoutine) {
 	using namespace hid::button;
 
@@ -41,11 +43,34 @@ ActionResult HorizontalMenu::buttonAction(hid::Button b, bool on, bool inCardRou
 }
 
 void HorizontalMenu::renderOLED() {
+	const bool isHorizontal = renderingStyle() == HORIZONTAL;
+	if (isHorizontal) {
+		paging = splitMenuItemsByPages(items);
+
+		// Lit the scale and cross-screen buttons LEDs to indicate that they can be used to switch between pages
+		const auto hasPages = paging.pages.size() > 1;
+		indicator_leds::setLedState(IndicatorLED::SCALE_MODE, hasPages);
+		indicator_leds::setLedState(IndicatorLED::CROSS_SCREEN_EDIT, hasPages);
+
+		// did the selected horizontal menu item position change?
+		// if yes, update the instrument LED corresponding to that menu item position
+		// store the last selected horizontal menu item position so that we don't update the LED's more than we have to
+		if (const auto posOnPage = paging.selectedItemPositionOnPage; posOnPage != lastSelectedItemPosition) {
+			lastSelectedItemPosition = posOnPage;
+			updateSelectedMenuItemLED(posOnPage);
+		}
+
+		// Read all the values beforehand as content can depend on the values
+		for (const auto it : items) {
+			it->readCurrentValue();
+		}
+	}
+
 	// need to draw the content first because the title can depend on the content.
 	// example: mod-fx menu where the second page title is depending on the selected mod-fx type
 	drawPixelsForOled();
-	hid::display::OLED::main.drawScreenTitle(getTitle(), false);
-	hid::display::OLED::markChanged();
+	OLED::main.drawScreenTitle(getTitle(), !isHorizontal);
+	OLED::markChanged();
 }
 
 void HorizontalMenu::drawPixelsForOled() {
@@ -53,25 +78,10 @@ void HorizontalMenu::drawPixelsForOled() {
 		return Submenu::drawPixelsForOled();
 	}
 
-	hid::display::oled_canvas::Canvas& image = hid::display::OLED::main;
-
-	paging = splitMenuItemsByPages();
-
-	// Lit the scale and cross-screen buttons LEDs to indicate that they can be used to switch between pages
-	const auto hasPages = paging.pages.size() > 1;
-	indicator_leds::setLedState(IndicatorLED::SCALE_MODE, hasPages);
-	indicator_leds::setLedState(IndicatorLED::CROSS_SCREEN_EDIT, hasPages);
-
-	// did the selected horizontal menu item position change?
-	// if yes, update the instrument LED corresponding to that menu item position
-	// store the last selected horizontal menu item position so that we don't update the LED's more than we have to
-	const auto posOnPage = paging.selectedItemPositionOnPage;
-	if (posOnPage != lastSelectedItemPosition) {
-		lastSelectedItemPosition = posOnPage;
-		updateSelectedMenuItemLED(posOnPage);
-	}
+	oled_canvas::Canvas& image = OLED::main;
 
 	constexpr int32_t baseY = 14 + OLED_MAIN_TOPMOST_PIXEL;
+	constexpr int32_t columnWidth = OLED_MAIN_WIDTH_PIXELS / 4;
 	int32_t currentX = 0;
 
 	auto& [pageNumber, pageItems] = paging.getVisiblePage();
@@ -80,19 +90,18 @@ void HorizontalMenu::drawPixelsForOled() {
 	// Render the page
 	for (size_t n = 0; n < pageItems.size() && it != pageItems.end(); n++) {
 		MenuItem* item = *it;
-		item->readCurrentValue();
 
-		const int32_t boxWidth = OLED_MAIN_WIDTH_PIXELS / (4 / item->getColumnSpan());
+		const int32_t boxWidth = columnWidth * item->getColumnSpan();
 		constexpr int32_t boxHeight = 25;
-		int32_t contentHeight = boxHeight;
-
 		constexpr int32_t labelHeight = kTextSpacingY;
 		constexpr int32_t labelY = baseY + boxHeight - labelHeight;
-		std::optional<ColumnLabelPosition> labelPos;
+		int32_t contentHeight = boxHeight;
+
+		const bool isSelected = n == paging.selectedItemPositionOnPage;
 
 		if (item->showColumnLabel()) {
 			// Draw the label at the bottom
-			labelPos = renderColumnLabel(item, labelY, currentX, boxWidth);
+			renderColumnLabel(item, labelY, currentX, boxWidth, isSelected);
 			contentHeight -= labelHeight;
 		}
 
@@ -102,22 +111,22 @@ void HorizontalMenu::drawPixelsForOled() {
 		}
 		else {
 			// Draw content of the menu item
-			item->renderInHorizontalMenu(currentX + 1, boxWidth, baseY, contentHeight);
+			item->renderInHorizontalMenu(currentX, boxWidth - 1, baseY, contentHeight);
 		}
 
 		// Highlight the selected item if it doesn't occupy the whole page
-		if (n == posOnPage && (pageItems.size() > 1 || pageItems[0]->getColumnSpan() < 4)) {
+		if (isSelected && (pageItems.size() > 1 || pageItems[0]->getColumnSpan() < 4)) {
 			if (FlashStorage::accessibilityMenuHighlighting) {
 				// Highlight just by drawing a line below
 				image.invertArea(currentX, boxWidth - 1, baseY + boxHeight, OLED_MAIN_VISIBLE_HEIGHT + 1);
 			}
-			else if (!labelPos.has_value() || item->isSubmenu()) {
+			else if (!item->showColumnLabel() || item->isSubmenu()) {
 				// Highlight the whole slot if it has no label or is a submenu
-				image.invertAreaRounded(currentX, boxWidth, baseY, baseY + boxHeight - 1);
+				image.invertAreaRounded(currentX, boxWidth - 1, baseY, baseY + boxHeight - 1);
 			}
 			else {
 				// Otherwise highlight only label
-				image.invertAreaRounded(labelPos->startX - 3, labelPos->width + 5, labelY, labelY + labelHeight - 1);
+				image.invertAreaRounded(currentX + 1, boxWidth - 3, labelY, labelY + labelHeight - 1);
 			}
 		}
 
@@ -125,21 +134,43 @@ void HorizontalMenu::drawPixelsForOled() {
 		it = std::next(it);
 	}
 
-	// Render the page counters
-	if (paging.pages.size() > 1) {
-		constexpr int32_t pageY = 1 + OLED_MAIN_TOPMOST_PIXEL;
-		int32_t endX = OLED_MAIN_WIDTH_PIXELS;
+	// Render placeholders for remaining slots
+	for (int32_t n = currentX / (OLED_MAIN_WIDTH_PIXELS / 4); n < 4; n++) {
+		const int32_t startX = n * columnWidth + 7;
+		const int32_t endX = startX + 17;
+		constexpr int32_t startY = baseY + 2;
+		constexpr int32_t endY = OLED_MAIN_HEIGHT_PIXELS - 6;
+		constexpr int32_t dotInterval = 5;
 
-		for (int32_t p = paging.pages.size(); p > 0; p--) {
-			DEF_STACK_STRING_BUF(pageNum, 2);
-			pageNum.appendInt(p);
-			const int32_t pageNumWidth = image.getStringWidthInPixels(pageNum.c_str(), kTextSpacingY);
-			image.drawString(pageNum.c_str(), endX - pageNumWidth, pageY, kTextSpacingX, kTextSpacingY);
-			endX -= pageNumWidth + 1;
-			if (p - 1 == pageNumber) {
-				image.invertAreaRounded(endX, pageNumWidth + 1, pageY, pageY + kTextSpacingY);
-			}
+		for (int32_t x = startX + 1; x < endX; x += dotInterval) {
+			image.drawPixel(x, startY + 1);
+			image.drawPixel(x, endY - 1);
 		}
+		for (int32_t y = startY + 3; y < endY; y += dotInterval) {
+			image.drawPixel(startX - 1, y);
+			image.drawPixel(endX + 1, y);
+		}
+	}
+
+	// Render the page counters
+	if (const int32_t pagesCount = paging.pages.size(); pagesCount > 1) {
+		constexpr int32_t y = 1 + OLED_MAIN_TOPMOST_PIXEL;
+		int32_t x = OLED_MAIN_WIDTH_PIXELS - kTextSpacingX - 1;
+
+		// Draw total count
+		DEF_STACK_STRING_BUF(currentPageNum, 2);
+		currentPageNum.appendInt(pagesCount);
+		image.drawString(currentPageNum.c_str(), x, y, kTextSpacingX, kTextSpacingY);
+		x -= kTextSpacingX - 1;
+
+		// Draw separator line
+		image.drawLine(x, y + kTextSpacingY - 2, x + 2, y + 1);
+		x -= 6;
+
+		// Draw the current one
+		currentPageNum.clear();
+		currentPageNum.appendInt(paging.visiblePageNumber + 1);
+		image.drawString(currentPageNum.c_str(), x, y, kTextSpacingX, kTextSpacingY);
 	}
 }
 
@@ -262,8 +293,8 @@ ActionResult HorizontalMenu::selectMenuItemOnVisiblePage(int32_t selectedColumn)
 	return ActionResult::DEALT_WITH;
 }
 
-HorizontalMenu::Paging HorizontalMenu::splitMenuItemsByPages() const {
-	std::vector<PageInfo> pages;
+HorizontalMenu::Paging HorizontalMenu::splitMenuItemsByPages(std::span<MenuItem*> menuItems) {
+	std::vector<Page> pages;
 	std::vector<MenuItem*> currentPageItems;
 	int32_t currentPageNumber = 0;
 	int32_t currentPageItemsSpan = 0;
@@ -275,7 +306,7 @@ HorizontalMenu::Paging HorizontalMenu::splitMenuItemsByPages() const {
 		return layout != FIXED || std::ranges::any_of(pageItems, [](MenuItem* item) { return isItemRelevant(item); });
 	};
 
-	for (auto* item : items) {
+	for (auto* item : menuItems) {
 		if (layout != FIXED && !isItemRelevant(item)) {
 			continue;
 		}
@@ -284,7 +315,7 @@ HorizontalMenu::Paging HorizontalMenu::splitMenuItemsByPages() const {
 		if (currentPageItemsSpan + itemSpan > 4) {
 			// Finalize the current page
 			if (shouldIncludePage(currentPageItems)) {
-				pages.push_back(PageInfo{.number = currentPageNumber, .items = currentPageItems});
+				pages.push_back(Page{.number = currentPageNumber, .items = currentPageItems});
 				++currentPageNumber;
 			}
 
@@ -304,7 +335,7 @@ HorizontalMenu::Paging HorizontalMenu::splitMenuItemsByPages() const {
 
 	if (!currentPageItems.empty() && shouldIncludePage(currentPageItems)) {
 		// Finalize the last page
-		pages.push_back(PageInfo{.number = currentPageNumber, .items = currentPageItems});
+		pages.push_back(Page{.number = currentPageNumber, .items = currentPageItems});
 	}
 
 	return Paging{visiblePageNumber, selectedItemPositionOnPage, pages};
@@ -364,14 +395,12 @@ Submenu::RenderingStyle HorizontalMenu::renderingStyle() const {
 	return VERTICAL;
 }
 
-HorizontalMenu::ColumnLabelPosition HorizontalMenu::renderColumnLabel(MenuItem* menuItem, int32_t labelY,
-                                                                      int32_t slotStartX, int32_t slotWidth) {
-	hid::display::oled_canvas::Canvas& image = hid::display::OLED::main;
+void HorizontalMenu::renderColumnLabel(MenuItem* menuItem, int32_t labelY, int32_t slotStartX, int32_t slotWidth,
+                                       bool isSelected) {
+	oled_canvas::Canvas& image = OLED::main;
 
 	DEF_STACK_STRING_BUF(label, kShortStringBufferSize);
 	menuItem->getColumnLabel(label);
-
-	// Remove any spaces
 	label.removeSpaces();
 
 	// If the name fits as-is, we'll squeeze it in. Otherwise, we chop off letters until
@@ -381,11 +410,16 @@ HorizontalMenu::ColumnLabelPosition HorizontalMenu::renderColumnLabel(MenuItem* 
 		label.truncate(label.size() - 1);
 	}
 
-	// center the label
-	const int32_t labelStartX = (slotStartX + (slotWidth - labelWidth) / 2);
-	hid::display::OLED::main.drawString(label.c_str(), labelStartX, labelY, kTextSpacingX, kTextSpacingY);
+	// Draw the label centered
+	const int32_t labelStartX = slotStartX + (slotWidth - labelWidth) / 2;
+	image.drawString(label.c_str(), labelStartX, labelY, kTextSpacingX, kTextSpacingY);
 
-	return {labelStartX, labelWidth};
+	if (menuItem->getColumnSpan() > 1 && !menuItem->isSubmenu() && !isSelected) {
+		// Draw small lines on the left and right side if the slot is too wide
+		const int32_t y = labelY + 4;
+		image.drawHorizontalLine(y, slotStartX + 4, labelStartX - 4);
+		image.drawHorizontalLine(y, labelStartX + labelWidth + 2, slotStartX + slotWidth - 6);
+	}
 }
 
 } // namespace deluge::gui::menu_item

src/deluge/gui/l10n/g_english.cpp
@@ -739,6 +739,8 @@ PLACE_SDRAM_DATA Language english{
         {STRING_FOR_UNISON_NUMBER_MENU_TITLE, "Unison number"},
         {STRING_FOR_UNISON_DETUNE_MENU_TITLE, "Unison detune"},
         {STRING_FOR_UNISON_STEREO_SPREAD_MENU_TITLE, "Unison stereo spread"},
+        {STRING_FOR_ARP_BASIC_MENU_TITLE, "Arp basic"},
+        {STRING_FOR_ARP_PATTERN_MENU_TITLE, "Arp pattern"},
         {STRING_FOR_ARP_PRESET_MENU_TITLE, "Arp preset"},
         {STRING_FOR_ARP_GATE_MENU_TITLE, "Arp gate"},
         {STRING_FOR_ARP_RATE_MENU_TITLE, "Arp rate"},

src/deluge/gui/l10n/strings.h
@@ -803,6 +803,8 @@ enum class String : size_t {
 	STRING_FOR_UNISON_NUMBER_MENU_TITLE,
 	STRING_FOR_UNISON_DETUNE_MENU_TITLE,
 	STRING_FOR_UNISON_STEREO_SPREAD_MENU_TITLE,
+	STRING_FOR_ARP_BASIC_MENU_TITLE,
+	STRING_FOR_ARP_PATTERN_MENU_TITLE,
 	STRING_FOR_ARP_PRESET_MENU_TITLE,
 	STRING_FOR_ARP_RANDOMIZER_LOCK_TITLE,
 	STRING_FOR_ARP_WALK_TITLE,

src/deluge/gui/menu_item/arpeggiator/arp_unpatched_param.h
@@ -19,7 +19,7 @@
 #include "gui/ui/sound_editor.h"
 
 namespace deluge::gui::menu_item::arpeggiator {
-class ArpUnpatchedParam final : public UnpatchedParam {
+class ArpUnpatchedParam : public UnpatchedParam {
 public:
 	using UnpatchedParam::UnpatchedParam;
 

src/deluge/gui/menu_item/arpeggiator/midi_cv/gate.h
@@ -21,6 +21,8 @@
 #include "gui/ui/sound_editor.h"
 #include "model/song/song.h"
 
+#include <hid/display/oled.h>
+
 namespace deluge::gui::menu_item::arpeggiator::midi_cv {
 class Gate final : public ArpNonSoundInteger {
 public:
@@ -32,5 +34,6 @@ public:
 		int32_t value = computeFinalValueForStandardMenuItem(this->getValue());
 		soundEditor.currentArpSettings->gate = value;
 	}
+	[[nodiscard]] NumberStyle getNumberStyle() const override { return LENGTH_SLIDER; }
 };
 } // namespace deluge::gui::menu_item::arpeggiator::midi_cv

src/deluge/gui/menu_item/arpeggiator/midi_cv/rate.h
@@ -32,9 +32,5 @@ public:
 		int32_t value = computeFinalValueForStandardMenuItem(this->getValue());
 		soundEditor.currentArpSettings->rate = value;
 	}
-	bool isRelevant(ModControllableAudio* modControllable, int32_t whichThing) override {
-		return (soundEditor.editingCVOrMIDIClip() || soundEditor.editingNonAudioDrumRow())
-		       && soundEditor.currentArpSettings->syncLevel == SYNC_LEVEL_NONE;
-	}
 };
 } // namespace deluge::gui::menu_item::arpeggiator::midi_cv

src/deluge/gui/menu_item/arpeggiator/midi_cv/rhythm.h
@@ -24,6 +24,9 @@
 #include "modulation/arpeggiator_rhythms.h"
 
 namespace deluge::gui::menu_item::arpeggiator::midi_cv {
+
+using namespace hid::display;
+
 class Rhythm final : public ArpNonSoundInteger {
 public:
 	using ArpNonSoundInteger::ArpNonSoundInteger;
@@ -38,23 +41,29 @@ public:
 	void drawValue() override { display->setScrollingText(arpRhythmPatternNames[this->getValue()]); }
 
 	void drawInteger(int32_t textWidth, int32_t textHeight, int32_t yPixel) override {
-		deluge::hid::display::oled_canvas::Canvas& canvas = hid::display::OLED::main;
+		oled_canvas::Canvas& canvas = OLED::main;
 
 		canvas.drawStringCentred(arpRhythmPatternNames[this->getValue()], yPixel + OLED_MAIN_TOPMOST_PIXEL, textWidth,
 		                         textHeight);
 	}
 
 	void renderInHorizontalMenu(int32_t startX, int32_t width, int32_t startY, int32_t height) override {
-		hid::display::oled_canvas::Canvas& image = hid::display::OLED::main;
+		oled_canvas::Canvas& image = OLED::main;
 
-		// Render current value
-		DEF_STACK_STRING_BUF(shortOpt, kShortStringBufferSize);
-		char name[12];
-		// Index:Name
-		snprintf(name, sizeof(name), "%d:%s", this->getValue(), arpRhythmPatternNames[this->getValue()]);
-		shortOpt.append(name);
+		const auto value = this->getValue();
+		const auto pattern = std::string_view(arpRhythmPatternNames[value]);
+		if (value == 0) {
+			return image.drawStringCentered(pattern.data(), startX, startY + 3, kTextSpacingX, kTextSpacingY, width);
+		}
 
-		image.drawStringCentered(shortOpt, startX, startY + 3, kTextSpacingX, kTextSpacingY, width);
+		constexpr int32_t paddingBetween = 2;
+		const int32_t rhythmWidth = pattern.size() * kTextSpacingX + pattern.size() * paddingBetween;
+
+		int32_t x = startX + (width - rhythmWidth) / 2 + 1;
+		for (const char character : pattern) {
+			image.drawChar(character == '0' ? 'X' : character, x, startY + 3, kTextSpacingX, kTextSpacingY);
+			x += kTextSpacingX + paddingBetween;
+		}
 	}
 
 protected:

src/deluge/gui/menu_item/arpeggiator/preset_mode.h
@@ -118,18 +118,21 @@ public:
 		return nullptr;
 	}
 
-	[[nodiscard]] int32_t getColumnSpan() const override { return 2; }
 	[[nodiscard]] bool showColumnLabel() const override { return false; }
-	[[nodiscard]] bool showPopup() const override { return false; }
+
+	void getColumnLabel(StringBuf& label) override { label.append(l10n::get(l10n::String::STRING_FOR_MODE)); }
 
 	void renderInHorizontalMenu(int32_t startX, int32_t width, int32_t startY, int32_t height) override {
 		using namespace deluge::hid::display;
 		oled_canvas::Canvas& image = OLED::main;
 
+		if (this->getValue<ArpPreset>() == ArpPreset::OFF) {
+			const auto offString = l10n::get(l10n::String::STRING_FOR_OFF);
+			return image.drawStringCentered(offString, startX, startY + 8, kTextTitleSpacingX, kTextTitleSizeY, width);
+		}
+
 		const std::vector<std::reference_wrapper<const std::vector<uint8_t>>> bitmaps = [&] {
 			switch (this->getValue<ArpPreset>()) {
-			case ArpPreset::OFF:
-				return std::vector{std::cref(OLED::switcherIconOff)};
 			case ArpPreset::UP:
 				return std::vector{std::cref(OLED::arpModeIconUp)};
 			case ArpPreset::DOWN:
@@ -142,24 +145,18 @@ public:
 				return std::vector{std::cref(OLED::arpModeIconWalk)};
 			case ArpPreset::CUSTOM:
 				return std::vector{std::cref(OLED::arpModeIconCustom)};
+			default:
+				return std::vector<std::reference_wrapper<const std::vector<uint8_t>>>{};
 			}
-
-			return std::vector{std::cref(OLED::switcherIconOff)};
 		}();
 
-		const std::string_view option = getOptions(OptType::FULL)[this->getValue()];
-
-		constexpr int32_t paddingBetween = 3;
 		constexpr int32_t numBytesTall = 2;
-		const int32_t textWidth = image.getStringWidthInPixels(option.data(), kTextSpacingY);
 		const int32_t bitmapsWidth = std::accumulate(
 		    bitmaps.begin(), bitmaps.end(), 0, [](int32_t acc, auto v) { return acc + v.get().size() / numBytesTall; });
 
-		const int32_t totalWidth = textWidth + paddingBetween + bitmapsWidth;
-
 		// Calc center position
-		int32_t x = startX + ((width - totalWidth) / 2) - 1;
-		int32_t y = startY + ((height - numBytesTall * 8) / 2) + 1;
+		int32_t x = startX + (width - bitmapsWidth) / 2;
+		int32_t y = startY + (height - numBytesTall * 8) / 2;
 
 		// Draw icons
 		for (auto bitmap : bitmaps) {
@@ -167,11 +164,6 @@ public:
 			                           numBytesTall);
 			x += bitmap.get().size() / numBytesTall;
 		}
-
-		// Draw mode text
-		x += paddingBetween;
-		y = startY + ((height - kTextSpacingY) / 2);
-		image.drawString(option.data(), x, y, kTextSpacingX, kTextSpacingY, 0, x + width - kTextSpacingX);
 	}
 };
 } // namespace deluge::gui::menu_item::arpeggiator

commit 4f46a8e8f3505eede8da14d93bb3c46f83d24d01

Bugfix/ Fixed crash on accessing randomizer params in some conditions
Small fix for a bug introduced in #3902 PR

src/deluge/gui/menu_item/horizontal_menu.cpp
@@ -62,7 +62,9 @@ void HorizontalMenu::renderOLED() {
 
 		// Read all the values beforehand as content can depend on the values
 		for (const auto it : items) {
-			it->readCurrentValue();
+			if (isItemRelevant(it)) {
+				it->readCurrentValue();
+			}
 		}
 	}
 