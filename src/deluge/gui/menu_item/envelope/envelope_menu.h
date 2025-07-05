SynthstromAudible/DelugeFirmware

commit 0ab60b9781adef09dec68b98b40b0337eccb9a9c

Horizontal menus/ Various adjustments & improvements (#3900)
* Horizontal menus/ Various adjustments & improvements

Further refinement of the horizontal menus.
Many small things were tweaked, graphics were added for some menus, etc

* arp menu tweaks

* center frequency sliders

src/deluge/gui/menu_item/envelope/envelope_menu.h
@@ -27,28 +27,12 @@ class EnvelopeMenu final : public HorizontalMenu {
 public:
 	using HorizontalMenu::HorizontalMenu;
 
-	void renderOLED() override {
-		OLED::main.drawScreenTitle(getTitle(), false);
-		drawPixelsForOled();
-		OLED::markChanged();
-	}
-
 	void drawPixelsForOled() override {
 		if (renderingStyle() != HORIZONTAL) {
 			return Submenu::drawPixelsForOled();
 		}
 
-		// Update the synth-kit-midi-cv buttons LEDs
-		paging = splitMenuItemsByPages();
-		if (paging.selectedItemPositionOnPage != lastSelectedItemPosition) {
-			lastSelectedItemPosition = paging.selectedItemPositionOnPage;
-			updateSelectedMenuItemLED(lastSelectedItemPosition);
-		}
-
 		// Get the values in 0-50 range
-		for (const auto item : items) {
-			item->readCurrentValue();
-		}
 		const int32_t attack = static_cast<Segment*>(items[0])->getValue();
 		const int32_t decay = static_cast<Segment*>(items[1])->getValue();
 		const int32_t sustain = static_cast<Segment*>(items[2])->getValue();
@@ -144,13 +128,6 @@ private:
 
 		// Draw a transition square
 		image.drawRectangle(ix - squareSize, iy - squareSize, ix + squareSize, iy + squareSize);
-	};
-
-	// --- Helper functions ----
-	static float sigmoidLikeCurve(const float x, const float softening, const float xMax) {
-		const float raw = x / (x + softening);
-		const float maxVal = xMax / (xMax + softening);
-		return raw / maxVal;
 	};
 };
 

src/deluge/gui/l10n/g_english.cpp
@@ -739,6 +739,8 @@ PLACE_SDRAM_DATA Language english{
         {STRING_FOR_UNISON_NUMBER_MENU_TITLE, "Unison number"},
         {STRING_FOR_UNISON_DETUNE_MENU_TITLE, "Unison detune"},
         {STRING_FOR_UNISON_STEREO_SPREAD_MENU_TITLE, "Unison stereo spread"},
+        {STRING_FOR_ARP_BASIC_MENU_TITLE, "Arp basic"},
+        {STRING_FOR_ARP_PATTERN_MENU_TITLE, "Arp pattern"},
         {STRING_FOR_ARP_PRESET_MENU_TITLE, "Arp preset"},
         {STRING_FOR_ARP_GATE_MENU_TITLE, "Arp gate"},
         {STRING_FOR_ARP_RATE_MENU_TITLE, "Arp rate"},

src/deluge/gui/l10n/strings.h
@@ -803,6 +803,8 @@ enum class String : size_t {
 	STRING_FOR_UNISON_NUMBER_MENU_TITLE,
 	STRING_FOR_UNISON_DETUNE_MENU_TITLE,
 	STRING_FOR_UNISON_STEREO_SPREAD_MENU_TITLE,
+	STRING_FOR_ARP_BASIC_MENU_TITLE,
+	STRING_FOR_ARP_PATTERN_MENU_TITLE,
 	STRING_FOR_ARP_PRESET_MENU_TITLE,
 	STRING_FOR_ARP_RANDOMIZER_LOCK_TITLE,
 	STRING_FOR_ARP_WALK_TITLE,

src/deluge/gui/menu_item/arpeggiator/arp_unpatched_param.h
@@ -19,7 +19,7 @@
 #include "gui/ui/sound_editor.h"
 
 namespace deluge::gui::menu_item::arpeggiator {
-class ArpUnpatchedParam final : public UnpatchedParam {
+class ArpUnpatchedParam : public UnpatchedParam {
 public:
 	using UnpatchedParam::UnpatchedParam;
 

src/deluge/gui/menu_item/arpeggiator/midi_cv/gate.h
@@ -21,6 +21,8 @@
 #include "gui/ui/sound_editor.h"
 #include "model/song/song.h"
 
+#include <hid/display/oled.h>
+
 namespace deluge::gui::menu_item::arpeggiator::midi_cv {
 class Gate final : public ArpNonSoundInteger {
 public:
@@ -32,5 +34,6 @@ public:
 		int32_t value = computeFinalValueForStandardMenuItem(this->getValue());
 		soundEditor.currentArpSettings->gate = value;
 	}
+	[[nodiscard]] NumberStyle getNumberStyle() const override { return LENGTH_SLIDER; }
 };
 } // namespace deluge::gui::menu_item::arpeggiator::midi_cv

src/deluge/gui/menu_item/arpeggiator/midi_cv/rate.h
@@ -32,9 +32,5 @@ public:
 		int32_t value = computeFinalValueForStandardMenuItem(this->getValue());
 		soundEditor.currentArpSettings->rate = value;
 	}
-	bool isRelevant(ModControllableAudio* modControllable, int32_t whichThing) override {
-		return (soundEditor.editingCVOrMIDIClip() || soundEditor.editingNonAudioDrumRow())
-		       && soundEditor.currentArpSettings->syncLevel == SYNC_LEVEL_NONE;
-	}
 };
 } // namespace deluge::gui::menu_item::arpeggiator::midi_cv

src/deluge/gui/menu_item/arpeggiator/midi_cv/rhythm.h
@@ -24,6 +24,9 @@
 #include "modulation/arpeggiator_rhythms.h"
 
 namespace deluge::gui::menu_item::arpeggiator::midi_cv {
+
+using namespace hid::display;
+
 class Rhythm final : public ArpNonSoundInteger {
 public:
 	using ArpNonSoundInteger::ArpNonSoundInteger;
@@ -38,23 +41,29 @@ public:
 	void drawValue() override { display->setScrollingText(arpRhythmPatternNames[this->getValue()]); }
 
 	void drawInteger(int32_t textWidth, int32_t textHeight, int32_t yPixel) override {
-		deluge::hid::display::oled_canvas::Canvas& canvas = hid::display::OLED::main;
+		oled_canvas::Canvas& canvas = OLED::main;
 
 		canvas.drawStringCentred(arpRhythmPatternNames[this->getValue()], yPixel + OLED_MAIN_TOPMOST_PIXEL, textWidth,
 		                         textHeight);
 	}
 
 	void renderInHorizontalMenu(int32_t startX, int32_t width, int32_t startY, int32_t height) override {
-		hid::display::oled_canvas::Canvas& image = hid::display::OLED::main;
+		oled_canvas::Canvas& image = OLED::main;
 
-		// Render current value
-		DEF_STACK_STRING_BUF(shortOpt, kShortStringBufferSize);
-		char name[12];
-		// Index:Name
-		snprintf(name, sizeof(name), "%d:%s", this->getValue(), arpRhythmPatternNames[this->getValue()]);
-		shortOpt.append(name);
+		const auto value = this->getValue();
+		const auto pattern = std::string_view(arpRhythmPatternNames[value]);
+		if (value == 0) {
+			return image.drawStringCentered(pattern.data(), startX, startY + 3, kTextSpacingX, kTextSpacingY, width);
+		}
 
-		image.drawStringCentered(shortOpt, startX, startY + 3, kTextSpacingX, kTextSpacingY, width);
+		constexpr int32_t paddingBetween = 2;
+		const int32_t rhythmWidth = pattern.size() * kTextSpacingX + pattern.size() * paddingBetween;
+
+		int32_t x = startX + (width - rhythmWidth) / 2 + 1;
+		for (const char character : pattern) {
+			image.drawChar(character == '0' ? 'X' : character, x, startY + 3, kTextSpacingX, kTextSpacingY);
+			x += kTextSpacingX + paddingBetween;
+		}
 	}
 
 protected:

src/deluge/gui/menu_item/arpeggiator/preset_mode.h
@@ -118,18 +118,21 @@ public:
 		return nullptr;
 	}
 
-	[[nodiscard]] int32_t getColumnSpan() const override { return 2; }
 	[[nodiscard]] bool showColumnLabel() const override { return false; }
-	[[nodiscard]] bool showPopup() const override { return false; }
+
+	void getColumnLabel(StringBuf& label) override { label.append(l10n::get(l10n::String::STRING_FOR_MODE)); }
 
 	void renderInHorizontalMenu(int32_t startX, int32_t width, int32_t startY, int32_t height) override {
 		using namespace deluge::hid::display;
 		oled_canvas::Canvas& image = OLED::main;
 
+		if (this->getValue<ArpPreset>() == ArpPreset::OFF) {
+			const auto offString = l10n::get(l10n::String::STRING_FOR_OFF);
+			return image.drawStringCentered(offString, startX, startY + 8, kTextTitleSpacingX, kTextTitleSizeY, width);
+		}
+
 		const std::vector<std::reference_wrapper<const std::vector<uint8_t>>> bitmaps = [&] {
 			switch (this->getValue<ArpPreset>()) {
-			case ArpPreset::OFF:
-				return std::vector{std::cref(OLED::switcherIconOff)};
 			case ArpPreset::UP:
 				return std::vector{std::cref(OLED::arpModeIconUp)};
 			case ArpPreset::DOWN:
@@ -142,24 +145,18 @@ public:
 				return std::vector{std::cref(OLED::arpModeIconWalk)};
 			case ArpPreset::CUSTOM:
 				return std::vector{std::cref(OLED::arpModeIconCustom)};
+			default:
+				return std::vector<std::reference_wrapper<const std::vector<uint8_t>>>{};
 			}
-
-			return std::vector{std::cref(OLED::switcherIconOff)};
 		}();
 
-		const std::string_view option = getOptions(OptType::FULL)[this->getValue()];
-
-		constexpr int32_t paddingBetween = 3;
 		constexpr int32_t numBytesTall = 2;
-		const int32_t textWidth = image.getStringWidthInPixels(option.data(), kTextSpacingY);
 		const int32_t bitmapsWidth = std::accumulate(
 		    bitmaps.begin(), bitmaps.end(), 0, [](int32_t acc, auto v) { return acc + v.get().size() / numBytesTall; });
 
-		const int32_t totalWidth = textWidth + paddingBetween + bitmapsWidth;
-
 		// Calc center position
-		int32_t x = startX + ((width - totalWidth) / 2) - 1;
-		int32_t y = startY + ((height - numBytesTall * 8) / 2) + 1;
+		int32_t x = startX + (width - bitmapsWidth) / 2;
+		int32_t y = startY + (height - numBytesTall * 8) / 2;
 
 		// Draw icons
 		for (auto bitmap : bitmaps) {
@@ -167,11 +164,6 @@ public:
 			                           numBytesTall);
 			x += bitmap.get().size() / numBytesTall;
 		}
-
-		// Draw mode text
-		x += paddingBetween;
-		y = startY + ((height - kTextSpacingY) / 2);
-		image.drawString(option.data(), x, y, kTextSpacingX, kTextSpacingY, 0, x + width - kTextSpacingX);
 	}
 };
 } // namespace deluge::gui::menu_item::arpeggiator

commit 209b856319ddb79cbf1c170ab560a936735760bb

Bugfix/ Fixed broken envelope decay visualization
Small fix for a bug introduced in #3902 PR

src/deluge/gui/menu_item/envelope/envelope_menu.h
@@ -50,7 +50,7 @@ public:
 
 		// Calculate widths
 		const float attackWidth = (attack / 50.0f) * maxSegmentWidth;
-		const float decayNormalized = sigmoidLikeCurve(decay, 10.0f, 50.0f); // Maps 0-50 to 0-1 range with steep start
+		const float decayNormalized = sigmoidLikeCurve(decay, 50.0f, 10.0f); // Maps 0-50 to 0-1 range with steep start
 		const float decayWidth = decayNormalized * maxSegmentWidth;
 
 		// X positions