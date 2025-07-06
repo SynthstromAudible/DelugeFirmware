e35b12d149040c4759c681e2217d22df876fb368
Raúl Muñoz <raul@mothership.com>
2025-07-06 02:14:24 +0200

    Rename STEP PROBABILITY to SWAP PROBABILITY (SWAP) (#3907)

--- src/deluge/model/mod_controllable/mod_controllable_audio.cpp
+++ src/deluge/model/mod_controllable/mod_controllable_audio.cpp
@@ -113,7 +113,7 @@ void ModControllableAudio::initParams(ParamManager* paramManager) {
 	unpatchedParams->params[params::UNPATCHED_ARP_GATE].setCurrentValueBasicForSetup(0);
 	unpatchedParams->params[params::UNPATCHED_NOTE_PROBABILITY].setCurrentValueBasicForSetup(2147483647);
 	unpatchedParams->params[params::UNPATCHED_ARP_BASS_PROBABILITY].setCurrentValueBasicForSetup(-2147483648);
-	unpatchedParams->params[params::UNPATCHED_ARP_STEP_PROBABILITY].setCurrentValueBasicForSetup(-2147483648);
+	unpatchedParams->params[params::UNPATCHED_ARP_SWAP_PROBABILITY].setCurrentValueBasicForSetup(-2147483648);
 	unpatchedParams->params[params::UNPATCHED_ARP_GLIDE_PROBABILITY].setCurrentValueBasicForSetup(-2147483648);
 	unpatchedParams->params[params::UNPATCHED_REVERSE_PROBABILITY].setCurrentValueBasicForSetup(-2147483648);
 	unpatchedParams->params[params::UNPATCHED_ARP_CHORD_PROBABILITY].setCurrentValueBasicForSetup(-2147483648);
@@ -509,7 +509,7 @@ void ModControllableAudio::writeParamAttributesToFile(Serializer& writer, ParamM
 	                                       writeAutomation);
 	unpatchedParams->writeParamAsAttribute(writer, "bassProbability", params::UNPATCHED_ARP_BASS_PROBABILITY,
 	                                       writeAutomation);
-	unpatchedParams->writeParamAsAttribute(writer, "stepProbability", params::UNPATCHED_ARP_STEP_PROBABILITY,
+	unpatchedParams->writeParamAsAttribute(writer, "swapProbability", params::UNPATCHED_ARP_SWAP_PROBABILITY,
 	                                       writeAutomation);
 	unpatchedParams->writeParamAsAttribute(writer, "glideProbability", params::UNPATCHED_ARP_GLIDE_PROBABILITY,
 	                                       writeAutomation);
@@ -659,10 +659,10 @@ bool ModControllableAudio::readParamTagFromFile(Deserializer& reader, char const
 		reader.exitTag("bassProbability");
 	}
 
-	else if (!strcmp(tagName, "stepProbability")) {
-		unpatchedParams->readParam(reader, unpatchedParamsSummary, params::UNPATCHED_ARP_STEP_PROBABILITY,
+	else if (!strcmp(tagName, "swapProbability")) {
+		unpatchedParams->readParam(reader, unpatchedParamsSummary, params::UNPATCHED_ARP_SWAP_PROBABILITY,
 		                           readAutomationUpToPos);
-		reader.exitTag("stepProbability");
+		reader.exitTag("swapProbability");
 	}
 
 	else if (!strcmp(tagName, "glideProbability")) {
