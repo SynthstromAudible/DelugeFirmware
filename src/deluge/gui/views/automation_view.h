3f6d2ed413feb5e9370e8f87c83ec5182e180e9b
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2025-07-08 23:29:50 -0400

    Refactor automation view and velocity view
    1st in what will be a series of refactorings
    - refactored automation editor and note velocity editor code to separate classes

--- src/deluge/gui/views/automation_view.h
+++ src/deluge/gui/views/automation_view.h
@@ -78,6 +78,10 @@ public:
 	bool possiblyRefreshAutomationEditorGrid(Clip* clip, deluge::modulation::params::Kind paramKind, int32_t paramID);
 	bool renderMainPads(uint32_t whichRows, RGB image[][kDisplayWidth + kSideBarWidth],
 	                    uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], bool drawUndefinedArea = true) override;
+	void renderUndefinedArea(int32_t xScroll, uint32_t xZoom, int32_t lengthToDisplay,
+	                         RGB image[][kDisplayWidth + kSideBarWidth],
+	                         uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], int32_t imageWidth,
+	                         TimelineView* timelineView, bool tripletsOnHere, int32_t xDisplay);
 	bool renderSidebar(uint32_t whichRows, RGB image[][kDisplayWidth + kSideBarWidth],
 	                   uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth]) override;
 	void renderDisplay(int32_t knobPosLeft = kNoSelection, int32_t knobPosRight = kNoSelection,
@@ -126,7 +130,7 @@ public:
 	bool interpolationBefore;
 	bool interpolationAfter;
 
-	// public to midi follow can access it
+	// public to midi follow and editor layouts can access it
 	ModelStackWithAutoParam*
 	getModelStackWithParamForClip(ModelStackWithTimelineCounter* modelStack, Clip* clip,
 	                              int32_t paramID = deluge::modulation::params::kNoParamID,
@@ -136,23 +140,35 @@ public:
 	void initParameterSelection(bool updateDisplay = true);
 	bool onArrangerView;
 
-	// public so uiTimerManager can access it
+	// public so uiTimerManager and editor layouts can access it
 	void blinkInterpolationShortcut();
 	void blinkPadSelectionShortcut();
 
-	// public so menu can access it
+	// public so menu and editor layouts can access it
 	bool onMenuView;
 	UI* previousUI; // previous UI so you can swap back UI after exiting menu
 	int32_t getAutomationParameterKnobPos(ModelStackWithAutoParam* modelStack, uint32_t pos);
 	void setAutomationKnobIndicatorLevels(ModelStackWithAutoParam* modelStack, int32_t knobPosLeft,
 	                                      int32_t knobPosRight);
+	void initInterpolation();
 	void resetInterpolationShortcutBlinking();
+	void initPadSelection();
 	void resetPadSelectionShortcutBlinking();
+
 	AutomationParamType automationParamType;
 	bool getAffectEntire() override;
 
 	// public so action logger can access it
 	void resetShortcutBlinking();
+
+	bool padSelectionOn;
+	bool multiPadPressActive;
+	bool middlePadPressSelected;
+	int32_t leftPadSelectedX;
+	int32_t leftPadSelectedY;
+	int32_t rightPadSelectedX;
+	int32_t rightPadSelectedY;
+	int32_t lastPadSelectedKnobPos;
 
 private:
 	// button action functions
@@ -178,29 +194,8 @@ private:
 	bool shortcutPadAction(ModelStackWithAutoParam* modelStackWithParam, Clip* clip, Output* output,
 	                       OutputType outputType, int32_t effectiveLength, int32_t x, int32_t y, int32_t velocity,
 	                       int32_t xScroll, int32_t xZoom, SquareInfo& squareInfo);
-	bool toggleAutomationInterpolation();
-	bool toggleAutomationPadSelectionMode(ModelStackWithAutoParam* modelStackWithParam, int32_t effectiveLength,
-	                                      int32_t xScroll, int32_t xZoom);
 	void handleParameterSelection(Clip* clip, Output* output, OutputType outputType, int32_t xDisplay,
 	                              int32_t yDisplay);
-	void noteEditPadAction(ModelStackWithNoteRow* modelStackWithNoteRow, NoteRow* noteRow, InstrumentClip* clip,
-	                       int32_t x, int32_t y, int32_t velocity, int32_t effectiveLength, SquareInfo& squareInfo);
-	void velocityEditPadAction(ModelStackWithNoteRow* modelStackWithNoteRow, NoteRow* noteRow, InstrumentClip* clip,
-	                           int32_t x, int32_t y, int32_t velocity, int32_t effectiveLength, SquareInfo& squareInfo);
-	int32_t getVelocityFromY(int32_t y);
-	int32_t getYFromVelocity(int32_t velocity);
-	void addNoteWithNewVelocity(int32_t x, int32_t velocity, int32_t newVelocity);
-	void adjustNoteVelocity(ModelStackWithNoteRow* modelStackWithNoteRow, NoteRow* noteRow, int32_t x, int32_t velocity,
-	                        int32_t newVelocity, uint8_t squareType);
-	void setVelocity(ModelStackWithNoteRow* modelStackWithNoteRow, NoteRow* noteRow, int32_t x, int32_t newVelocity);
-	void setVelocityRamp(ModelStackWithNoteRow* modelStackWithNoteRow, NoteRow* noteRow,
-	                     SquareInfo rowSquareInfo[kDisplayWidth], int32_t velocityIncrement);
-	void recordNoteEditPadAction(int32_t x, int32_t velocity);
-	void automationEditPadAction(ModelStackWithAutoParam* modelStackWithParam, Clip* clip, int32_t xDisplay,
-	                             int32_t yDisplay, int32_t velocity, int32_t effectiveLength, int32_t xScroll,
-	                             int32_t xZoom);
-	bool recordAutomationSinglePadPress(int32_t xDisplay, int32_t yDisplay);
-
 	// mute pad action
 	ActionResult handleMutePadAction(ModelStackWithTimelineCounter* modelStackWithTimelineCounter,
 	                                 InstrumentClip* instrumentClip, Output* output, OutputType outputType, int32_t y,
@@ -221,68 +216,17 @@ private:
 	                              OutputType outputType, RGB image[][kDisplayWidth + kSideBarWidth],
 	                              uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], int32_t xDisplay,
 	                              bool isMIDICVDrum);
-	void renderAutomationEditor(ModelStackWithAutoParam* modelStackWithParam, Clip* clip,
-	                            RGB image[][kDisplayWidth + kSideBarWidth],
-	                            uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], int32_t renderWidth,
-	                            int32_t xScroll, uint32_t xZoom, int32_t effectiveLength, int32_t xDisplay,
-	                            bool drawUndefinedArea, deluge::modulation::params::Kind kind, bool isBipolar);
-	void renderAutomationColumn(ModelStackWithAutoParam* modelStackWithParam,
-	                            RGB image[][kDisplayWidth + kSideBarWidth],
-	                            uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], int32_t lengthToDisplay,
-	                            int32_t xDisplay, bool isAutomated, int32_t xScroll, int32_t xZoom,
-	                            deluge::modulation::params::Kind kind, bool isBipolar);
-	void renderAutomationBipolarSquare(RGB image[][kDisplayWidth + kSideBarWidth],
-	                                   uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], int32_t xDisplay,
-	                                   int32_t yDisplay, bool isAutomated, deluge::modulation::params::Kind kind,
-	                                   int32_t knobPos);
-	void renderAutomationUnipolarSquare(RGB image[][kDisplayWidth + kSideBarWidth],
-	                                    uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], int32_t xDisplay,
-	                                    int32_t yDisplay, bool isAutomated, int32_t knobPos);
-	void renderNoteEditor(ModelStackWithNoteRow* modelStackWithNoteRow, InstrumentClip* clip,
-	                      RGB image[][kDisplayWidth + kSideBarWidth],
-	                      uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], int32_t renderWidth, int32_t xScroll,
-	                      uint32_t xZoom, int32_t effectiveLength, int32_t xDisplay, bool drawUndefinedArea,
-	                      SquareInfo& squareInfo);
-	void renderNoteColumn(ModelStackWithNoteRow* modelStackWithNoteRow, InstrumentClip* clip,
-	                      RGB image[][kDisplayWidth + kSideBarWidth],
-	                      uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], int32_t xDisplay, int32_t xScroll,
-	                      int32_t xZoom, SquareInfo& squareInfo);
-	void renderNoteSquare(RGB image[][kDisplayWidth + kSideBarWidth],
-	                      uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], int32_t xDisplay, int32_t yDisplay,
-	                      uint8_t squareType, int32_t value);
-	void renderUndefinedArea(int32_t xScroll, uint32_t xZoom, int32_t lengthToDisplay,
-	                         RGB image[][kDisplayWidth + kSideBarWidth],
-	                         uint8_t occupancyMask[][kDisplayWidth + kSideBarWidth], int32_t imageWidth,
-	                         TimelineView* timelineView, bool tripletsOnHere, int32_t xDisplay);
 	void renderDisplayOLED(Clip* clip, Output* output, OutputType outputType, int32_t knobPosLeft = kNoSelection,
 	                       int32_t knobPosRight = kNoSelection);
 	void renderAutomationOverviewDisplayOLED(deluge::hid::display::oled_canvas::Canvas& canvas, Output* output,
 	                                         OutputType outputType);
-	void renderAutomationEditorDisplayOLED(deluge::hid::display::oled_canvas::Canvas& canvas, Clip* clip,
-	                                       OutputType outputType, int32_t knobPosLeft, int32_t knobPosRight);
-	void renderNoteEditorDisplayOLED(deluge::hid::display::oled_canvas::Canvas& canvas, InstrumentClip* clip,
-	                                 OutputType outputType, int32_t knobPosLeft, int32_t knobPosRight);
 	void renderDisplay7SEG(Clip* clip, Output* output, OutputType outputType, int32_t knobPosLeft = kNoSelection,
 	                       bool modEncoderAction = false);
 	void renderAutomationOverviewDisplay7SEG(Output* output, OutputType outputType);
-	void renderAutomationEditorDisplay7SEG(Clip* clip, OutputType outputType, int32_t knobPosLeft,
-	                                       bool modEncoderAction);
-	void renderNoteEditorDisplay7SEG(InstrumentClip* clip, OutputType outputType, int32_t knobPosLeft);
 
 	// Horizontal Encoder Action
 	void shiftAutomationHorizontally(ModelStackWithAutoParam* modelStackWithParam, int32_t offset,
 	                                 int32_t effectiveLength);
-
-	// Mod Encoder Action
-	bool automationModEncoderActionForSelectedPad(ModelStackWithAutoParam* modelStackWithParam, int32_t whichModEncoder,
-	                                              int32_t offset, int32_t effectiveLength);
-	void automationModEncoderActionForUnselectedPad(ModelStackWithAutoParam* modelStackWithParam,
-	                                                int32_t whichModEncoder, int32_t offset, int32_t effectiveLength);
-
-	// Mod Encoder Button Action
-	void copyAutomation(ModelStackWithAutoParam* modelStackWithParam, Clip* clip, int32_t xScroll, int32_t xZoom);
-	void pasteAutomation(ModelStackWithAutoParam* modelStackWithParam, Clip* clip, int32_t effectiveLength,
-	                     int32_t xScroll, int32_t xZoom);
 
 	// Select Encoder Action
 	void selectGlobalParam(int32_t offset, Clip* clip);
@@ -295,62 +239,13 @@ private:
 	void getLastSelectedNonGlobalParamArrayPosition(Clip* clip);
 	void getLastSelectedGlobalParamArrayPosition(Clip* clip);
 
-	// Automation Lanes Functions
-	void initPadSelection();
-	void initInterpolation();
-	ParamManagerForTimeline* getParamManagerForClip(Clip* clip);
-	int32_t getEffectiveLength(ModelStackWithTimelineCounter* modelStack);
-	uint32_t getSquareWidth(int32_t square, int32_t effectiveLength, int32_t xScroll, int32_t xZoom);
-	uint32_t getMiddlePosFromSquare(int32_t xDisplay, int32_t effectiveLength, int32_t xScroll, int32_t xZoom);
-
-	void getAutomationParameterName(Clip* clip, OutputType outputType, StringBuf& parameterName);
-
-	bool getAutomationNodeInterpolation(ModelStackWithAutoParam* modelStack, int32_t pos, bool reversed);
-	void setAutomationParameterValue(ModelStackWithAutoParam* modelStack, int32_t knobPos, int32_t squareStart,
-	                                 int32_t xDisplay, int32_t effectiveLength, int32_t xScroll, int32_t xZoom,
-	                                 bool modEncoderAction = false);
-	void updateAutomationModPosition(ModelStackWithAutoParam* modelStack, uint32_t squareStart,
-	                                 bool updateDisplay = true, bool updateIndicatorLevels = true);
-
-	void handleAutomationSinglePadPress(ModelStackWithAutoParam* modelStackWithParam, Clip* clip, int32_t xDisplay,
-	                                    int32_t yDisplay, int32_t effectiveLength, int32_t xScroll, int32_t xZoom);
-	void handleAutomationParameterChange(ModelStackWithAutoParam* modelStackWithParam, Clip* clip,
-	                                     OutputType outputType, int32_t xDisplay, int32_t yDisplay,
-	                                     int32_t effectiveLength, int32_t xScroll, int32_t xZoom);
-	int32_t calculateAutomationKnobPosForPadPress(ModelStackWithAutoParam* modelStackWithParam, OutputType outputType,
-	                                              int32_t yDisplay);
-	int32_t calculateAutomationKnobPosForMiddlePadPress(deluge::modulation::params::Kind kind, int32_t yDisplay);
-	int32_t calculateAutomationKnobPosForSinglePadPress(deluge::modulation::params::Kind kind, int32_t yDisplay);
-
-	void handleAutomationMultiPadPress(ModelStackWithAutoParam* modelStackWithParam, Clip* clip, int32_t firstPadX,
-	                                   int32_t firstPadY, int32_t secondPadX, int32_t secondPadY,
-	                                   int32_t effectiveLength, int32_t xScroll, int32_t xZoom,
-	                                   bool modEncoderAction = false);
-	void renderAutomationDisplayForMultiPadPress(ModelStackWithAutoParam* modelStackWithParam, Clip* clip,
-	                                             int32_t effectiveLength, int32_t xScroll, int32_t xZoom,
-	                                             int32_t xDisplay = kNoSelection, bool modEncoderAction = false);
-
-	int32_t calculateAutomationKnobPosForModEncoderTurn(ModelStackWithAutoParam* modelStackWithParam, int32_t knobPos,
-	                                                    int32_t offset);
-
 	void blinkShortcuts();
 	void resetParameterShortcutBlinking();
-
 	bool parameterShortcutBlinking;
-
 	bool interpolationShortcutBlinking;
 	bool padSelectionShortcutBlinking;
 
-	bool padSelectionOn;
-	bool multiPadPressActive;
-	bool middlePadPressSelected;
-	int32_t leftPadSelectedX;
-	int32_t leftPadSelectedY;
-	int32_t rightPadSelectedX;
-	int32_t rightPadSelectedY;
-	int32_t lastPadSelectedKnobPos;
-
-	bool playbackStopped;
+	int32_t getEffectiveLength(ModelStackWithTimelineCounter* modelStack);
 
 	// grid sized array to assign midi cc values to each pad on the grid
 	void initMIDICCShortcutsForAutomation();
