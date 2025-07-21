b15b6b0277b9c20cdced3ace2e6914fa97eeb52d
Katherine Whitlock <kate@skylinesynths.nyc>
2025-07-21 10:25:03 -0400

    Convert applyAmplitude to Argon (#3439)
    * convert applyAmplitude to Argon
    
    * Simplify loop

--- src/deluge/dsp/oscillators/oscillator.h
+++ src/deluge/dsp/oscillators/oscillator.h
@@ -17,15 +17,16 @@
 #pragma once
 
 #include "storage/wave_table/wave_table.h"
+#include <span>
 namespace deluge::dsp {
 
 class Oscillator {
 	static void maybeStorePhase(const OscType& type, uint32_t* startPhase, uint32_t phase, bool doPulseWave);
-	static void applyAmplitudeVectorToBuffer(int32_t amplitude, int32_t numSamples, int32_t amplitudeIncrement,
-	                                         int32_t* outputBufferPos, int32_t* inputBuferPos);
+	static void applyAmplitude(std::span<q31_t> input_span, std::span<q31_t> output_span, int32_t amplitude,
+	                           int32_t amplitude_increment);
 
 public:
-	static void renderOsc(OscType type, int32_t amplitude, int32_t* bufferStart, int32_t* bufferEnd, int32_t numSamples,
+	static void renderOsc(OscType type, int32_t amplitude, int32_t* bufferStart, int32_t* bufferEnd, size_t numSamples,
 	                      uint32_t phaseIncrement, uint32_t pulseWidth, uint32_t* startPhase, bool applyAmplitude,
 	                      int32_t amplitudeIncrement, bool doOscSync, uint32_t resetterPhase,
 	                      uint32_t resetterPhaseIncrement, uint32_t retriggerPhase, int32_t waveIndexIncrement,
