fbe8dc49b631ab926a82124ca015e2a5295cbd14
Metamere <62861841+Metamere@users.noreply.github.com>
2025-07-24 21:48:35 -0500

    Fix kit velocity drum info popup display glitches and crash (#3973)
    There was a bug where for certain combinations of kit sounds, if a pad for one drum sound was held and then a pad for a different sound was pressed and released, it sent a null pointer or other incorrect drum object, which resulted in it displaying incorrect information (either midi info, "no sound", empty, or a different drum name from the kit), and if the second pad was pressed and released enough times, it would inevitably cause a crash.
    
    To remedy this, the drum object was replaced with the selectedDrum object, with a check for a nullptr. This required the drawNoteCode function call to be moved below the setSelectedDrum function call for when it is not in the velocity drum view.

--- src/deluge/gui/views/instrument_clip_view.cpp
+++ src/deluge/gui/views/instrument_clip_view.cpp
@@ -1801,7 +1801,7 @@ void InstrumentClipView::selectEncoderAction(int8_t offset) {
 		}
 	}
 
-	// Or, if user holding a note(s) down, we'll adjust proability / iterance instead
+	// Or, if user holding a note(s) down, we'll adjust probability / iterance instead
 	else if (currentUIMode == UI_MODE_NOTES_PRESSED) {
 		bool hasProbabilityPopup = display->hasPopupOfType(PopupType::PROBABILITY);
 		bool hasIterancePopup = display->hasPopupOfType(PopupType::ITERANCE);
@@ -5205,7 +5205,6 @@ bool InstrumentClipView::startAuditioningRow(int32_t velocity, int32_t yDisplay,
 		enterUIMode(UI_MODE_AUDITIONING);
 	}
 
-	drawNoteCode(yDisplay);
 	bool lastAuditionedYDisplayChanged = lastAuditionedYDisplay != yDisplay;
 	lastAuditionedYDisplay = yDisplay;
 
@@ -5224,8 +5223,13 @@ bool InstrumentClipView::startAuditioningRow(int32_t velocity, int32_t yDisplay,
 
 	if (isKit) {
 		setSelectedDrum(drum);
+		drawNoteCode(yDisplay);
 		return false; // No need to redraw any squares, because setSelectedDrum() has done it
 	}
+	else {
+		drawNoteCode(yDisplay);
+	}
+
 	return true;
 }
 
@@ -5441,7 +5445,11 @@ void InstrumentClipView::drawNoteCode(uint8_t yDisplay) {
 		drawActualNoteCode(getCurrentInstrumentClip()->getYNoteFromYDisplay(yDisplay, currentSong));
 	}
 	else {
-		drawDrumName(getCurrentInstrumentClip()->getNoteRowOnScreen(yDisplay, currentSong)->drum);
+		InstrumentClip* clip = getCurrentInstrumentClip();
+		Kit* thisKit = (Kit*)clip->output;
+		if (thisKit->selectedDrum != nullptr) {
+			drawDrumName(thisKit->selectedDrum);
+		}
 	}
 }
 
