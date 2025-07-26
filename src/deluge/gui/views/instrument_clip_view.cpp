c6af62c9f017b155893cf44674b297dca412b4f1
Metamere <62861841+Metamere@users.noreply.github.com>
2025-07-26 14:56:06 -0500

    Prevent the creation of "NO SOUND" kit rows (#3977)
    * Prevent the creation of "no sound" kit rows
    
    Prevent the creation of a kit row upon pressing a pad in the main screen outside of the bounds of the current kit. Instead, now it will simply deselect  any selected rows.
    
    * comment correction
    
    comment correction

--- src/deluge/gui/views/instrument_clip_view.cpp
+++ src/deluge/gui/views/instrument_clip_view.cpp
@@ -4151,7 +4151,7 @@ doDisplayError:
 		}
 	}
 
-	// Or, if a kit
+	// If a kit, prevents creating a new kit row beyond the adjacent empty rows
 	else {
 		// If it's more than one row below, we can't do it
 		if (yDisplay < -1 - clip->yScroll) {
@@ -4161,16 +4161,6 @@ doDisplayError:
 		// If it's more than one row above, we can't do it
 		if (yDisplay > clip->getNumNoteRows() - clip->yScroll) {
 			goto getOut;
-		}
-
-		noteRow = createNewNoteRowForKit(modelStack, yDisplay, &noteRowId);
-
-		if (!noteRow) {
-			goto doDisplayError;
-		}
-
-		else {
-			uiNeedsRendering(this, 0, 1 << yDisplay);
 		}
 	}
 
