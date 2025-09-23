895554b0f1d6b0654e004ae6a1f415174dce7cf0
Chris Griggs <cg1207@gmail.com>
2025-09-23 16:35:29 +0100

    Fix channel display in view.cpp
    - Set nameToDraw to buffer for proper channel display
    - Ensures channel numbers are properly displayed on OLED

--- src/deluge/gui/views/view.cpp
+++ src/deluge/gui/views/view.cpp
@@ -2162,6 +2162,7 @@ yesAlignRight:
 		if (display->haveOLED()) {
 			if (channel < 16) {
 				slotToString(channel + 1, channelSuffix, buffer, 1);
+				nameToDraw = buffer;
 				goto oledDrawString;
 			}
 			else if (channel == MIDI_CHANNEL_MPE_LOWER_ZONE || channel == MIDI_CHANNEL_MPE_UPPER_ZONE) {
