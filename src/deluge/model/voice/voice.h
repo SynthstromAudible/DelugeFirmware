9d47f9cf36e155f95b25a722c313752606c1d062
Mark Adams <mark.adams@queensu.ca>
2025-10-15 19:24:54 -0400

    actually release fast for fast release (#4146)

--- src/deluge/model/voice/voice.h
+++ src/deluge/model/voice/voice.h
@@ -113,7 +113,7 @@ public:
 
 	/// Release immediately with provided release rate
 	/// Returns whether voice should still be left active
-	bool doFastRelease(uint32_t releaseIncrement = 4096);
+	bool doFastRelease(uint32_t releaseIncrement);
 
 	/// Sets envelope to off (will interpolate through this render window).
 	/// Returns whether voice should still be left active
