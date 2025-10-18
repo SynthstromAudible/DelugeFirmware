d0ba1237a12005ef6e19dcea83a98cf091038c5d
Leonid Burygin <52497264+lburygin@users.noreply.github.com>
2025-10-18 21:34:57 +0300

    initial commit

--- src/deluge/gui/menu_item/automation/automation.cpp
+++ src/deluge/gui/menu_item/automation/automation.cpp
@@ -176,7 +176,7 @@ void Automation::selectAutomationViewParameter(bool clipMinder) {
 void Automation::handleAutomationViewParameterUpdate() {
 	UI* root = getRootUI();
 	if (root == &automationView) {
-		bool clipMinder = automationView.previousUI->toClipMinder() != nullptr;
+		bool clipMinder = getRootUI()->toClipMinder() != nullptr;
 		Automation::selectAutomationViewParameter(clipMinder);
 		uiNeedsRendering(root);
 	}
