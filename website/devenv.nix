7c184dd0a1865256d3d331fce774cbd7215ee527
mgabor3141 <>
2025-11-10 19:19:14 +0100

    Add devenv.sh for website dev environment

---
+++ website/devenv.nix
@@ -0,0 +1,50 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/integrations/codespaces-devcontainer/
+  devcontainer.enable = true;
+
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+  languages.javascript.enable = true;
+  languages.javascript.bun.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.install.exec = ''
+    bun install
+  '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    install         # Run scripts directly
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
