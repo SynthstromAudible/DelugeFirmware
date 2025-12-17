82c20ee85bfc366f0704ed30bdf36eb5412b380c
Jordan Schwartz <jordan.kyle.schwartz@gmail.com>
2025-12-17 16:52:30 -0600

    commenting out number stuff for sorting

--- src/deluge/util/functions.cpp
+++ src/deluge/util/functions.cpp
@@ -1735,130 +1735,131 @@ int32_t strcmpspecial(char const* first, char const* second) {
 		bool secondIsNumber = (*second >= '0' && *second <= '9');
 
 		// If they're both numbers...
-		if (firstIsNumber && secondIsNumber) {
+		/*
+		if (firstIsNumber && secondIsNumber && false) {
 
-			// If we haven't yet seen a differing number of leading zeros in a number, see if that exists here.
-			if (!resultIfGetToEndOfBothStrings) {
-				char const* firstHere = first;
-				char const* secondHere = second;
-				while (true) {
-					char firstChar = *firstHere;
-					char secondChar = *secondHere;
-					bool firstDigitIsLeadingZero = (firstChar == '0');
-					bool secondDigitIsLeadingZero = (secondChar == '0');
+		    // If we haven't yet seen a differing number of leading zeros in a number, see if that exists here.
+		    if (!resultIfGetToEndOfBothStrings) {
+		        char const* firstHere = first;
+		        char const* secondHere = second;
+		        while (true) {
+		            char firstChar = *firstHere;
+		            char secondChar = *secondHere;
+		            bool firstDigitIsLeadingZero = (firstChar == '0');
+		            bool secondDigitIsLeadingZero = (secondChar == '0');
 
-					if (firstDigitIsLeadingZero && secondDigitIsLeadingZero) { // If both are zeros, look at next chars.
-						firstHere++;
-						secondHere++;
-						continue;
-					}
+		            if (firstDigitIsLeadingZero && secondDigitIsLeadingZero) { // If both are zeros, look at next chars.
+		                firstHere++;
+		                secondHere++;
+		                continue;
+		            }
 
-					// else if (!firstDigitIsLeadingZero || !secondDigitIsLeadingZero) break;	// If both are not
-					// zeros, we're done.
-					//  Actually, the same end result is achieved without that line.
+		            // else if (!firstDigitIsLeadingZero || !secondDigitIsLeadingZero) break;	// If both are not
+		            // zeros, we're done.
+		            //  Actually, the same end result is achieved without that line.
 
-					// If we're still here, one is a leading zero and the other isn't.
-					resultIfGetToEndOfBothStrings =
-					    (int32_t)firstChar
-					    - (int32_t)secondChar; // Will still end up as zero when that needs to happen.
-					break;
+		            // If we're still here, one is a leading zero and the other isn't.
+		            resultIfGetToEndOfBothStrings =
+		                (int32_t)firstChar
+		                - (int32_t)secondChar; // Will still end up as zero when that needs to happen.
+		            break;
+		        }
+		    }
+
+		    int32_t firstNumber = *first - '0';
+		    int32_t secondNumber = *second - '0';
+		    first++;
+		    second++;
+
+		    while (*first >= '0' && *first <= '9') {
+		        firstNumber *= 10;
+		        firstNumber += *first - '0';
+		        first++;
+		    }
+
+		    while (*second >= '0' && *second <= '9') {
+		        secondNumber *= 10;
+		        secondNumber += *second - '0';
+		        second++;
+		    }
+
+		    int32_t difference = firstNumber - secondNumber;
+		    if (difference) {
+		        return difference;
+		    }
+		}
+		*/
+		// Otherwise, if not both numbers...
+		// else {
+
+		char firstChar = *first;
+		char secondChar = *second;
+
+		// Make lowercase
+		if (firstChar >= 'A' && firstChar <= 'Z') {
+			firstChar += 32;
+		}
+		if (secondChar >= 'A' && secondChar <= 'Z') {
+			secondChar += 32;
+		}
+
+		// If we're doing note ordering...
+		if (shouldInterpretNoteNames) {
+			ComparativeNoteNumber firstResult, secondResult;
+			firstResult.noteNumber = 100000;
+			firstResult.stringLength = 0;
+			secondResult.noteNumber = 100000;
+			secondResult.stringLength = 0;
+
+			if (firstChar >= 'a' && firstChar <= 'g') {
+				firstResult = getComparativeNoteNumberFromChars(first, firstChar, octaveStartsFromA);
+			}
+
+			if (secondChar >= 'a' && secondChar <= 'g') {
+				secondResult = getComparativeNoteNumberFromChars(second, secondChar, octaveStartsFromA);
+			}
+
+			if (firstResult.noteNumber == secondResult.noteNumber) {
+				if (!firstResult.stringLength && !secondResult.stringLength) {
+					goto doNormal;
 				}
+				first += firstResult.stringLength;
+				second += secondResult.stringLength;
 			}
+			else {
+				return firstResult.noteNumber - secondResult.noteNumber;
+			}
+		}
 
-			int32_t firstNumber = *first - '0';
-			int32_t secondNumber = *second - '0';
-			first++;
-			second++;
-
-			while (*first >= '0' && *first <= '9') {
-				firstNumber *= 10;
-				firstNumber += *first - '0';
+		else {
+doNormal:
+			// If they're the same, carry on
+			if (firstChar == secondChar) {
 				first++;
-			}
-
-			while (*second >= '0' && *second <= '9') {
-				secondNumber *= 10;
-				secondNumber += *second - '0';
 				second++;
 			}
 
-			int32_t difference = firstNumber - secondNumber;
-			if (difference) {
-				return difference;
-			}
-		}
-
-		// Otherwise, if not both numbers...
-		else {
-
-			char firstChar = *first;
-			char secondChar = *second;
-
-			// Make lowercase
-			if (firstChar >= 'A' && firstChar <= 'Z') {
-				firstChar += 32;
-			}
-			if (secondChar >= 'A' && secondChar <= 'Z') {
-				secondChar += 32;
-			}
-
-			// If we're doing note ordering...
-			if (shouldInterpretNoteNames) {
-				ComparativeNoteNumber firstResult, secondResult;
-				firstResult.noteNumber = 100000;
-				firstResult.stringLength = 0;
-				secondResult.noteNumber = 100000;
-				secondResult.stringLength = 0;
-
-				if (firstChar >= 'a' && firstChar <= 'g') {
-					firstResult = getComparativeNoteNumberFromChars(first, firstChar, octaveStartsFromA);
-				}
-
-				if (secondChar >= 'a' && secondChar <= 'g') {
-					secondResult = getComparativeNoteNumberFromChars(second, secondChar, octaveStartsFromA);
-				}
-
-				if (firstResult.noteNumber == secondResult.noteNumber) {
-					if (!firstResult.stringLength && !secondResult.stringLength) {
-						goto doNormal;
-					}
-					first += firstResult.stringLength;
-					second += secondResult.stringLength;
-				}
-				else {
-					return firstResult.noteNumber - secondResult.noteNumber;
-				}
-			}
-
+			// Otherwise...
 			else {
-doNormal:
-				// If they're the same, carry on
-				if (firstChar == secondChar) {
-					first++;
-					second++;
+				// Dot then underscore comes first
+				if (firstChar == '.') {
+					return -1;
+				}
+				else if (secondChar == '.') {
+					return 1; // We know they're not both the same - see above.
 				}
 
-				// Otherwise...
-				else {
-					// Dot then underscore comes first
-					if (firstChar == '.') {
-						return -1;
-					}
-					else if (secondChar == '.') {
-						return 1; // We know they're not both the same - see above.
-					}
-
-					if (firstChar == '_') {
-						return -1;
-					}
-					else if (secondChar == '_') {
-						return 1;
-					}
-
-					return (int32_t)firstChar - (int32_t)secondChar;
+				if (firstChar == '_') {
+					return -1;
 				}
+				else if (secondChar == '_') {
+					return 1;
+				}
+
+				return (int32_t)firstChar - (int32_t)secondChar;
 			}
 		}
+		//}
 	}
 }
 
