b92f5f6a9577ae948f0b81cc3b90ae6d69f5e178
m-m-adams <mark.adams@queensu.ca>
2026-04-21 00:21:34 -0400

    clearer code

--- src/OSLikeStuff/timers_interrupts/timers_interrupts.c
+++ src/OSLikeStuff/timers_interrupts/timers_interrupts.c
@@ -18,44 +18,55 @@
 #include "timers_interrupts.h"
 
 #include "RZA1/system/r_typedefs.h"
-#include "RZA1/uart/sio_char.h"
-#include "deluge/deluge.h"
+#include "definitions.h"
 #include "deluge/drivers/mtu/mtu.h"
 #include "stdatomic.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-atomic_int critical_section_depth = 0;
+_Atomic int32_t critical_section_depth = 0;
 
 void ENTER_CRITICAL_SECTION() {
-	atomic_int expected = 0;
-	atomic_int desired = 1;
-	// if we go from 0 to 1 then we need to issue the barrier instructions, otherwise just increment depth
-	if (atomic_compare_exchange_strong(&critical_section_depth, &expected, desired)) {
+	int32_t expected = 0;
+	int32_t desired = 1;
+	// if we go from 0 to 1 then we need to issue the synchronization instructions, otherwise just increment depth
+	// avoids having to clear the full pipeline
+	if (atomic_compare_exchange_strong_explicit(&critical_section_depth, &expected, desired, memory_order_relaxed,
+	                                            memory_order_relaxed)) {
 		// memory creates a memory barrier in GCC to avoid reordering
 		// http://www.ibiblio.org/gferg/ldp/GCC-Inline-Assembly-HOWTO.html#ss5.3
 		__asm volatile("CPSID i" ::: "memory");
 		__asm volatile("DSB");
 		__asm volatile("ISB");
 	}
+	// in this case we're already in a critical section so just increment the count
 	else {
-		critical_section_depth++;
+		atomic_fetch_add_explicit(&critical_section_depth, 1, memory_order_relaxed);
 	}
 }
 
 void EXIT_CRITICAL_SECTION() {
 
-	/// if the section depth is greater than 0 then we are guaranteed to be in a critical section and therefore
-	/// can't have a toctou issue here
-	if (critical_section_depth > 0) {
-		atomic_fetch_sub(&critical_section_depth, 1);
-		if (critical_section_depth == 0) {
-			__asm volatile("CPSIE i" ::: "memory");
-			__asm volatile("DSB");
-			__asm volatile("ISB");
-		}
+	int32_t expected = 1;
+	int32_t desired = 0;
+	// if we go from 1 to 0 then exit the critical section. In any other case just decrement the counter
+	if (atomic_compare_exchange_strong_explicit(&critical_section_depth, &expected, desired, memory_order_relaxed,
+	                                            memory_order_relaxed)) {
+		__asm volatile("DSB");
+		__asm volatile("ISB");
+		__asm volatile("CPSIE i" ::: "memory");
 	}
+	// if that fails we're still in a critical section so just decrement the count
+	else {
+		atomic_fetch_sub_explicit(&critical_section_depth, 1, memory_order_relaxed);
+	}
+
+#if ALPHA_OR_BETA_VERSTION
+	if (atomic_load_explicit(&critical_section_depth, memory_order_relaxed) < 0) {
+		FREEZE_WITH_ERROR("OH NO");
+	}
+#endif
 }
 
 void clearIRQInterrupt(int irqNumber) {
