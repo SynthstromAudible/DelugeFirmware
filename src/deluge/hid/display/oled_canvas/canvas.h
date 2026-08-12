ea1e4b095c97a1698c7cf289220f99cc8bf69757
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2026-08-12 01:20:48 -0400

    Kerning rule adjustments for larger fonts
    Added ability to adjust kerning for specific combinations of larger fonts on the Deluge.
    
    Adjustments are tracked by font size, and the exact combination of previous and current character in a string.
    
    These adjustments are manual adjustments initially provided to me by Reza with some additional tweaks where needed.

--- src/deluge/hid/display/oled_canvas/canvas.h
+++ src/deluge/hid/display/oled_canvas/canvas.h
@@ -219,6 +219,13 @@ public:
 	/// @param isLastChar a boolean to specify whether any char's follow this char
 	int32_t getCharSpacingInPixels(uint8_t theChar, int32_t textHeight, bool isLastChar);
 
+	/// Returns spacing adjustment in pixels between characters drawn in a string
+	///
+	/// @param previousChar The previous character in the string
+	/// @param currentChar A single character being drawn after the previous character in the string
+	/// @param textHeight The height of the character ((to distinguish between non-bold and bold characters))
+	int32_t getPreviousCharSpacingAdjustmentInPixels(uint8_t previousChar, uint8_t currentChar, int32_t textHeight);
+
 	/// Returns width of a string in pixels
 	///
 	/// @param string A null-terminated C string
