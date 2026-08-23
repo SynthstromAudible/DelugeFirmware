3b22df820a6d0c65687d51f024dd9c882ecc02b2
Mark Adams <mark.adams@queensu.ca>
2026-08-23 04:35:21 +0000

    fix warning from unneeded fallthrough label (#4845)

--- src/deluge/gui/views/view.cpp
+++ src/deluge/gui/views/view.cpp
@@ -2803,9 +2803,8 @@ ActionResult View::clipStatusPadAction(Clip* clip, bool on, int32_t yDisplayIfIn
 		}
 		break;
 
-	// No break
 	case UI_MODE_CLIP_PRESSED_IN_SONG_VIEW:
-	[[fallthrough]] // fall through into UI_MODE_STUTTERING so you can toggle clip status while holding a clip
+	// fall through into UI_MODE_STUTTERING so you can toggle clip status while holding a clip
 	case UI_MODE_STUTTERING:
 		// this code is needed to allow users to launch clips while stuttering
 		// without it the deluge becomes unresponsive if you try to launch a clip while stuttering
