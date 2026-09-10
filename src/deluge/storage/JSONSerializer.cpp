5a0ba489671977e33bf9fd2f114fe9ce3b3f6d80
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2026-09-10 12:28:05 +0000

    Various Serializer/Deserializer Fixes (#4908)
    Fixed typo in JsonDeserializer
    Add some missing initializations
    Cleanup includes
    Fix buffer definitions

--- src/deluge/storage/JSONSerializer.cpp
+++ src/deluge/storage/JSONSerializer.cpp
@@ -16,31 +16,9 @@
  */
 
 #include "definitions_cxx.hpp"
-#include "drivers/pic/pic.h"
-#include "fatfs/fatfs.hpp"
-#include "gui/ui/sound_editor.h"
-#include "gui/ui_timer_manager.h"
-#include "hid/display/display.h"
-#include "io/debug/log.h"
-#include "memory/general_memory_allocator.h"
-#include "model/clip/instrument_clip.h"
-#include "model/drum/gate_drum.h"
-#include "model/drum/midi_drum.h"
-#include "model/instrument/cv_instrument.h"
-#include "model/instrument/kit.h"
-#include "model/instrument/midi_instrument.h"
-#include "model/song/song.h"
-#include "modulation/midi/midi_param.h"
-#include "modulation/midi/midi_param_collection.h"
-#include "processing/engines/audio_engine.h"
-#include "processing/sound/sound_drum.h"
-#include "processing/sound/sound_instrument.h"
-#include "storage/audio/audio_file_manager.h"
 #include "storage/storage_manager.h"
-#include "util/firmware_version.h"
-#include "util/functions.h"
-#include "util/try.h"
-#include "version.h"
+#include "util/d_string.h"
+#include "util/d_stringbuf.h"
 #include <string.h>
 
 extern "C" {
@@ -61,6 +39,8 @@ JsonSerializer::JsonSerializer() {
 
 void JsonSerializer::reset() {
 	resetWriter();
+	// Reset formatting as well as comma state when reusing a writer.
+	indentAmount = 0;
 	firstItemHasBeenWritten = false;
 }
 
@@ -70,7 +50,7 @@ void JsonSerializer::write(char const* output) {
 }
 
 void JsonSerializer::writeTag(char const* tag, int32_t number, bool box) {
-	char* buffer = shortStringBuffer;
+	char buffer[12];
 	intToString(number, buffer);
 	writeTag(tag, buffer, box, false);
 }
